1b8dbf85f38947d01dcec00c356a7e4482fd3933
ktonder <ktonder@localhost>
2013-05-13 18:51:22 +0200

    Sending mail to storeowner, only if the email address is not equal to the orders email address

---
+++ com.getshop.core/src/com/thundashop/core/databasemanager/SetCartPageToSystemPage.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thundashop.core.databasemanager;
+
+import com.thundashop.core.common.AppContext;
+import com.thundashop.core.common.DataCommon;
+import com.thundashop.core.common.ErrorException;
+import com.thundashop.core.databasemanager.data.Credentials;
+import com.thundashop.core.pagemanager.PageManager;
+import com.thundashop.core.pagemanager.data.Page;
+import com.thundashop.core.storemanager.StoreManager;
+import com.thundashop.core.storemanager.data.Store;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author ktonder
+ */
+@Component
+public class SetCartPageToSystemPage {
+    
+    @Autowired
+    public Database database;
+    
+    public void update() throws ErrorException {
+        Credentials credentials = new Credentials(StoreManager.class);
+        credentials.manangerName = "StoreManager";
+        credentials.storeid = "all";
+        credentials.password = "ADSFASDF";
+        for (DataCommon data : database.retreiveData(credentials)) {
+            if (data instanceof Store) {
+                Store store = (Store)data;
+                
+                Credentials cred = new Credentials(PageManager.class);
+                cred.storeid = store.id;
+                cred.manangerName = PageManager.class.getSimpleName();
+                cred.password = "ASDFASDF2";
+                
+                List<DataCommon> dataCommons = database.retreiveData(cred);
+                for (DataCommon pagecommon : dataCommons) {
+                    if (pagecommon instanceof Page) {
+                        Page page = (Page)pagecommon;
+                        if (page.id.equals("cart")) {
+                            System.out.println(page.id);
+                            page.type = Page.PageType.HeaderMiddleFooter;
+                            database.save(page, cred);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    public static void main(String args[]) throws ErrorException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("All.xml");
+        AppContext.appContext = context;
+        
+        SetCartPageToSystemPage update = context.getBean(SetCartPageToSystemPage.class);
+        update.update();
+    }
+}

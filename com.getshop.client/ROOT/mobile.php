60ef4b4e12990ba2d9be2ce47fd0f09efa842f7d
Pal Tonder <Boggibill@gmail.com>
2013-06-14 20:10:07 +0200

    added login page for mobile applications.

--- com.getshop.client/ROOT/mobile.php
+++ com.getshop.client/ROOT/mobile.php
@@ -7,8 +7,28 @@
     include '../loader.php';
     $factory = IocContainer::getFactorySingelton();
     $factory->loadJavascriptFiles(false);
+    
+    if(isset($_POST['username'])) {
+        $factory->getApi()->getUserManager()->logOn($_POST['username'], $_POST['password']);
+    }
+    
+    if(!$factory->getApi()->getUserManager()->isLoggedIn()) {
+        ?>
+        <form action="" method="post">
+            <div style="text-align: center;">
+                <div><? echo $factory->__f("You are not logged on, please log on."); ?></div>
+                <div><input type='text' name="username"></div>
+                <div><input type='password' name="password"></div>
+                <div><input type='submit' value="Sign in"></div>
+            </div>
+        </form>
+        <?
+        return;
+    }
+    
     $app = $factory->getApplicationPool()->getApplicationsInstancesByNamespace("ns_" . str_replace("-","_",$_GET['app']));
     $app = $app[0];
+    
     echo "<div class='applicationarea middle inline' area='middle'>";
     echo "<div class='app ".$app->applicationSettings->appName."' appsettingsid='".$app->applicationSettings->id."' app='".$app->applicationSettings->appName."' appid='".$app->configuration->id."'>";
     $app->renderMobile();

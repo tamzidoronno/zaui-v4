df458898c87aabb122f742d3a804e9044c1c9070
Pal Tonder <Boggibill@gmail.com>
2013-06-28 13:13:33 +0200

    added save button to ckeditor!

--- com.getshop.client/ROOT/js/ckeditor/config.js
+++ com.getshop.client/ROOT/js/ckeditor/config.js
@@ -3,21 +3,38 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	
+CKEDITOR.editorConfig = function(config) {
+
     config.resize_minWidth = 100;
     config.width = 100;
     config.toolbar = [
-            {name: 'document', items: ['source', 'Preview', 'MediaEmbed']},
-            {name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
-            {name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt', 'JustifyLeft', 'JustifyCenter', 'JustifyRight']},
-            {name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar']},
-            {name: 'links', items: ['Link', 'Unlink', 'Anchor']},
-            '/',
-            {name: 'styles', items: ['Styles', 'Format', 'FontSize']},
-            {name: 'colors', items: ['TextColor', 'BGColor', 'Center']},
-            {name: 'basicstyles', items: ['Bold', 'Italic', 'Strike', '-', 'RemoveFormat']},
-            {name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote']}
-        ];
+        {name: 'document', items: ['Save']},
+        {name: 'clipboard', items: ['MediaEmbed','Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
+        {name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt', 'JustifyLeft', 'JustifyCenter', 'JustifyRight']},
+        {name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar']},
+        {name: 'links', items: ['Link', 'Unlink', 'Anchor']},
+        '/',
+        {name: 'styles', items: ['Styles', 'Format', 'FontSize']},
+        {name: 'colors', items: ['TextColor', 'BGColor', 'Center']},
+        {name: 'basicstyles', items: ['Bold', 'Italic', 'Strike', '-', 'RemoveFormat']},
+        {name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote']}
+    ];
     config.extraPlugins = 'mediaembed';
 };
+CKEDITOR.plugins.registered['save'] = {
+    init: function(editor)
+    {
+        var command = editor.addCommand('save',
+                {
+                    modes: {wysiwyg: 1, source: 1},
+                    exec: function(editor) {
+                        thundashop.common.addNotificationProgress('contentmanager', "Saving content");
+                        console.log(editor);
+                        thundashop.common.saveCKEditor(editor.getData(), $('#'+editor.name));
+                        thundashop.common.Alert('Saved', 'content has been saved');
+                    }
+                }
+        );
+        editor.ui.addButton('Save', {label: 'My Save', command: 'save'});
+    }
+}

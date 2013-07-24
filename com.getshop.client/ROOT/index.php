0cdef5b4cf8418684d7e41ece5b36491be29b738
Pal Tonder <Boggibill@gmail.com>
2013-07-24 19:35:33 +0200

    added wordpress support.

--- com.getshop.client/ROOT/index.php
+++ com.getshop.client/ROOT/index.php
@@ -29,7 +29,7 @@ function init($factory) {
     ob_end_clean();
 
     return $html;
-}
+} 
 
 include '../loader.php';
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">

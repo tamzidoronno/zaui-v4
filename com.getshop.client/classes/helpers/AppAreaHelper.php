1c822e9a077f7a6017d74cb7e48412f829144cb2
Pal Tonder <Boggibill@gmail.com>
2013-09-01 20:03:16 +0200

    adapting the rest of the skeletons to fit the latest design.

--- com.getshop.client/classes/helpers/AppAreaHelper.php
+++ com.getshop.client/classes/helpers/AppAreaHelper.php
@@ -1,11 +1,60 @@
 <?php
+
 class AppAreaHelper {
-    public static function printAppArea($page, $name, $include_bottom_margin = false) {
+
+    public static function printRows($page, $numberOfEntries, $offset = 1) {
+        $width = 100;
+        if ($numberOfEntries == 2) {
+            $width = 50;
+        } else if ($numberOfEntries == 3) {
+            $width = 33;
+        } else if ($numberOfEntries == 4) {
+            $width = 25;
+        } else if ($numberOfEntries == 5) {
+            $width = 20;
+        }
+        $numberOfEntries = $numberOfEntries + $offset-1;
         ?>
-        <div area="<? echo $name; ?>" class="applicationarea <? if($include_bottom_margin) { echo "gs_margin_bottom"; } ?>">
-            <?php $page->getApplicationArea($name)->render(); ?>
+        <table width="100%" cellspacing='0' cellpadding='0'>
+            <tr>
+                <?
+                for ($i = $offset; $i <= $numberOfEntries; $i++) {
+                    $class = "gs_col c$i ";
+                    if ($i == $offset) {
+                        $class .= "gs_margin_right";
+                    } else if ($i == $numberOfEntries) {
+                        $class .= "gs_margin_left";
+                    } else {
+                        $class .= "gs_margin_left gs_margin_right";
+                    }
+
+                    echo "<td width='$width%' valign='top' class='$class'>";
+                    AppAreaHelper::printAppArea($page, "col_$i");
+                    echo "</td>";
+                }
+                ?>
+            </tr>
+        </table>
+        <?
+    }
+
+    public static function printAppArea($page, $name, $include_bottom_margin = false, $include_right_margin = false, $include_left_margin = false) {
+        ?>
+        <div area="<? echo $name; ?>" class="applicationarea <?
+             if ($include_bottom_margin) {
+                 echo " gs_margin_bottom";
+             }
+             if ($include_right_margin) {
+                 echo " gs_margin_right";
+             }
+             if ($include_left_margin) {
+                 echo " gs_margin_left";
+             }
+             ?>">
+        <?php $page->getApplicationArea($name)->render(); ?>
         </div>
         <?
     }
+
 }
 ?>

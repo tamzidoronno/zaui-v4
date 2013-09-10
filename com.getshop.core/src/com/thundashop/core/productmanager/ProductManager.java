37bfe070c9e10911c66a27f9ce7c641ac397e5c6
Pal Tonder <Boggibill@gmail.com>
2013-09-10 15:42:54 +0200

    testing and fixing bugs.

--- com.getshop.core/src/com/thundashop/core/productmanager/ProductManager.java
+++ com.getshop.core/src/com/thundashop/core/productmanager/ProductManager.java
@@ -12,6 +12,8 @@ import com.thundashop.core.productmanager.data.AttributeValue;
 import com.thundashop.core.productmanager.data.Product;
 import com.thundashop.core.productmanager.data.ProductCriteria;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
@@ -20,42 +22,46 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
- * ProductManager is essential for a webshop. You can look at productmanager <br>
- * as a productcontainer, this container contains all the products for the webshop<br>
+ * ProductManager is essential for a webshop. You can look at productmanager
  * <br>
- * It has a set of API functions that can be used to receive data from it, and should<br>
+ * as a productcontainer, this container contains all the products for the
+ * webshop<br>
+ * <br>
+ * It has a set of API functions that can be used to receive data from it, and
+ * should<br>
  * be pretty straight forward to use.
  */
 @Component
 @Scope("prototype")
 public class ProductManager extends AProductManager implements IProductManager {
- 
+
     @Autowired
     public ProductManager(Logger log, DatabaseSaver databaseSaver) {
         super(log, databaseSaver);
     }
-    
+
     @Override
-     public void onReady() {
-     }
+    public void onReady() {
+    }
 
     @Override
     public Product saveProduct(Product product) throws ErrorException {
-        if (product.id == null || product.id.equals(""))
-            throw new ErrorException(87);        
-        
+        if (product.id == null || product.id.equals("")) {
+            throw new ErrorException(87);
+        }
+
 
         product.attributes = new ArrayList();
-        for(String group : product.attributesToSave.keySet()) {
+        for (String group : product.attributesToSave.keySet()) {
             AttributeValue value = pool.findAttributeValue(group, product.attributesToSave.get(group));
             product.attributes.add(value.id);
-            
+
         }
-        
+
         product.storeId = storeId;
         databaseSaver.saveObject(product, credentials);
         products.put(product.id, product);
-        
+
         return product;
     }
 
@@ -70,17 +76,18 @@ public class ProductManager extends AProductManager implements IProductManager {
     public List<Product> getProducts(ProductCriteria searchCriteria) throws ErrorException {
         return super.getProducts(searchCriteria);
     }
-    
+
     @Override
     public void removeProduct(String productId) throws ErrorException {
         Product product = products.get(productId);
-        
-        if (product == null)
+
+        if (product == null) {
             throw new ErrorException(27);
-        
+        }
+
         products.remove(product.id);
         databaseSaver.deleteObject(product, credentials);
-        
+
         throwEvent(Events.PRODUCT_DELETED, productId);
         ListManager manager = getManager(ListManager.class);
         manager.removeProductFromListsIfExists(productId);
@@ -94,10 +101,10 @@ public class ProductManager extends AProductManager implements IProductManager {
         product.id = UUID.randomUUID().toString();
         createProductPage(product);
         saveProduct(product);
-        
+
         return product;
     }
-    
+
     @Override
     public ArrayList<Product> getRandomProducts(Integer fetchSize, String ignoreProductId) throws ErrorException {
         return randomProducts(ignoreProductId, fetchSize);
@@ -106,9 +113,9 @@ public class ProductManager extends AProductManager implements IProductManager {
     @Override
     public HashMap<String, String> translateEntries(List<String> entryIds) throws ErrorException {
         HashMap result = new HashMap();
-        for(String id : entryIds) {
-            for(Product prod : products.values()) {
-                if(prod.pageId.equals(id)) {
+        for (String id : entryIds) {
+            for (Product prod : products.values()) {
+                if (prod.pageId.equals(id)) {
                     result.put(id, prod.name);
                 }
             }
@@ -122,14 +129,14 @@ public class ProductManager extends AProductManager implements IProductManager {
         product.setMainImage(imageId);
         saveProduct(product);
     }
-    
+
     @Override
     public void addImage(String productId, String productImageId, String description) throws ErrorException {
         Product product = getProduct(productId);
         product.addImage(productImageId, description);
         saveProduct(product);
     }
-    
+
     @Override
     public Product getProduct(String productId) throws ErrorException {
         return super.getProduct(productId);
@@ -140,12 +147,11 @@ public class ProductManager extends AProductManager implements IProductManager {
         return super.latestProducts(count);
     }
 
-
     @Override
     public List<AttributeValue> getAllAttributes() throws ErrorException {
         return pool.getAll();
     }
-    
+
     @Override
     public AttributeSummary getAttributeSummary() throws ErrorException {
         return cachedResult;
@@ -157,12 +163,12 @@ public class ProductManager extends AProductManager implements IProductManager {
         if (variations == null || variations.isEmpty() || product == null) {
             return product.price;
         }
-        
+
         double conversionRate = 1.0;
         if (getSession().currentUser == null || !getSession().currentUser.isAdministrator()) {
             conversionRate = ExchangeConvert.getExchangeRate(getSettings("Settings"));
         }
-        
+
         return product.getPrice(variations, conversionRate);
     }
 
@@ -172,13 +178,13 @@ public class ProductManager extends AProductManager implements IProductManager {
         List<String> uuidlist = new ArrayList();
         uuidlist.add(app_uuid);
         HashMap<String, List<String>> result = pmgr.getPagesForApplications(uuidlist);
-        if(result.isEmpty()) {
+        if (result.isEmpty()) {
             throw new ErrorException(1011);
         }
         ProductCriteria criteria = new ProductCriteria();
         criteria.pageIds.add(result.get(app_uuid).get(0));
         List<Product> foundresult = getProducts(criteria);
-        if(foundresult.isEmpty()) {
+        if (foundresult.isEmpty()) {
             throw new ErrorException(1011);
         }
         return foundresult.get(0);
@@ -186,33 +192,40 @@ public class ProductManager extends AProductManager implements IProductManager {
 
     @Override
     public List<Product> getAllProducts() throws ErrorException {
-        return new ArrayList(products.values());
+        ArrayList<Product> list = new ArrayList(products.values());
+        Comparator<Product> comparator = new Comparator<Product>() {
+            public int compare(Product c1, Product c2) {
+                return c1.rowCreatedDate.compareTo(c2.rowCreatedDate);
+            }
+        };
+        
+        Collections.sort(list, comparator); 
+        return list;
     }
 
     @Override
     public void updateAttributePool(List<AttributeValue> groups) throws ErrorException {
         List<String> valuesAdded = new ArrayList();
-        for(AttributeValue val : groups) {
+        for (AttributeValue val : groups) {
             val.storeId = storeId;
             databaseSaver.saveObject(val, credentials);
             pool.addAttributeValue(val);
             valuesAdded.add(val.id);
         }
-        
-        for(AttributeValue value : pool.getAll()) {
-            if(!valuesAdded.contains(value.id)) {
+
+        for (AttributeValue value : pool.getAll()) {
+            if (!valuesAdded.contains(value.id)) {
                 databaseSaver.deleteObject(value, credentials);
                 pool.remove(value.id);
-                
-                for(Product product : products.values()) {
-                    if(product.attributes != null && product.attributes.contains(value.id)) {
+
+                for (Product product : products.values()) {
+                    if (product.attributes != null && product.attributes.contains(value.id)) {
                         product.attributes.remove(value.id);
                         databaseSaver.saveObject(product, credentials);
                     }
                 }
             }
         }
-        
-    }
 
+    }
 }

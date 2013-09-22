b5a640d2089da49f1fd32c72964ec7c95b30f00b
Kai Tønder <ktonder@gmail.com>
2013-09-22 21:27:55 +0200

    Inital version of autoakademiet app for android and ios

--- com.getshop.mobile/autoakademiet/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/boggi/source/core/com.getshop.mobile/test/platforms/android/bin/AndroidManifest.xml : \
-/home/boggi/source/core/com.getshop.mobile/test/platforms/android/AndroidManifest.xml \

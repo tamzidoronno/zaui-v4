6a7c2c94e2404faa81837e4dc0ff1353fe8c30d5
Kai Tønder <ktonder@gmail.com>
2013-09-26 19:25:48 +0200

    Made common functions to seo url

--- com.getshop.client/ROOT/sitemap.php
+++ com.getshop.client/ROOT/sitemap.php
@@ -15,7 +15,7 @@ $address = "http://" . $_SERVER['SERVER_NAME'];
 
 foreach ($allProducts as $product) {
     echo "<url>\n";
-    echo "<loc>" . $address . "/?page=" . $product->pageId . "</loc>\n";
+    echo "<loc>" . $address .  \GetShopHelper::makeSeoUrl($product->name)."</loc>\n";
     echo "</url>\n";
 }
 
@@ -27,7 +27,7 @@ foreach ($lists as $list) {
                 continue;
             }
             echo "<url>\n";
-            echo "<loc>" . $address . "/?page=" . $entry->pageId . "</loc>\n";
+            echo "<loc>" . $address . GetShopHelper::makeSeoUrl($entry->name) . "</loc>\n";
             echo "</url>\n";
 
         }

dc693b006c5bea773f78575d75d440739ce0d4bb
Pal Tonder <Boggibill@gmail.com>
2013-10-17 09:34:57 +0200

    do not add footer on logon.

--- com.getshop.client/ROOT/index.php
+++ com.getshop.client/ROOT/index.php
@@ -94,8 +94,6 @@ if (!isset($_SESSION['checkifloggedout']) || !$_SESSION['checkifloggedout']) {
 if (isset($_GET['logonwithkey'])) {
     $key = $_GET['logonwithkey'];
     $_SESSION['loggedin'] = serialize($factory->getApi()->getUserManager()->logonUsingKey($key));
-    $sb = new SiteBuilder();
-    $sb->addFooterContent("<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
     header('location:index.php');
 }
 ?>

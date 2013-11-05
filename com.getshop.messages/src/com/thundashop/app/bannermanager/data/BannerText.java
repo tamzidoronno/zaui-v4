af5ca8f84a81671027148de44b0a548bb0240226
Pal Tonder <Boggibill@gmail.com>
2013-11-05 15:19:45 +0100

    tested and completed the banner manager.

--- com.getshop.messages/src/com/thundashop/app/bannermanager/data/BannerText.java
+++ com.getshop.messages/src/com/thundashop/app/bannermanager/data/BannerText.java
@@ -6,4 +6,6 @@ public class BannerText implements Serializable {
     public Double x;
     public Double y;
     public String text;
+    public Integer size = 12;
+    public String colour = "0000000";
 }

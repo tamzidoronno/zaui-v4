3aa0f52fc1bf47e721f714f9fb03eeff56a9dfac
Kai Tønder <ktonder@gmail.com>
2013-12-08 10:38:10 +0100

    Translated Changed Pay on delivery to be a webshop text instead of framework.

--- com.getshop.client/classes/Page.php
+++ com.getshop.client/classes/Page.php
@@ -145,8 +145,14 @@ class Page extends FactoryBase {
         }
         
         echo "<div class='gs_outer_mainarea'><div class='mainarea'>";
-        $pb = new PageBuilder($this->layout, $this->skeletonType, $this);
-        $pb->build();
+        if ($this->skeletonType == 5 ) {
+            $this->includefile("skeleton5");
+        } else if ($this->skeletonType == 6 ) {
+            $this->includefile("skeleton6");
+        } else {
+            $pb = new PageBuilder($this->layout, $this->skeletonType, $this);
+            $pb->build();
+        }
         echo "</div></div>";
 
         if (!$this->backendPage->hideFooter && $this->skeletonType != 5) {

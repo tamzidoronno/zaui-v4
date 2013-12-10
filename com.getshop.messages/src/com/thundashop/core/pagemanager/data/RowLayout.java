1c2a686807677ba63897c580ff948b7e80e2e647
Pal Tonder <Boggibill@gmail.com>
2013-12-10 18:03:37 +0100

    makes a page layout builder.

--- com.getshop.messages/src/com/thundashop/core/pagemanager/data/RowLayout.java
+++ com.getshop.messages/src/com/thundashop/core/pagemanager/data/RowLayout.java
@@ -1,9 +1,12 @@
 package com.thundashop.core.pagemanager.data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RowLayout implements Serializable {
     int numberOfCells = 3;
     int marginBottom = 0;
     int marginTop = 0;
+    List<Double> rowWidth = new ArrayList();
 }

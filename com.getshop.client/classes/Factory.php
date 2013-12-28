c2edda1922deaeaad2f4a7d1ef63ae476605d531
Pal Tonder <Boggibill@gmail.com>
2013-12-28 15:12:41 +0100

    extracted google font api to design applications.

--- com.getshop.client/classes/Factory.php
+++ com.getshop.client/classes/Factory.php
@@ -131,9 +131,11 @@ class Factory extends FactoryBase {
         echo "\n" . '<script type="text/javascript" src="js/getshop.rotate.js"></script>';
         echo "\n" . '<script type="text/javascript" src="js/getshop.PagePicker.js"></script>';
         echo "\n" . '<!--[if gte IE 8]><script src="js/jquery.xdr-transport.js"></script><![endif]-->';
-        echo "\n" . '<link id="google_webfont_1" rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600">';
         echo "\n" . '<link rel="stylesheet" type="text/css" href="js/jcrop/css/jquery.Jcrop.css">';
         echo "\n" . '<script type="text/javascript" src="js/getshopwebsocketapi/GetShopApiWebSocket.js"></script>';
+        if(method_exists($this->getApplicationPool()->getSelectedThemeApp(), "addScripts")) {
+            $this->getApplicationPool()->getSelectedThemeApp()->addScripts();
+        }
         ?>
         <script>
             $( document ).tooltip({ 

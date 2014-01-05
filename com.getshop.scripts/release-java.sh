11c9571fffeabdff8b8914e58438f14f27b6e1fc
Kai Tønder <ktonder@gmail.com>
2014-01-05 13:35:18 +0100

    Added api functions to release script

--- com.getshop.scripts/release-java.sh
+++ com.getshop.scripts/release-java.sh
@@ -7,14 +7,25 @@ cd lib
 put com.getshop.messages/dist/com.thundashop.messages.jar
 EOF
 
+cat << EOF > batchfile2
+lcd ../
+put apitodb.json
+EOF
+
 echo -e "Uploading thundashop jar files"
 sftp -b batchfile naxa@backend.getshop.com &> /dev/null
 rm -rf batchfile
 
+echo -e "Uploading api file"
+sftp -b batchfile2 naxa@backend.getshop.com &> /dev/null
+rm -rf batchfile2
+
 echo -e "Restarting java!";
-ssh -T naxa@backend.getshop.com << EOF &> /dev/null
+ssh -T naxa@backend.getshop.com << EOF &> /dev/null;
+
 cd dist; 
 kill -9 `ps aux |grep thunda |grep -v "auto" |awk '{print $2}'`
+java -Xmx1024m -XX:MaxPermSize=1024m -cp com.thundashop.core.jar:lib/* com.thundashop.core.apigenerator.ImportApiCallsToApplications;
 java -Xmx1024m -XX:MaxPermSize=1024m -cp com.thundashop.core.jar:lib/* com.thundashop.core.databasemanager.AddApplicationsToDatabase;
 ./start.sh
 EOF

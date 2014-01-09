6266dba141a7f8d46817cc2ab94d5f19fb6eb9f5
Kai Tønder <ktonder@gmail.com>
2014-01-09 20:19:47 +0100

    Added ProMeister certificates. Added FrameworkConfig reading from config.txt

--- com.getshop.core/src/com/thundashop/core/mobilemanager/AppleNotificationThread.java
+++ com.getshop.core/src/com/thundashop/core/mobilemanager/AppleNotificationThread.java
@@ -4,6 +4,7 @@
  */
 package com.thundashop.core.mobilemanager;
 
+import com.thundashop.core.common.FrameworkConfig;
 import javapns.Push;
 
 /**
@@ -11,21 +12,31 @@ import javapns.Push;
  * @author ktonder
  */
 public class AppleNotificationThread implements Runnable {
-    private String message;
-    private String tokenId;
-    private int badge;
+    private final String message;
+    private final String tokenId;
+    private final int badge;
+    private final String certificate;
+    private final String password;
+    private final boolean production;
 
-    public AppleNotificationThread(String message, String tokenId, int badge) {
+    public AppleNotificationThread(String message, String tokenId, int badge, String certificate, String password, FrameworkConfig frameworkConfig) {
         this.message = message;
         this.tokenId = tokenId;
+        this.production = frameworkConfig.productionMode;
         this.badge = badge;
+        this.certificate = certificate;
+        this.password = password;
     }
     
     
     @Override
     public void run() {
         try {
-            Push.combined(message, this.badge, "alert", "GetShop.p12", "auto1000", false, tokenId);
+            if (production) {
+                Push.combined(message, this.badge, "alert", "certs/"+certificate+"_prod.p12", password, true, tokenId);
+            } else {
+                Push.combined(message, this.badge, "alert", "certs/"+certificate+"_dev.p12", password, false, tokenId);
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }

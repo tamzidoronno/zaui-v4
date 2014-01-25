038fa321099ae9484a034213ffb2d5063493aa90
Kai Tønder <ktonder@gmail.com>
2014-01-25 11:14:36 +0100

    Changed logo for pdf document.

--- com.getshop.client/ROOT/generatePDF.php
+++ com.getshop.client/ROOT/generatePDF.php
@@ -8,8 +8,14 @@ if($type == "contentmanager") {
     $logo = $factory->getApplicationPool()->getApplicationsInstancesByNamespace("ns_974beda7_eb6e_4474_b991_5dbc9d24db8e");
     $logo = $logo[0];
     $imageId = $factory->getApi()->getLogoManager()->getLogo()->LogoId;
+    
+    if ($factory->getStore()->id == "2fac0e57-de1d-4fdf-b7e4-5f93e3225445") {
+        $imageId = "d1f46c58-0f53-498a-8f8c-a47d0012cb91";
+    }
+    
     $address = $factory->getStore()->webAddressPrimary;
-    $div = "<div style='background-color: #000; padding: 5px; margin-bottom: 20px; text-align: center;'><img style='' src='http://$address/displayImage.php?id=$imageId'/></div>";
+    
+    $div = "<div style='margin-bottom: 20px; '><img src='http://$address/displayImage.php?id=$imageId'/></div>";
     $content = $div.$factory->getApi()->getContentManager()->getContent($id);
 }
 

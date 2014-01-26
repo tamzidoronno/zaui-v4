ba213690f1288820127c8e56bf31ccb990db8801
Kai Tønder <ktonder@gmail.com>
2014-01-26 18:07:37 +0100

    Adjusted testing values

--- com.getshop.scripts/slowdown.sh
+++ com.getshop.scripts/slowdown.sh
@@ -1,3 +1,3 @@
 sudo tc qdisc add dev lo root handle 1: htb default 12
-sudo tc class add dev lo parent 1:1 classid 1:12 htb rate 260kbps ceil 1280kbps
-sudo tc qdisc add dev lo parent 1:12 netem delay 0ms
+sudo tc class add dev lo parent 1:1 classid 1:12 htb rate 256kbps ceil 1802kbps
+sudo tc qdisc add dev lo parent 1:12 netem delay 14ms

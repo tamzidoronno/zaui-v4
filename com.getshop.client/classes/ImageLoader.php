408542087a8a16c985c8433f227d1c955c430708
Kai Tønder <ktonder@gmail.com>
2014-02-24 15:55:45 +0100

    Fixed transparent images.

--- com.getshop.client/classes/ImageLoader.php
+++ com.getshop.client/classes/ImageLoader.php
@@ -24,8 +24,10 @@ class ImageLoader {
             $filename = "../demoimages/$id";
         }
         $image_info = getimagesize($filename);
+//        print_r($image_info);
         $this->raw = file_get_contents($filename);
         $this->image_type = $image_info[2];
+        
         if ($this->image_type == IMAGETYPE_JPEG) {
             $this->image = imagecreatefromjpeg($filename);
         } elseif ($this->image_type == IMAGETYPE_GIF) {
@@ -41,12 +43,21 @@ class ImageLoader {
         $top = $y;
         $crop_width = $x2-$x;
         $crop_height = $y2-$y;
+  
+        if ($this->image_type == IMAGETYPE_PNG) {
+            $canvas = imagecreatetruecolor($crop_width, $crop_height);
+            imagealphablending($canvas, false);
+            imagesavealpha($canvas, true);
+            imagecopy($canvas, $this->image, 0, 0, $left, $top, $this->getWidth(), $this->getHeight());
+            $this->image = $canvas;
+        } else {
+            $canvas = imagecreatetruecolor($crop_width, $crop_height);
+            imagecopy($canvas, $this->image, 0, 0, $left, $top, $this->getWidth(), $this->getHeight());
+            $this->image = $canvas;
+
+        }
         
-        $canvas = imagecreatetruecolor($crop_width, $crop_height);
-        imagealphablending($canvas, false);
-        imagesavealpha($canvas, true);
-        imagecopy($canvas, $this->image, 0, 0, $left, $top, $this->getWidth(), $this->getHeight());
-        $this->image = $canvas;
+        
     }
 
     function save($filename, $image_type = IMAGETYPE_JPEG, $compression = 75, $permissions = null) {
@@ -72,8 +83,7 @@ class ImageLoader {
             imagejpeg($this->image);
         } elseif ($image_type == IMAGETYPE_GIF) {
             imagegif($this->image);
-        } elseif ($image_type == IMAGETYPE_PNG) {
-            
+        } elseif ($image_type == IMAGETYPE_PNG) {            
             imagepng($this->image);
         }
     }
@@ -86,15 +96,88 @@ class ImageLoader {
         return imagesy($this->image);
     }
 
+    function resizeToHeight($height) {
+        $ratio = $height / $this->getHeight();
+        $width = $this->getWidth() * $ratio;
+        $this->resize($width, $height);
+    }
+
+    function resizeToWidth($width) {
+        $ratio = $width / $this->getWidth();
+        $height = $this->getheight() * $ratio;
+        $this->resize($width, $height);
+    }
+
+    function scale($scale) {
+        $width = $this->getWidth() * $scale / 100;
+        $height = $this->getheight() * $scale / 100;
+        $this->resize($width, $height);
+    }
+
+    function resize($in_width, $in_height) {
+        //Do not zoom.
+        //Keep the ratio
+        if ($this->getWidth() > $this->getHeight()) {
+            $ratio = $this->getHeight() / $this->getWidth();
+            $height = $in_width * $ratio;
+            $width = $in_width;
+        } else if ($this->getWidth() <= $this->getHeight()) {
+            $ratio = $this->getWidth() / $this->getHeight();
+            $width = $in_height * $ratio;
+            $height = $in_height;
+        }
+
+        if ($height > $in_height) {
+            $height = $in_height;
+            $ratio = $this->getWidth() / $this->getHeight();
+            $width = $in_height * $ratio;
+        }
+
+        if ($width > $in_width) {
+            $width = $in_width;
+            $ratio = $this->getHeight() / $this->getWidth();
+            $height = $in_width * $ratio;
+        }
+
+
+        if (!isset($width))
+            $width = $in_width;
+
+        if (!isset($height))
+            $height = $in_height;
+
+        if (($width > $this->getWidth() || $height > $this->getHeight()) && !$this->zoom) {
+            return;
+        }
+
+        if ($this->zoom) {
+            $width = $in_width;
+            $height = $in_height;
+        }
+
+        $new_image = imagecreatetruecolor($width, $height);
+        imagealphablending($new_image, false);
+        imagesavealpha($new_image, true);
+        imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+        $this->image = $new_image;
+        $this->displayRaw = false;
+    }
+
     public function zoom($zoom) {
         $this->zoom = $zoom;
     }
 
     public function rotate($degrees) {
-        imagesavealpha($this->image , true);
-        $pngTransparency = imagecolorallocatealpha($this->image , 0, 0, 0, 127);
-        imagefill($this->image , 0, 0, $pngTransparency);
-        $this->image= imagerotate($this->image, -1*$degrees, $pngTransparency);
+        if ($this->image_type === IMAGETYPE_PNG) {
+            imagesavealpha($this->image , true);
+            $pngTransparency = imagecolorallocatealpha($this->image , 0, 0, 0, 127);
+            imagefill($this->image , 0, 0, $pngTransparency);
+            $this->image= imagerotate($this->image, -1*$degrees, $pngTransparency);            
+        } else {
+            $pngTransparency = imagecolorallocatealpha($this->image , 0, 0, 0, 127);
+            $this->image= imagerotate($this->image, -1*$degrees, $pngTransparency);
+        }
+        
     }
 
 }

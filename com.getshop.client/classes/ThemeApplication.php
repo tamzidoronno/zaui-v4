eeb130bb5abfb233651dedc2c0d65924c4c372a7
Kai Tønder <ktonder@gmail.com>
2014-03-01 18:34:37 +0100

    Fixed banner height for snickles theme

--- com.getshop.client/classes/ThemeApplication.php
+++ com.getshop.client/classes/ThemeApplication.php
@@ -119,6 +119,12 @@ class ThemeApplication extends PredefinedPagesConfig {
         
         return null;
     }
+    
+    public function addBannerSlider($pageTag, $index, $pageTagGroup, $pageId, $area) {
+        $appconf = $this->api->getPageManager()->addApplicationToPage($pageId, "d612904c-8e44-4ec0-abf9-c03b62159ce4", $area);
+        $this->getApi()->getBannerManager()->addImage($appconf->id, "7cfb35c2-f43e-45fa-9c61-3f6d67b5c8f2");
+        $this->getApi()->getBannerManager()->addImage($appconf->id, "7007e885-e19f-4d42-98a5-84f3b1196f87");
+    }
 }
 
 ?>

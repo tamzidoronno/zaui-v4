396c79593a23ce03b0c9475e9a623a96a3724a50
Kai Tønder <ktonder@gmail.com>
2014-03-23 19:13:24 +0100

    Moved logic from one class to another.

--- com.getshop.core/src/com/thundashop/core/messagemanager/NewsLetterManager.java
+++ com.getshop.core/src/com/thundashop/core/messagemanager/NewsLetterManager.java
@@ -1,11 +1,18 @@
 package com.thundashop.core.messagemanager;
 
+import com.thundashop.core.common.DataCommon;
 import com.thundashop.core.common.DatabaseSaver;
 import com.thundashop.core.common.ErrorException;
 import com.thundashop.core.common.Logger;
 import com.thundashop.core.common.ManagerBase;
+import com.thundashop.core.databasemanager.data.DataRetreived;
 import com.thundashop.core.usermanager.UserManager;
+import com.thundashop.core.usermanager.data.User;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -13,13 +20,31 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 public class NewsLetterManager extends ManagerBase implements INewsLetterManager {
+    private List<NewsLetterGroup> groups = new CopyOnWriteArrayList();
+
+    @Autowired
+    private NewsLetterSender sender;    
     
     @Autowired
-    NewsLetterSender sender;    
+    private MailFactory mailFactory;
+ 
+    @Override
+    public void dataFromDatabase(DataRetreived data) {
+        for(DataCommon tmpData : data.data) {
+            if(tmpData instanceof NewsLetterGroup) {
+                groups.add((NewsLetterGroup) tmpData);
+            }
+        }
+    }
     
     @Autowired
     public NewsLetterManager(Logger log, DatabaseSaver databaseSaver) {
         super(log, databaseSaver);
+    }
+    
+    @PostConstruct
+    public void addManager() {
+        sender.addManager(this);
     }
     
     @Override
@@ -38,8 +63,9 @@ public class NewsLetterManager extends ManagerBase implements INewsLetterManager
             group.users.put(userId, manager.getUserById(userId));
         }
         
+        group.currentStoreId = storeId;
         
-        sender.addGroup(group);
+        addGroup(group);
     }
 
     @Override
@@ -48,7 +74,58 @@ public class NewsLetterManager extends ManagerBase implements INewsLetterManager
         String userid = group.userIds.get(0);
         UserManager manager = getManager(UserManager.class);
         group.users.put(userid, manager.getUserById(userid));
-        sender.sendNewsLetterGroup(group);
+        sendNewsLetterGroup(group);
     }
 
+    public void run() {
+        List<NewsLetterGroup> toRemove = new ArrayList();
+        for(NewsLetterGroup group : groups) {
+            if(group.userIds.isEmpty()) {
+                toRemove.add(group);
+                continue;
+            }
+            
+            try {
+                sendNewsLetterGroup(group);
+            } catch (ErrorException ex) {
+                log.error(this, "Could not send newsletter", ex);
+            }
+        }
+        
+        for(NewsLetterGroup remove : toRemove) {
+            groups.remove(remove);
+            
+            try {
+                databaseSaver.deleteObject(remove, credentials);
+            } catch (ErrorException ex) {
+                log.error(this, "Could not save newsletter", ex);
+            }   
+        }
+    }
+    
+    private void sendNewsLetterGroup(NewsLetterGroup group) throws ErrorException {
+        String userId = group.userIds.get(0);
+        User user = group.users.get(userId);
+        String body = group.emailBody;
+        if(user != null) {
+            body = body.replaceAll("(?i)\\{Contact.name\\}", user.fullName);
+        }
+        group.userIds.remove(userId);
+        group.SentMailTo.add(userId);
+        sendEmail(userId, group.title, body, group.currentStoreId);
+        databaseSaver.saveObject(group, credentials);
+    }
+    
+    
+    private void sendEmail(String email, String title, String body, String storeId) {
+        mailFactory.setStoreId(storeId);
+        mailFactory.send("post@getshop.com", email, title, body);
+    }
+    
+
+    private void addGroup(NewsLetterGroup group) throws ErrorException {
+        group.storeId = storeId;
+        groups.add(group);
+        databaseSaver.saveObject(group, credentials);
+    }
 }
--- com.getshop.core/src/com/thundashop/core/messagemanager/NewsLetterSender.java
+++ com.getshop.core/src/com/thundashop/core/messagemanager/NewsLetterSender.java
@@ -5,13 +5,10 @@
 
 package com.thundashop.core.messagemanager;
 
-import com.thundashop.core.common.DataCommon;
 import com.thundashop.core.common.DatabaseSaver;
 import com.thundashop.core.common.ErrorException;
 import com.thundashop.core.common.Logger;
 import com.thundashop.core.common.ManagerBase;
-import com.thundashop.core.databasemanager.data.DataRetreived;
-import com.thundashop.core.usermanager.data.User;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,66 +17,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class NewsLetterSender extends ManagerBase {
-    public List<NewsLetterGroup> groups = new ArrayList();
-
-    @Autowired
-    MailFactory mailFactory;
+    public List<NewsLetterManager> managers = new ArrayList();
     
     @Autowired
     public NewsLetterSender(Logger log, DatabaseSaver databaseSaver) {
         super(log, databaseSaver);
     }
     
-    @Override
-    public void dataFromDatabase(DataRetreived data) {
-        for(DataCommon tmpData : data.data) {
-            if(tmpData instanceof NewsLetterGroup) {
-                groups.add((NewsLetterGroup) tmpData);
-            }
-        }
-    }
-
-    public void sendNewsLetterGroup(NewsLetterGroup group) throws ErrorException {
-        String userId = group.userIds.get(0);
-        User user = group.users.get(userId);
-        String body = group.emailBody;
-        if(user != null) {
-            body = body.replaceAll("(?i)\\{Contact.name\\}", user.fullName);
-        }
-        group.userIds.remove(userId);
-        group.SentMailTo.add(userId);
-        sendEmail(userId, group.title, body);
-        databaseSaver.saveObject(group, credentials);
-    }
-    
-        
     @Scheduled(fixedRate=300000)
     public void scheduledMailSending() throws ErrorException {
-        List<NewsLetterGroup> toRemove = new ArrayList();
-        for(NewsLetterGroup group : groups) {
-            if(group.userIds.isEmpty()) {
-                toRemove.add(group);
-                continue;
-            }
-            sendNewsLetterGroup(group);
-        }
-        for(NewsLetterGroup remove : toRemove) {
-            groups.remove(remove);
-            databaseSaver.deleteObject(remove, credentials);
+        for (NewsLetterManager manager : managers) {
+            manager.run();
         }
     }
-
     
-    public void sendEmail(String email, String title, String body) {
-        mailFactory.send("post@getshop.com", email, title, body);
+    public void addManager(NewsLetterManager manager) {
+        managers.add(manager);
     }
-    
-
-    void addGroup(NewsLetterGroup group) throws ErrorException {
-        groups.add(group);
-        group.storeId = "all";
-        databaseSaver.saveObject(group, credentials);
-    }
-
-    
 }

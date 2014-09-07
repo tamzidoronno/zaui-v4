0fb70d5844c49e2fef4ca379df890faba6c92426
boggi <boggibill@gmail.com>
2014-09-07 15:51:23 +0200

    temp commit.

---
+++ com.getshop.client/apps/HotelbookingCleaning/HotelbookingCleaning.php
@@ -0,0 +1,43 @@
+<?php
+namespace ns_020b57c2_8f80_46e1_a420_cb163f4fb2d2;
+
+class HotelbookingCleaning extends \ApplicationBase implements \Application {
+    var $entries;
+    var $dept;
+    var $currentMenuEntry;
+    
+    function __construct() {
+    }
+
+    public function getDescription() {
+        return "My application";
+    }
+
+    public function getAvailablePositions() {
+        return "left";
+    }
+    
+    public function getName() {
+        return "My application";
+    }
+
+    public function postProcess() {
+        
+    }
+
+    public function preProcess() {
+        
+    }
+    
+    public function getStarted() {
+    }
+
+    public function render() {
+        echo "<pre>";
+        $reservations = $this->getApi()->getHotelBookingManager()->getAllReservations();
+        print_r($reservations);
+        echo "</pre>";
+        $this->includefile("cleaning");
+    }
+}
+?>
--- com.getshop.messages/src/com/thundashop/core/hotelbookingmanager/BookingReference.java
+++ com.getshop.messages/src/com/thundashop/core/hotelbookingmanager/BookingReference.java
@@ -3,6 +3,7 @@ package com.thundashop.core.hotelbookingmanager;
 import com.thundashop.core.common.DataCommon;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 public class BookingReference extends DataCommon {
@@ -11,7 +12,16 @@ public class BookingReference extends DataCommon {
     public Date endDate;
     public List<Integer> codes = new ArrayList();
     public List<String> roomIds = new ArrayList();
+    public HashMap<String, Boolean> isApprovedForCheckIn = new HashMap();
     public ContactData contact = new ContactData();
     public Double bookingFee = 0.0;
     public boolean updateArx = true;
+    
+    public boolean isApprovedForCheckin(String roomId) {
+        if(isApprovedForCheckIn.containsKey(roomId)) {
+            return isApprovedForCheckIn.get(roomId);
+        }
+        return false;
+    }
+    
 }

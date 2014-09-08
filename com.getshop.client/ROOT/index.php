b5046653cd0cde929c797c3fc5c631b3c60d659c
boggi <boggibill@gmail.com>
2014-09-08 20:35:12 +0200

    lighter theme.

--- com.getshop.client/ROOT/index.php
+++ com.getshop.client/ROOT/index.php
@@ -104,7 +104,7 @@ if (@$factory->getApplicationPool()->getSelectedThemeApp()->applicationSettings-
 }
 
 if ($factory->isEditorMode()) {
-    echo '<link rel="stylesheet" type="text/css" href="skin/default/settings.css" />';
+    echo '<link rel="stylesheet" type="text/css" href="skin/default/settings2.css" />';
 }
 
 if (!isset($_SESSION['checkifloggedout']) || !$_SESSION['checkifloggedout']) {

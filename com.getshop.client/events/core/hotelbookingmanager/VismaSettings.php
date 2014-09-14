555cb547455399749d20500b903ad15497fc77f4
Pal Tonder <boggibill@gmail.com>
2014-09-14 10:57:47 +0200

    Visma integration.

---
+++ com.getshop.client/events/core/hotelbookingmanager/VismaSettings.php
@@ -0,0 +1,16 @@
+<?php
+class core_hotelbookingmanager_VismaSettings extends core_common_DataCommon  {
+	/** @var String */
+	public $address;
+
+	/** @var String */
+	public $username;
+
+	/** @var String */
+	public $password;
+
+	/** @var String */
+	public $port;
+
+}
+?>
---
+++ com.getshop.messages/src/com/thundashop/core/hotelbookingmanager/VismaSettings.java
@@ -0,0 +1,10 @@
+package com.thundashop.core.hotelbookingmanager;
+
+import com.thundashop.core.common.DataCommon;
+
+public class VismaSettings extends DataCommon {
+    public String address = "";
+    public String username = "";
+    public String password = "";
+    public Integer port = 21;
+}

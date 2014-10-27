347f836d1e81cfaaa329441fe4a592dea218c47e
Kai Tønder <ktonder@gmail.com>
2014-10-27 20:27:22 +0100

    Loading all javascript even if page is not editor mode.

--- com.getshop.client/ROOT/javascripts.php
+++ com.getshop.client/ROOT/javascripts.php
@@ -12,17 +12,17 @@ function endsWith($haystack, $needle)
 
 $factory = IocContainer::getFactorySingelton(false);
 $apps = $factory->getApplicationPool()->getAllApplicationSettings();
-if (!$factory->isEditorMode()) {
-    $apps = $factory->getApi()->getAppManager()->getApplicationSettingsUsedByWebPage();
-    $apps[] = $factory->getApplicationPool()->getApplicationByName("ns_bf35979f_6965_4fec_9cc4_c42afd3efdd7\MainMenu");
-    $apps[] = $factory->getApplicationPool()->getApplicationByName("ns_7093535d_f842_4746_9256_beff0860dbdf\BreadCrumb");
-    $apps[] = $factory->getApplicationPool()->getApplicationByName("ns_b741283d_920d_460b_8c08_fad5ef4294cb\ProductWidget");
-    
-    // Login app
-    $appSettings = new core_applicationmanager_ApplicationSettings();
-    $appSettings->id = "df435931-9364-4b6a-b4b2-951c90cc0d70";
-    $apps[] = $appSettings;
-}
+//if (!$factory->isEditorMode()) {
+//    $apps = $factory->getApi()->getAppManager()->getApplicationSettingsUsedByWebPage();
+//    $apps[] = $factory->getApplicationPool()->getApplicationByName("ns_bf35979f_6965_4fec_9cc4_c42afd3efdd7\MainMenu");
+//    $apps[] = $factory->getApplicationPool()->getApplicationByName("ns_7093535d_f842_4746_9256_beff0860dbdf\BreadCrumb");
+////    $apps[] = $factory->getApplicationPool()->getApplicationByName("ns_b741283d_920d_460b_8c08_fad5ef4294cb\ProductWidget");
+//    
+//    // Login app
+//    $appSettings = new core_applicationmanager_ApplicationSettings();
+//    $appSettings->id = "df435931-9364-4b6a-b4b2-951c90cc0d70";
+//    $apps[] = $appSettings;
+//}
 
 foreach ($apps as $app) {
     $appInstance = $factory->getApplicationPool()->createInstace($app);

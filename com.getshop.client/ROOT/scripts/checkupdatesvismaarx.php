dcc7848179ffabe651e14bcf499747ff2be7ef45
Pal Tonder <boggibill@gmail.com>
2014-10-29 17:46:39 +0100

    removed zeroes.

--- com.getshop.client/ROOT/scripts/checkupdatesvismaarx.php
+++ com.getshop.client/ROOT/scripts/checkupdatesvismaarx.php
@@ -2,8 +2,7 @@
 chdir("../");
 include '../loader.php';
 $factory = IocContainer::getFactorySingelton();
-
+$factory->getApi()->getHotelBookingManager()->checkForWelcomeMessagesToSend();
 $factory->getApi()->getHotelBookingManager()->checkForVismaTransfer();
 $factory->getApi()->getHotelBookingManager()->checkForArxTransfer();
-$factory->getApi()->getHotelBookingManager()->checkForWelcomeMessagesToSend();
 ?>

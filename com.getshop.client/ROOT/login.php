c2dd4279e7d1f1380067594b7bde9af29c0d2727
Kai Tønder <ktonder@gmail.com>
2014-11-09 13:23:43 +0100

    Seperated login

--- com.getshop.client/ROOT/login.php
+++ com.getshop.client/ROOT/login.php
@@ -1,10 +1,12 @@
 <?
 include '../loader.php';
 $factory = IocContainer::getFactorySingelton();
+$instance = new \ns_df435931_9364_4b6a_b4b2_951c90cc0d70\Login();
+$instance->preProcess();
 ?>
 
 <div class="inner <? echo $notloggedInClass; ?>">
-    <form method="POST" action="/index.php" name="loginform" class="loginform">
+    <form method="POST" action="/login.php" name="loginform" class="loginform">
             <div class="entry">Login</div>
             <div class="form">
                 <div class="username">Username / Email<br><input class="tstextfield" name="username" type="textfield"></input></div>

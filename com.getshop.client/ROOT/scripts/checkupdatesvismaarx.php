88d3e7d148158bb520ac8328b866570a2d0b0c3b
Pal Tonder <boggibill@gmail.com>
2014-11-20 19:10:32 +0100

    bugs found for semlagerhotell.

--- com.getshop.client/ROOT/scripts/checkupdatesvismaarx.php
+++ com.getshop.client/ROOT/scripts/checkupdatesvismaarx.php
@@ -3,7 +3,7 @@ chdir("../");
 include '../loader.php';
 $factory = IocContainer::getFactorySingelton();
 $factory->getApi()->getUserManager()->logOn("crontabjob@smh.no", "crontabjob12345");
-//$factory->getApi()->getHotelBookingManager()->checkForWelcomeMessagesToSend();
+$factory->getApi()->getHotelBookingManager()->checkForWelcomeMessagesToSend();
 $factory->getApi()->getHotelBookingManager()->checkForVismaTransfer();
-//$factory->getApi()->getHotelBookingManager()->checkForArxTransfer();
+$factory->getApi()->getHotelBookingManager()->checkForArxTransfer();
 ?>

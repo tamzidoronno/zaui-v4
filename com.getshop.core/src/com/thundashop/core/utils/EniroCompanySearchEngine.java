eed6327ec159f54761184545f51d79e2f3f0a7fa
Kai Tønder <ktonder@gmail.com>
2014-11-27 15:11:42 +0100

    Added url encoding to searchword.

--- com.getshop.core/src/com/thundashop/core/utils/EniroCompanySearchEngine.java
+++ com.getshop.core/src/com/thundashop/core/utils/EniroCompanySearchEngine.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -24,52 +25,51 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class EniroCompanySearchEngine implements CompanySearchEngine {
-	private Gson gson = new Gson();
-	
-	@Override
-	public Company getCompany(String organisationNumber) {
-		List<Company> companies = search(organisationNumber);
-		if (companies.size() == 1) {
-			return companies.iterator().next();
-		}
-		
-		return null;
-	}
+    private Gson gson = new Gson();
 
-	@Override
-	public List<Company> search(String search) {
-		List<Company> companies = new ArrayList();
-		
-		try {
-			String content = getContent("http://api.eniro.com/cs/search/basic?country=se&search_word="+search+"&key=1085211466286583430&profile=GetShop&version=1.1.3");
-			EniroSearchResult result = gson.fromJson(content, EniroSearchResult.class);
-			for (EniroSearchResult.Adverts advert : result.adverts) {
-				Company company = new Company();
-				company.vatNumber = advert.companyInfo.orgNumber;
-				
-				if (company.vatNumber == null || company.vatNumber.contains("#")) {
-					continue;
-				}
-				
-				company.name = advert.companyInfo.companyName;
-				company.city = advert.address.postArea;
-				company.country = "SE";
-				company.postnumber = advert.address.postCode;
-				company.streetAddress = advert.address.streetName;
-				companies.add(company);
-			}
-		} catch (IOException ex) {
-			Logger.getLogger(EniroCompanySearchEngine.class.getName()).log(Level.SEVERE, null, ex);
-		}
-		
-		return companies;
-	}
-	
-	private String getContent(String url) throws MalformedURLException, IOException {
+    @Override
+    public Company getCompany(String organisationNumber) {
+        List<Company> companies = search(organisationNumber);
+        if (companies.size() == 1) {
+            return companies.iterator().next();
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Company> search(String search) {
+        List<Company> companies = new ArrayList();
+
+        try {
+            String content = getContent("http://api.eniro.com/cs/search/basic?country=se&search_word=" + URLEncoder.encode(search, "UTF-8") + "%&key=1085211466286583430&profile=GetShop&version=1.1.3");
+            EniroSearchResult result = gson.fromJson(content, EniroSearchResult.class);
+            for (EniroSearchResult.Adverts advert : result.adverts) {
+                Company company = new Company();
+                company.vatNumber = advert.companyInfo.orgNumber;
+
+                if (company.vatNumber == null || company.vatNumber.contains("#")) {
+                    continue;
+                }
+
+                company.name = advert.companyInfo.companyName;
+                company.city = advert.address.postArea;
+                company.country = "SE";
+                company.postnumber = advert.address.postCode;
+                company.streetAddress = advert.address.streetName;
+                companies.add(company);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(EniroCompanySearchEngine.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return companies;
+    }
+
+    private String getContent(String url) throws MalformedURLException, IOException {
         URL oracle = new URL(url);
         String content;
-        try (BufferedReader in = new BufferedReader(
-             new InputStreamReader(oracle.openStream()))) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()))) {
             content = "";
             String readed = "";
             while ((readed = in.readLine()) != null) {
@@ -79,9 +79,9 @@ public class EniroCompanySearchEngine implements CompanySearchEngine {
         return content;
     }
 
-	@Override
-	public String getName() {
-		return "eniro";
-	}
-	
+    @Override
+    public String getName() {
+        return "eniro";
+    }
+
 }

57640dae35471a4b07e2e13b48481700764dd5da
Kai Tønder <ktonder@gmail.com>
2015-01-05 11:04:44 +0100

    Added check to avoid double registration of users.

--- com.getshop.client/apps/Booking/javascript/Booking.js
+++ com.getshop.client/apps/Booking/javascript/Booking.js
@@ -1,5 +1,5 @@
 app.Booking = {
-    loadSettings: function(element, application) {
+    loadSettings: function (element, application) {
         var config = {
             draggable: true,
             application: application,
@@ -8,7 +8,7 @@ app.Booking = {
                 {
                     icontype: "awesome",
                     icon: "fa-gears",
-                    iconsize : "30",
+                    iconsize: "30",
                     title: __f("Settings"),
                     click: app.Booking.showSettings
                 }
@@ -19,19 +19,18 @@ app.Booking = {
         toolbox.show();
         toolbox.attachToElement(application, 2);
     },
-    
-    showSettings: function(event, app) {
+    showSettings: function (event, app) {
         var event = thundashop.Ajax.createEvent(null, "showSettings", app, {});
         thundashop.common.showInformationBox(event, __f("Settings"));
     }
 };
 
 Booking = {
-    check: function(test) {
+    check: function (test) {
         if (!test || test == "")
             throw "empty";
     },
-    bindEvents: function() {
+    bindEvents: function () {
         $(document).on('keyup', '.Booking #birthday.updateOnBlur', this.updateCompanyInformation);
         $(document).on('click', '.Booking .donemarker', this.markAsDone);
         $(document).on('click', '.Booking .find_company_for_selection', this.startSearchCompany);
@@ -43,88 +42,86 @@ Booking = {
         $(document).on('click', '.Booking .company_selection', this.selectCompanyFromBrreg);
         $(document).on('click', '.Booking .toggleUserId', this.toggleInvoiced);
     },
-            
-    toggleInvoiced: function() {
+    toggleInvoiced: function () {
         var data = {
-            userId : $(this).attr('userId')
+            userId: $(this).attr('userId')
         }
-        
+
         var event = thundashop.Ajax.createEvent(null, "toggleInvoiced", this, data);
         thundashop.Ajax.post(event);
     },
-            
-    selectCompanyFromBrreg : function(event) {
-        if(event.type === "click") {
+    selectCompanyFromBrreg: function (event) {
+        if (event.type === "click") {
             var row = $(this);
         } else {
             var row = $(this).closest('tr');
         }
         var brregnumber = row.attr('orgnr');
         var name = row.find('.selected_name').html();
-        
+
         $(this).closest('.app').find('.search_company').val(name);
         $(this).closest('.app').find('#birthday').val(brregnumber);
         $(this).closest('.app').find('#birthday').keyup();
 //        $(this).closest('.app').find('#birthday').closest('tr').show();
         $('.Booking .search_result_area').hide();
     },
-    searchBrreg : function(event) {
+    searchBrreg: function (event) {
         var me = this;
-        
+
         $('.Booking .search_result_area').show();
-        $('.Booking .search_result_area').html('<i class="fa fa-refresh fa-spin"></i> '+ __f("Loading"));
-        
-        var search = function() {
+        $('.Booking .search_result_area').html('<i class="fa fa-refresh fa-spin"></i> ' + __f("Loading"));
+
+        var search = function () {
             $('.Booking .search_result_area').show();
             var value = $(me).val();
-            var event = thundashop.Ajax.createEvent('','findCompanies',$(me),{'name':value});
-            thundashop.Ajax.postWithCallBack(event, function(data)  {
+            var event = thundashop.Ajax.createEvent('', 'findCompanies', $(me), {'name': value});
+            thundashop.Ajax.postWithCallBack(event, function (data) {
                 $('.Booking .search_result_area').html(data);
             });
         };
-        
+
         if (app.Booking.delaySearchTimer) {
             clearTimeout(app.Booking.delaySearchTimer);
         }
-        
+
         app.Booking.delaySearchTimer = setTimeout(search, 500);
     },
-    startSearchCompany : function() {
+    startSearchCompany: function () {
         $('.Booking .selectcompanyform').slideDown();
     },
-    updateCourseLocation : function() {
+    updateCourseLocation: function () {
         var selection = $(this).closest('.app').find('#event');
         var location = $(this).val();
         var foundcourse = false;
-        selection.find('option').each(function() {
-           if($(this).attr('location') === location) {
-               if(!foundcourse) {
-                   foundcourse = $(this).attr('value');
-               }
-               $(this).show();
-           } else {
-               $(this).hide();
-           }
+        selection.find('option').each(function () {
+            if ($(this).attr('location') === location) {
+                if (!foundcourse) {
+                    foundcourse = $(this).attr('value');
+                }
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
         });
-        if(foundcourse) {
-            selection.find('[value="'+foundcourse+'"]').attr('selected','selected');
+        if (foundcourse) {
+            selection.find('[value="' + foundcourse + '"]').attr('selected', 'selected');
             selection.show();
         } else {
             selection.hide();
         }
-        
+
     },
-    markAsDone: function() {
+    markAsDone: function () {
         var data = {
-            userId : $(this).attr('userId')
+            userId: $(this).attr('userId')
         }
-        
+
         var event = thundashop.Ajax.createEvent(null, "markAsDone", this, data);
         thundashop.Ajax.post(event);
     },
-    updateCompanyInformation: function() {
+    updateCompanyInformation: function () {
         var value = $(this).val();
-		var orgLength = $(this).attr('orglength');
+        var orgLength = $(this).attr('orglength');
         if (value.length == orgLength) {
             var outer = $('<div/>');
             var loader = $('<img src="skin/default/images/ajaxloader.gif"/><br>');
@@ -140,12 +137,10 @@ Booking = {
             $(this).closest('table').find('.companyinformation').html(invalidText);
         }
     },
-            
-    gotCompanyInformation: function(response) {
+    gotCompanyInformation: function (response) {
         $('.Booking table .companyinformation').html(response);
     },
-            
-    validate: function(evt) {
+    validate: function (evt) {
         var theEvent = evt || window.event;
         var key = theEvent.keyCode || theEvent.which;
         key = String.fromCharCode(key);
@@ -158,7 +153,7 @@ Booking = {
     }
 }
 
-$('.Booking .groupselection .selectbox').live('click', function() {
+$('.Booking .groupselection .selectbox').live('click', function () {
     var groupid = $(this).attr('groupid');
     var data = {
         groupid: groupid
@@ -167,26 +162,26 @@ $('.Booking .groupselection .selectbox').live('click', function() {
     thundashop.Ajax.post(event);
 });
 
-$('.Booking .selected .changeit').live('click', function() {
+$('.Booking .selected .changeit').live('click', function () {
     var event = thundashop.Ajax.createEvent('Booking', 'unsetGroup', $(this), {});
     thundashop.Ajax.post(event);
 });
 
-$('.Booking .savebooking').live('click', function() {
+$('.Booking .savebooking').live('click', function () {
     var data = {}
     data.name = $('#name').val();
     data.email = $('#email').val();
     data.cellphone = $('#cellphone').val();
     data.birthday = $('#birthday').val();
-    
+
     if ($('#invoiceemail').length > 0) {
         data.invoiceemail = $('#invoiceemail').val();
     }
-	
-	if ($("#extradep").length > 0) {
-		data.extradep = $("#extradep").val();
-	}
-    
+
+    if ($("#extradep").length > 0) {
+        data.extradep = $("#extradep").val();
+    }
+
     if ($('#birthday').hasClass("updateOnBlur")) {
         data.company = $('.companyinformation').html();
     } else {
@@ -221,21 +216,24 @@ $('.Booking .savebooking').live('click', function() {
             Booking.check(data.company);
             if ($('#event').length > 0)
                 Booking.check(data.eventid);
-        } catch(error) {
+        } catch (error) {
             thundashop.common.Alert(__w('Stop'), __w('The company you have selected could not be found.'), true);
             return;
         }
     }
 
+    if ($(this).attr('started') == "true") {
+        return;
+    }
+    
+    $(this).attr('started', 'true');
+    
     var event = thundashop.Ajax.createEvent('Booking', 'runRegisterEvent', $(this), data);
-//    if ($('#event').length > 0) {
-        var result = thundashop.Ajax.postSynchronWithReprint(event);
-        if (result !== false)
-            thundashop.common.Alert(__w('Completed'), __w('Your are now signed up for the event'));
-//    } else {
-//        var result = thundashop.Ajax.post(event);
-//        thundashop.common.Alert(__w('Completed'), __w('Your you for your order'));
-//    }
+
+    var result = thundashop.Ajax.postSynchronWithReprint(event);
+    if (result !== false)
+        thundashop.common.Alert(__w('Completed'), __w('Your are now signed up for the event'));
+
 });
 
 Booking.bindEvents();

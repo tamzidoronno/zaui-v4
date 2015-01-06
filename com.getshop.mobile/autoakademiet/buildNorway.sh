e40f6399bd9b810e73fbd2b367bdf8a0e5c12b94
Kai Tønder <ktonder@gmail.com>
2015-01-06 11:20:15 +0100

    Few adjustments

--- com.getshop.mobile/autoakademiet/buildNorway.sh
+++ com.getshop.mobile/autoakademiet/buildNorway.sh
@@ -1,9 +1,9 @@
 echo "Please enter the versionnumber. followed by [ENTER]:"
 read version
-phonegap local build android;
 cd platforms/android/;
 cp AndroidManifest-NO.xml AndroidManifest.xml;
 sed -i "s/{APK_VERSION}/$version/g" AndroidManifest.xml
+phonegap local build android;
 ant release;
 cp -rf bin/HelloWorld-release.apk ../../apk/ProMeister\ Academy\ Norway-$version.apk
 cd ../../;

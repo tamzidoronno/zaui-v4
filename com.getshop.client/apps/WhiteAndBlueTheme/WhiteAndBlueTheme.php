94772ba934fef870774ccd32675e71aa72de1e12
Kai Tønder <ktonder@gmail.com>
2015-02-24 21:41:56 +0100

    A new theme in making.

--- com.getshop.client/apps/WhiteAndBlueTheme/WhiteAndBlueTheme.php
+++ com.getshop.client/apps/WhiteAndBlueTheme/WhiteAndBlueTheme.php
@@ -16,21 +16,11 @@ class WhiteAndBlueTheme extends \ThemeApplication {
     }
     
     function __construct() {
-        $this->setTotalWidth(900);
-        $this->setWidthLeft(190);
-        $this->setWidthMidle(490);
-        $this->setWidthRight(200);
-        $this->setHeaderHeight(135);
-        $this->setFooterHeight(30);
         
-        // LOGO
-        $this->setVariable('ns_974beda7_eb6e_4474_b991_5dbc9d24db8e', 'height', 100);
-        $this->setVariable('ns_974beda7_eb6e_4474_b991_5dbc9d24db8e', 'width', 900);
-        
-        $this->setCategoryImageWidth(140);
-        $this->setCategoryImageHeight(140);
-        $this->setCategoryColumnCount2Layout(4);
-        $this->setCategoryColumnCount3Layout(3);
+    }
+    
+    public function addScripts() {
+        echo "\n" . "<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>";
     }
 }
 

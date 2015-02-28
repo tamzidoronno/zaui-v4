675f056a22540f37d758ee56adfec626e1c126ce
Pal Tonder <boggibill@gmail.com>
2015-02-28 21:30:32 +0100

    doh.

--- com.getshop.client/ROOT/index.php
+++ com.getshop.client/ROOT/index.php
@@ -326,7 +326,7 @@ if (ns_df435931_9364_4b6a_b4b2_951c90cc0d70\Login::isAdministrator()) {
         CKEDITOR.config.allowedContent = true;
     }
 </script>
-<div id="gs_customcss">test
+<div id="gs_customcss">
     <style>
         <? echo $factory->getStoreConfiguration()->customCss; ?>
     </style>

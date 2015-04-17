72b89d61e0cc3db586fdf11b8614a4859e1813f6
Kai Tønder <ktonder@gmail.com>
2015-04-17 22:36:41 +0200

    Changed price model

--- com.getshop.core/src/com/thundashop/core/hotelbookingmanager/DatePricedLibrary.java
+++ com.getshop.core/src/com/thundashop/core/hotelbookingmanager/DatePricedLibrary.java
@@ -6,30 +6,48 @@
 package com.thundashop.core.hotelbookingmanager;
 
 import com.thundashop.core.cartmanager.data.CartItem;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
  *
  * @author ktonder
  */
 public class DatePricedLibrary {
-    private final Date startDate;
-    private final Date endDate;
+    private List<Date> dates = new ArrayList();
+    
     private double extra = 400;
 
     public DatePricedLibrary() {
         Calendar cal = Calendar.getInstance();
-        cal.set(2015, 3, 19, 0, 0, 0);
-        this.startDate = cal.getTime();
+        cal.set(2015, 3, 18);
+        dates.add(cal.getTime());
         
-        cal.set(2015, 3, 20, 23, 59, 59);
-        this.endDate = cal.getTime();
+        cal.set(2015, 3, 20);
+        dates.add(cal.getTime());
     }
     
     public double getPrice(CartItem cartItem, Date date) {
-        if (date.after(startDate) && date.before(endDate)) {
-            return cartItem.getProduct().getPrice(null) + extra;
+        Calendar cal = Calendar.getInstance();
+        
+        for (Date checkDate : dates) {
+            cal.setTime(checkDate);
+            cal.set(Calendar.HOUR, 0);
+            cal.set(Calendar.MINUTE, 0);
+            cal.set(Calendar.SECOND, 0);
+            Date startDate = cal.getTime();
+            
+            cal.setTime(checkDate);
+            cal.set(Calendar.HOUR, 23);
+            cal.set(Calendar.MINUTE, 59);
+            cal.set(Calendar.SECOND, 59);
+            Date endDate = cal.getTime();
+            
+            if (date.after(startDate) && date.before(endDate)) {
+                return cartItem.getProduct().getPrice(null) + extra;
+            }
         }
         
         return cartItem.getProduct().getPrice(null);

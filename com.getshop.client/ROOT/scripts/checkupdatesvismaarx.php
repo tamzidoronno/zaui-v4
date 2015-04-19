c2df339bbe24e80ac9b1f1ea85263c6f0a1de2c4
Kai Tønder <ktonder@gmail.com>
2015-04-19 19:18:22 +0200

    Added login stuff

--- com.getshop.client/ROOT/scripts/checkupdatesvismaarx.php
+++ com.getshop.client/ROOT/scripts/checkupdatesvismaarx.php
@@ -2,6 +2,7 @@
 chdir("../");
 include '../loader.php';
 $factory = IocContainer::getFactorySingelton();
+$user = $factory->getApi()->getUserManager()->logOn($_GET['user'], $_GET['password']);
 $factory->getApi()->getHotelBookingManager()->checkForOrdersToGenerate();
 $factory->getApi()->getHotelBookingManager()->checkForWelcomeMessagesToSend();
 echo "done";

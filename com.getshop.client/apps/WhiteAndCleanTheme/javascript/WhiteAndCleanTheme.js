e3addc7d289ef74bcfc51794c3a15a7085b28414
Kai Tønder <ktonder@gmail.com>
2015-06-22 18:52:54 +0200

    Disabled changing of menusize

--- com.getshop.client/apps/WhiteAndCleanTheme/javascript/WhiteAndCleanTheme.js
+++ com.getshop.client/apps/WhiteAndCleanTheme/javascript/WhiteAndCleanTheme.js
@@ -1,39 +1,39 @@
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 140
-        }, 500);
-        return false;
-      }
-    }
-  });
-});
-
-var isTop = true;
-$(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-    if (scroll !== 0) {
-        if (isTop) {
-            isTop = false;
-            $('.gsdepth_0.gscount_0.gsrow img').animate({
-                height: 80
-            });
-            $('.gsarea[area="header"]').animate({
-                height: 80
-            });
-        }
-    } else {
-        $('.gsdepth_0.gscount_0.gsrow img').animate({
-            height: 150
-        });
-        $('.gsarea[area="header"]').animate({
-            height: 110
-        }, 20);
-        isTop = true;
-        
-    }
-});
+//$(function() {
+//  $('a[href*=#]:not([href=#])').click(function() {
+//    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//      var target = $(this.hash);
+//      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//      if (target.length) {
+//        $('html,body').animate({
+//          scrollTop: target.offset().top - 140
+//        }, 500);
+//        return false;
+//      }
+//    }
+//  });
+//});
+//
+//var isTop = true;
+//$(window).scroll(function (event) {
+//    var scroll = $(window).scrollTop();
+//    if (scroll !== 0) {
+//        if (isTop) {
+//            isTop = false;
+//            $('.gsdepth_0.gscount_0.gsrow img').animate({
+//                height: 80
+//            });
+//            $('.gsarea[area="header"]').animate({
+//                height: 80
+//            });
+//        }
+//    } else {
+//        $('.gsdepth_0.gscount_0.gsrow img').animate({
+//            height: 150
+//        });
+//        $('.gsarea[area="header"]').animate({
+//            height: 110
+//        }, 20);
+//        isTop = true;
+//        
+//    }
+//});

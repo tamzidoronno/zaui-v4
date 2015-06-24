c9c66154787d03cb03aeaea8605bf6898864b085
Kai Tønder <ktonder@gmail.com>
2015-06-24 13:27:27 +0200

    Added proper anchor functionallity

--- com.getshop.client/apps/Menu/Menu.php
+++ com.getshop.client/apps/Menu/Menu.php
@@ -205,7 +205,7 @@ class Menu extends \SystemApplication implements \Application {
             }
             
             if ($entry->scrollPageId && $entry->scrollAnchor) {
-                echo "<div class='entry'><a href='#'><div scrollPageId='$entry->scrollPageId' scrollAnchor='$entry->scrollAnchor' class='gs_scrollitem'>$fontAwesome $name</div></a>";
+                echo "<div class='entry'><div scrollPageId='$entry->scrollPageId' scrollAnchor='$entry->scrollAnchor' class='gs_scrollitem'>$fontAwesome $name</div>";
                 if ($entry->subentries) {
                     $this->printEntries($entry->subentries, $level+1);
                 }

d9174a7feed8cb7e4e8f55a0d60284c8f22b5cb0
Pal Tonder <boggibill@gmail.com>
2015-07-23 19:28:10 +0200

    incorrect prefix.

--- com.getshop.core/src/com/thundashop/core/hotelbookingmanager/HotelBookingManager.java
+++ com.getshop.core/src/com/thundashop/core/hotelbookingmanager/HotelBookingManager.java
@@ -1204,7 +1204,7 @@ public class HotelBookingManager extends ManagerBase implements IHotelBookingMan
         }
                 
         if (storeId != null && storeId.equals("3292fa74-32a2-4d52-b88f-6be6f3dff813")) {
-            getMsgManager().smsFactory.send("Sem Lagerhotell", "+4746190000", "Ny bestilling opprettet, Navn: " + name + ", Rom: " + roomType);
+            getMsgManager().smsFactory.send("Sem Lagerhotell", "46190000", "Ny bestilling opprettet, Navn: " + name + ", Rom: " + roomType);
         }
     }
 }

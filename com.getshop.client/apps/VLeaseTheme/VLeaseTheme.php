5a1091ccde7b0654a8678ba07450c4116a77e749
Kai Tønder <ktonder@gmail.com>
2015-09-01 11:52:13 +0200

    Added VLease theme

---
+++ com.getshop.client/apps/VLeaseTheme/VLeaseTheme.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of VLeaseTheme
+ *
+ * @author ktonder
+ */
+
+namespace ns_a940cb75_40ef_4f4b_a691_40eb9490b3b0;
+
+class VLeaseTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        return "VLeaseTheme";
+    }
+
+    public function render() {
+        
+    }
+}

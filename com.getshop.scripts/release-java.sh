3d2d7a4d4e6e2d0ece2e201a107fa4c8568a401b
Kai Tønder <ktonder@gmail.com>
2015-09-07 15:48:45 +0200

    Added a ignore file

--- com.getshop.scripts/release-java.sh
+++ com.getshop.scripts/release-java.sh
@@ -1,23 +1,23 @@
 #transfer file
 cat << EOF > batchfile
 cd dist
-lcd ../
-put com.getshop.core/dist/com.thundashop.core.jar
+lcd $HOME/netbeans/1.0.0
+put "GetShop Core/dist/GetShop_Core.jar" com.thundashop.core.jar
 cd lib
-put com.getshop.messages/dist/com.thundashop.messages.jar
+put "GetShop Messages/dist/GetShop_Messages.jar" com.thundashop.messages.jar
 EOF
 
 cat << EOF > batchfile2
-lcd ../
+lcd $HOME/netbeans/1.0.0
 put apitodb.json
 EOF
 
 echo -e "Uploading thundashop jar files"
-sftp -b batchfile naxa@backend.getshop.com &> /dev/null
+sftp -b batchfile naxa@backend.getshop.com #&> /dev/null
 rm -rf batchfile
 
 echo -e "Uploading api file"
-sftp -b batchfile2 naxa@backend.getshop.com &> /dev/null
+sftp -b batchfile2 naxa@backend.getshop.com #&> /dev/null
 rm -rf batchfile2
 
 echo -e "Restarting java!";

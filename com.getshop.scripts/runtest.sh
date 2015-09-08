214f25178f48252395c148357c6de3c1cd68099d
Kai Tønder <ktonder@gmail.com>
2015-09-08 20:22:13 +0200

    cleaning up

--- com.getshop.scripts/runtest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd com.getshop.core
-#ant -f /test/thundashop/com.getshop.core/build.xml clean jar
-java -jar dist/com.thundashop.core.jar 20000 &
-cd ..
-cd com.getshop.client/integrationtest
-php runner.php alttest
--- com.getshop.scripts/slowdown.sh
+++
@@ -1,3 +0,0 @@
-sudo tc qdisc add dev lo root handle 1: htb default 12
-sudo tc class add dev lo parent 1:1 classid 1:12 htb rate 256kbps ceil 1802kbps
-sudo tc qdisc add dev lo parent 1:12 netem delay 14ms

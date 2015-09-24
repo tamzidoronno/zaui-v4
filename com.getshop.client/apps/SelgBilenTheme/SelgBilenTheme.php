87880d4e6de747166e7a33ea8e9e7042212a5a45
Pal Tonder <boggibill@gmail.com>
2015-09-24 07:28:54 +0200

    created selgbilenminapp.

---
+++ com.getshop.client/apps/SelgBilenTheme/SelgBilenTheme.php
@@ -0,0 +1,15 @@
+<?php
+namespace ns_68bfe8b2_655b_4099_87b7_964e078370f2;
+
+class SelgBilenTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        return "Selg bilen theme";
+    }
+
+    public function render() {
+        echo "TEST";
+    }
+
+}
+
+?>

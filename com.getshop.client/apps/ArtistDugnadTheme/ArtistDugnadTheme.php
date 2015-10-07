758ab72a524df3f0ff493d2bd41aeb7051a6c098
Pal Tonder <boggibill@gmail.com>
2015-10-07 21:57:53 +0200

    artistdugnad theme.

---
+++ com.getshop.client/apps/ArtistDugnadTheme/ArtistDugnadTheme.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_e5f96f95_f61d_4316_94a8_bfdb1a6d5a88;
+
+class ArtistDugnadTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "ArtistDugnadTheme";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

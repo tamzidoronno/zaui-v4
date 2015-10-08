6a6ee65863e852f40c4f30aa384ac816e5d160b8
Pal Tonder <boggibill@gmail.com>
2015-10-08 19:44:30 +0200

    generate api localpal.

--- core/src/main/java/com/thundashop/core/apigenerator/GenerateApiLocalPal.java
+++ core/src/main/java/com/thundashop/core/apigenerator/GenerateApiLocalPal.java
@@ -13,10 +13,7 @@ import java.io.IOException;
  */
 public class GenerateApiLocalPal {
     public static void main(String[] args) throws ClassNotFoundException, IOException, Exception {
-        String homefolder = "boggi";
-        GenerateApi ga = new GenerateApi("/home/"+homefolder+"/netbeans/3.0.0/GetShop Core/", "/home/ktonder/netbeans/3.0.0/GetShop Messages/", "/source/getshop/3.0.0/com.getshop.client/", "/source/getshop/3.0.0/core", false);
-        ga.generate();
-        ga = new GenerateApi("/home/"+homefolder+"/netbeans/3.0.0/GetShop Core/", "/home/ktonder/netbeans/3.0.0/GetShop Messages/", "/source/getshop/3.0.0/com.getshop.pullserver/", "/source/getshop/3.0.0/core", false);
+        GenerateApi ga = new GenerateApi("/home/boggi/netbeans/3.0.0/GetShop Core/", "/home/ktonder/netbeans/3.0.0/GetShop Messages/", "/source/getshop/3.0.0/com.getshop.pullserver/", "/source/getshop/3.0.0/core", false);
         ga.generate();
     }
 

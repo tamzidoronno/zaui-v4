8d7cdcddb175c603f8d403c49939b1962f88405b
Kai Tønder <ktonder@gmail.com>
2015-10-24 18:05:39 +0200

    Fixed a bug

--- com.getshop.client/ROOT/js/getshop.ScrollMangic.js
+++ com.getshop.client/ROOT/js/getshop.ScrollMangic.js
@@ -7,51 +7,47 @@
 
 getshopScrollMagic = {
     controllers : {},
-    
-    rowLoaded: function(cellId) {
+
+        rowLoaded: function(cellId) {
         var cell = $('.gscell[cellid="'+cellId+'"]');
         var cellSettings = cell.find('.gsCellSettings_attrs');
-        
+
         var trigger = cell.closest('.gsdepth_0.gsucell').find('.getshopScrollMagicTriggerRow');
         var triggerId = trigger.attr('id');
-        
-        if (getshopScrollMagic[cellId]) {
-            getshopScrollMagic[cellId].destroy();
-        }
 
         var sceneFadeIn = false;
-        
+
         if (cellSettings.attr('scrollFadeIn')) {
             cell.css('opacity', cellSettings.attr('scrollFadeInStartOpacity'));
-            
+
             cell.css('right', cellSettings.attr('slideLeft')+'px');
             cell.css('top', (cellSettings.attr('slideTop')*-1)+'px');
 
-            
+
             var scrollSettings = {
                 opacity: cellSettings.attr('scrollFadeInEndOpacity'),
                 translateX: cellSettings.attr('slideLeft'),
                 translateY: cellSettings.attr('slideTop')
             }
-            
+
             sceneFadeIn = new ScrollMagic.Scene({triggerElement: '#'+triggerId})
-                .setVelocity('.gscell[cellid="'+cellId+'"]', scrollSettings, 
+                .setVelocity('.gscell[cellid="'+cellId+'"]', scrollSettings,
                     {
                         duration: cellSettings.attr('scrollFadeInDuration')
                     });
         }
-        
-        
+
+
         if (sceneFadeIn) {
             var controller = new ScrollMagic.Controller();
-            
-            if (sceneFadeIn) 
+
+            if (sceneFadeIn)
                 sceneFadeIn.addTo(controller);
-            
-            getshopScrollMagic[cellId] = controller;
+
         }
-        
-        
+
+
 
     }
+
 }

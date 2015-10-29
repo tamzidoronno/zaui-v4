e76de7a6635b3ea74557c079371742daa8849eb0
Kai Tønder <ktonder@gmail.com>
2015-10-29 13:08:01 +0100

    Fixed realease script

--- com.getshop.scripts/release-java-tuningfiles.sh
+++ com.getshop.scripts/release-java-tuningfiles.sh
@@ -3,7 +3,7 @@ cat << EOF > batchfile
 cd dist
 lcd $HOME/netbeans/1.0.0
 put "GetShop Core/dist/GetShop_Core.jar" com.thundashop.core.jar
-cd libs
+cd lib
 put "GetShop Messages/dist/GetShop_Messages.jar" com.thundashop.messages.jar
 EOF
 

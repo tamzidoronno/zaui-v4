0fd1f6e3e79b5e23f70900aa8920a5cb9ddf9cd0
Pal Tonder <boggibill@gmail.com>
2015-11-12 19:36:44 +0100

    new arx app online.

--- com.getshop.client/ROOT/arx/info.php
+++
@@ -1,9 +0,0 @@
-<div class='backbutton'><i class="fa fa-arrow-left"></i> Back to main menu</div>
-<center>
-Produsert av GetShop AS<br>
-<br>
-http://www.getshop.no<br>
-<br>
-Tlf: 0047 483 11 484<br>
-</center>
-

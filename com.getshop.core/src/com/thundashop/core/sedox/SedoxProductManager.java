86543e2eace6eb73a6caa56b136bd0dd7f1e6990
Kai Tønder <ktonder@gmail.com>
2015-11-19 14:09:25 +0100

    Removed copy sending of emails

--- com.getshop.core/src/com/thundashop/core/sedox/SedoxProductManager.java
+++ com.getshop.core/src/com/thundashop/core/sedox/SedoxProductManager.java
@@ -395,7 +395,6 @@ public class SedoxProductManager extends ManagerBase implements ISedoxProductMan
 
         if (newProduct != null) {
             sendNotificationEmail("files@tuningfiles.com", newProduct, comment);
-            sendNotificationEmail("support@sedox.com", newProduct, comment);
         }
 
         newProduct.firstUploadedByUserId = getSession().currentUser.id;
@@ -727,7 +726,6 @@ public class SedoxProductManager extends ManagerBase implements ISedoxProductMan
         }
 
         sendNotificationEmail("files@tuningfiles.com", sedoxProduct);
-        sendNotificationEmail("support@sedox.com", sedoxProduct);
     }
 
     private void sendAirGramMessage(String emailAddress, SedoxProduct sedoxProduct) throws ErrorException {

9fbfb6d9179855001366ee849e9e6044b043e327
Pal Tonder <boggibill@gmail.com>
2015-12-08 21:10:26 +0100

    design for abbbetalings design.

---
+++ com.getshop.client/apps/AbbBetalingsTheme/AbbBetalingsTheme.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_69f5da4a_ffca_422a_b134_72d0345997c9;
+
+class AbbBetalingsTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "AbbBetalingsTheme";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

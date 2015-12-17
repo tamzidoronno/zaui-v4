64ced10cf6ada6275da0312ec409a75fdf3f82dc
Pal Tonder <boggibill@gmail.com>
2015-12-17 16:04:34 +0100

    images.

---
+++ com.getshop.client/apps/bjaroytheme/bjaroytheme.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_2dc6c222_527d_4297_aa8c_dc9830dc3404;
+
+class bjaroytheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "bjaroytheme";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

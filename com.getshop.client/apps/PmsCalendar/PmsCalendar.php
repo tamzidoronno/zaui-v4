daa0c7c7b1efd9b63cab81b029c149786eed3471
Pal Tonder <boggibill@gmail.com>
2015-12-20 10:31:23 +0100

    added pms calendar app.

---
+++ com.getshop.client/apps/PmsCalendar/PmsCalendar.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_2059b00f_8bcb_466d_89df_3de79acdf3a1;
+
+class PmsCalendar extends \WebshopApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "PmsCalendar";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

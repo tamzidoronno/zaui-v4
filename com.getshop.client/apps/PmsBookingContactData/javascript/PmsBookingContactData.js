b607eefc082a71254ec138f78a154ed63c9440ea
boggi <boggi@boggi-G501JW>
2016-01-14 20:20:24 +0100

    added autofilling from brreg and posten.

--- com.getshop.client/apps/PmsBookingContactData/javascript/PmsBookingContactData.js
+++ com.getshop.client/apps/PmsBookingContactData/javascript/PmsBookingContactData.js
@@ -1,6 +1,47 @@
 app.PmsBookingContactData = {
     init : function() {
         $(document).on('click', '.PmsBookingContactData .chooseregform input', app.PmsBookingContactData.choseRegType);
+        $(document).on('keyup', '.PmsBookingContactData input[gsname="user_address_postCode"]', app.PmsBookingContactData.updatePostalPlace);
+        $(document).on('keyup', '.PmsBookingContactData input[gsname="company_vatNumber"]', app.PmsBookingContactData.updateBrreg);
+    },
+    updatePostalPlace: function () {
+        var val = $(this).val();
+        if(val.length !== 4) {
+            return;
+        }
+        $.ajax({
+            "dataType" : "jsonp",
+            "url": "https://api.bring.com/shippingguide/api/postalCode.json?clientUrl=insertYourClientUrlHere&country=no&pnr=" + val,
+            "success": function (data) {
+                if(data.valid) {
+                    $('input[gsname="user_address_city"]').val(data.result);
+                }
+            }
+        }
+        );
+    },
+    updateBrreg : function() {
+        var val = $(this).val();
+        if(val.indexOf(".") >= 0) {
+            return;
+        } 
+        if(val.length === 9) {
+            $.ajax({
+                "url" : "http://hotell.difi.no/api/json/brreg/enhetsregisteret?query=" + val,
+                "success" : function(data) {
+                    var data = data.entries[0];
+                    $('input[gsname="company_vatNumber"]').val(data.orgnr);
+                    $('input[gsname="company_address_address"]').val(data.forretningsadr);
+                    $('input[gsname="company_address_postCode"]').val(data.forradrpostnr);
+                    $('input[gsname="company_address_city"]').val(data.forradrpoststed);
+                    $('input[gsname="company_name"]').val(data.navn);
+                }
+            });
+        }
+    },    
+    
+    searchPostalCode : function() {
+       alert('test'); 
     },
     choseRegType : function() {
         var type = $(this).val();

d7b93e2a69af61850d78beb0cb069eeb9634f857
Kai Tønder <ktonder@gmail.com>
2016-02-25 19:44:57 +0100

    Skytech Theme

---
+++ com.getshop.client/apps/SkyTecTheme/SkyTecTheme.php
@@ -0,0 +1,21 @@
+<?php
+namespace ns_566fb905_a4a7_43bf_ad09_b9b6f15b8ead;
+
+class SkyTecTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "SkyTecTheme";
+    }
+
+    public function render() {
+        
+    }
+    
+    public function addScripts() {
+        return "\n"."<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>";
+    }
+}
+?>

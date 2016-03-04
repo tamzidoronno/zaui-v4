d5e001c41a6bfd412b7db7fb56bd54d33fd6e934
Kai Tønder <ktonder@gmail.com>
2016-03-04 11:07:39 +0100

    New merging round

--- com.getshop.client/ROOT/scripts/mergeUsers.php
+++ com.getshop.client/ROOT/scripts/mergeUsers.php
@@ -2,6 +2,8 @@
 chdir("../");
 include '../loader.php';
 $factory = IocContainer::getFactorySingelton();
-$factory->getApi()->getUserManager()->mergeUsers("52176229-8c6a-4626-bb7c-68b622999598", ['9ee1cbc7-0ccb-4ea4-a872-1cdb71855e18']);
-$factory->getApi()->getUserManager()->mergeUsers("96063867-ee76-417d-aec6-ae592556b67b", ['428d315f-e5f1-4eac-800a-49b2ab2bcafe']);
+$factory->getApi()->getUserManager()->mergeUsers("d3ace5a5-addd-440f-91b7-32d7f6a72327", ['c91dc937-2a7e-4826-9f3c-6269745412bd']);
+$factory->getApi()->getUserManager()->mergeUsers("f3f51113-50e4-4242-9269-96b4a614888b", ['a31cfe6d-399e-4985-82f8-ccea5297a59c']);
+$factory->getApi()->getUserManager()->mergeUsers("a2d2a59a-dfaf-403a-b4ca-aaf0d073bb69", ['74906ecd-ce23-47e7-aac1-69f4333ff415']);
+$factory->getApi()->getUserManager()->mergeUsers("093a2f3b-b571-46c1-9cc7-8ebeefd0623d", ['7fc14571-0a8b-41fd-b2ac-3fe1979f55ea']);
 ?>

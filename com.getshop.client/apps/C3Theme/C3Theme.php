09c1b6efcee14a93f2609d1f2d2a6c4ac0c6d896
boggi <boggi@boggi-ThinkPad-W550s>
2016-03-09 15:58:18 +0100

    herregud så jeg roter.

---
+++ com.getshop.client/apps/C3Theme/C3Theme.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_4391b706_73bd_47c9_9da8_6db413ed0f45;
+
+class C3Theme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "C3Theme";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

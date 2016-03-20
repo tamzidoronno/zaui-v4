21463406f4735ba79199a362da52ff832c4a43e3
boggi <boggi@boggi-ThinkPad-W550s>
2016-03-20 20:16:16 +0100

    debug data to start with.

--- core/src/main/java/com/thundashop/core/pmsmanager/ArxLogFetcher.java
+++ core/src/main/java/com/thundashop/core/pmsmanager/ArxLogFetcher.java
@@ -25,12 +25,19 @@ public class ArxLogFetcher extends GetShopSchedulerBase {
         ArxConnection connection = new ArxConnection();
         
         long start = System.currentTimeMillis();
+        int i = 0;
         while(true) {
             long next = System.currentTimeMillis();
             String hostName = "https://" + config.arxHostname + ":5002/arx/eventexport?start_date="+start+"&end_date="+next;
             String result = "";
             try {
+                long now = System.currentTimeMillis();
                 result = connection.httpLoginRequest(hostName, config.arxUsername, config.arxPassword, "");
+                long diff = System.currentTimeMillis();
+                diff = diff - now;
+                if(i < 3) {
+                    System.out.println("Time takes to fetch from arx server 12: " + diff + "(" + hostName + "), result length: " + result.length());
+                }
                 start = next;
                 HashMap<String, List<AccessLog>> res = getApi().getArxManager().generateDoorLogForAllDoorsFromResult(result);
                 for(String doorId : res.keySet()) {
@@ -38,6 +45,7 @@ public class ArxLogFetcher extends GetShopSchedulerBase {
                         getApi().getPmsManager().handleDoorControl(getMultiLevelName(), doorId, res.get(doorId));
                     }
                 }
+                i++;
             }catch(Exception e) {
                 e.printStackTrace();
             }

d8ad8ac6085060b831f24dfbf2194acb58e633cd
boggi <boggi@boggi-ThinkPad-W550s>
2016-03-23 13:08:09 +0100

    design elements.

--- com.getshop.client/apps/bjaroytheme/bjaroytheme.php
+++ com.getshop.client/apps/bjaroytheme/bjaroytheme.php
@@ -11,6 +11,7 @@ class bjaroytheme extends \ThemeApplication implements \Application {
         echo "\n" . "<link href='https://fonts.googleapis.com/css?family=Quicksand:400,300,100' rel='stylesheet' type='text/css'>";
         echo "\n" . "<link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>";
         echo "\n" . "<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>";
+        echo "\n" . "<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>";
     }   
     
     public function getName() {

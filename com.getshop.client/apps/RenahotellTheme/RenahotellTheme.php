c01c77778c47f3272bbbd36300fac306b10ca7fb
boggi <boggi@boggi-ThinkPad-W550s>
2016-04-11 14:02:24 +0200

    started on rena hotell.

---
+++ com.getshop.client/apps/RenahotellTheme/RenahotellTheme.php
@@ -0,0 +1,21 @@
+<?php
+namespace ns_7b4f9efd_f5a2_433f_8c48_10d297c974e7;
+
+class RenahotellTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "RenahotellTheme";
+    }
+
+    public function render() {
+        
+    }
+    
+        public function getThemeClasses() {
+        return ["frontpagebox"];
+    }
+}
+?>

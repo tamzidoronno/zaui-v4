4e514714ff522026421b25d51d2df4578416e570
boggi <boggi@boggi-ThinkPad-W550s>
2016-04-20 21:02:05 +0200

    initial design for rena treningsenter.

---
+++ com.getshop.client/apps/RenaTreningsTheme/RenaTreningsTheme.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_969e6b86_3cdb_4653_a5b2_8f1aa3cbddac;
+
+class RenaTreningsTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "RenaTreningsTheme";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

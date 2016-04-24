f226b910c9b21b44153c46293ce0e3fb6dbc35ed
Kai Tønder <ktonder@gmail.com>
2016-04-24 19:29:21 +0200

    Reverted caching

--- com.getshop.client/apps/Company/javascript/Company.js
+++ com.getshop.client/apps/Company/javascript/Company.js
@@ -11,7 +11,7 @@ app.Company = {
     },
     
     showSelectedCompany: function() {
-        var companyId = $(this).closest('.gss_companyrow').attr('gs_model_attr');
+        var companyId = $(this).closest('.gss_companyrow').find('i').attr('gss_value_2')
         
         if (!companyId) {
             alert("No company selected");

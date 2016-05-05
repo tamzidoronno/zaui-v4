405367dc3d4fc837cadac36c7fc4a7d26b077e7f
Kai Tønder <ktonder@gmail.com>
2016-05-05 11:09:53 +0200

    Tested windows app for Sedox Perforamnce

--- com.getshop.client/ROOT/external/filehistory.php
+++ com.getshop.client/ROOT/external/filehistory.php
@@ -1,15 +1,16 @@
 <?php
-function getTuningfile($product, $orgFileName=false) {
+
+function getTuningfile($product, $orgFileName = false) {
     foreach ($product->binaryFiles as $binFile) {
         if ($orgFileName && $binFile->fileType == "Original") {
             return $binFile->orgFilename;
         }
-        
+
         if ($binFile->fileType != "Original") {
             return $binFile->id;
         }
     }
-    
+
     return 0;
 }
 
@@ -17,32 +18,39 @@ chdir("../");
 include '../loader.php';
 $factory = IocContainer::getFactorySingelton();
 
-// TODO : FIX THIS - SORTING AND PAGING ADDED TO API CALL.
-// ALSO REMEMBER TO TAKE THE LATEST VERSION.
-$orders = $factory->getApi()->getSedoxProductManager()->getProductsFirstUploadedByCurrentUser();
-$orders = array_splice($orders , 0, 10);
+
+
+$filterdata = new \core_sedox_FilterData();
+$filterdata->ascending = false;
+$filterdata->filterText = "";
+$filterdata->sortBy = "sedoxproduct_sedoxid";
+$filterdata->pageNumber = 1;
+$filterdata->pageSize = 10;
+
+$orders = $factory->getApi()->getSedoxProductManager()->getProductsFirstUploadedByCurrentUser($filterdata);
+$orders = array_splice($orders, 0, 10);
 
 $files = array();
 
 foreach ($orders as $product) {
     /* @var $product SedoxProduct */
-	$data = array();
-    
-	$data['Car'] = $product->brand;
-	$data['Model'] = $product->model;
-	$data['EngineSize'] = $product->engineSize;
-	$data['HorsePower'] = $product->power;
-	$data['Year'] = $product->year;
+    $data = array();
+
+    $data['Car'] = $product->brand;
+    $data['Model'] = $product->model;
+    $data['EngineSize'] = $product->engineSize;
+    $data['HorsePower'] = $product->power;
+    $data['Year'] = $product->year;
     $data['originalFileName'] = getTuningfile($product, true);
-	$data['tuningfileid'] = getTuningfile($product);
-	$data['ProductId'] = (int)$product->id;
-	$data['dateCreated'] = $product->rowCreatedDate;
+    $data['tuningfileid'] = getTuningfile($product);
+    $data['ProductId'] = (int) $product->id;
+    $data['dateCreated'] = $product->rowCreatedDate;
     $data['comment'] = $product->comment;
     $data['tool'] = $product->tool;
     $data['gear'] = $product->gearType;
     $data['started'] = $product->started == true;
     $data['originalChecksum'] = $product->originalChecksum;
-	$files[] = $data;
+    $files[] = $data;
 }
 
 echo json_encode($files);

cda3a5c45643e013656492582aa6ed69dc9b5e9f
hung <hung@hungary>
2016-05-17 16:53:09 +0200

    Changed Sedox hierarchy - income and passive setting for slaves

--- com.getshop.client/apps/SedoxMasterSlave/SedoxMasterSlave.php
+++ com.getshop.client/apps/SedoxMasterSlave/SedoxMasterSlave.php
@@ -24,6 +24,11 @@ class SedoxMasterSlave extends \MarketingApplication implements \Application {
     
     public function saveInformation() {
         $this->getApi()->getSedoxProductManager()->addCommentToUser($_POST['data']['userid'], $_POST['data']['comment']);
+        
+        foreach($_POST['data']['slavedatas'] as $slaveData) {
+            $this->getApi()->getSedoxProductManager()->addCreditToSlave($slaveData['slaveid'], filter_var($slaveData['income'], FILTER_VALIDATE_FLOAT));
+            $this->getApi()->getSedoxProductManager()->togglePassiveSlaveMode($slaveData['slaveid'], filter_var($slaveData['passiveslave'], FILTER_VALIDATE_BOOLEAN));  
+        }
     }
     
     public function removeSlaveFromMaster() {
@@ -34,11 +39,17 @@ class SedoxMasterSlave extends \MarketingApplication implements \Application {
         $this->getApi()->getSedoxProductManager()->addSlaveToUser($_POST['data']['masterid'], $_POST['data']['slaveid']);
         
         $slaveUser = $this->getApi()->getUserManager()->getUserById($_POST['data']['slaveid']);
+        $slave = $this->getApi()->getSedoxProductManager()->getSedoxUserAccountById($_POST['data']['slaveid']);
         
         echo "<div class='slave_information'>";
         echo "<div class='slave_name float-left' master_id='" . $_POST['data']['masterid'] . "' slave_id='" . $slaveUser->id . "'>" . $slaveUser->fullName . "</div>";
         echo "<div class='float-right'>";
-        echo " <div class='slave_options'>TestData</div>";
+        echo "<div class='slave_options checkbox' slave_id='" .  $slaveUser->id . "'>";
+        echo "<input class='income_input' value='" .  $slave->slaveIncome . "'>";
+        echo " Passive: "; 
+        echo "<input class='passsive_slave_input' id='passiveslavebox_" . $slaveUser->id . "' type='checkbox' " . ($slave->isPassiveSlave ? "checked" : "") . ">";
+        echo "<label for='passiveslavebox_" . $slaveUser->id ."'></label>";
+        echo "</div>";
         echo " <div class='delete_slave'><i class='fa fa-times' slave_id='" . $slaveUser->id . "'></i></div>";
         echo "</div>";
         echo "</div>";

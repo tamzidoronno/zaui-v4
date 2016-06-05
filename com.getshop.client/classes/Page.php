160b477fece7cb9edeac1ca5b098e27ceea1b5c1
Kai Tønder <ktonder@gmail.com>
2016-06-05 11:28:47 +0200

    Readded missing code that has accidently been removed due to a merge

--- com.getshop.client/classes/Page.php
+++ com.getshop.client/classes/Page.php
@@ -606,7 +606,7 @@ class Page {
         $permobject->{'link'} = $cell->link;
         $permobject->{'hideOnMobile'} = $cell->hideOnMobile;
         $permobject->{'hideOnDesktop'} = $cell->hideOnDesktop;
-        $permissions = "data-settings='".json_encode($cell->settings) . "'";
+        $permissions = "data-settings='".json_encode($cell->settings) . "' data-groupaccess='".  json_encode($cell->groupAccess->access)."'";
       
         $anchor = $cell->anchor;
         
@@ -747,6 +747,16 @@ class Page {
                 <span class='tabbtn' target='background'><? echo $this->factory->__w("Styling"); ?></span>
                 <span class='tabbtn' target='cellsettings'><? echo $this->factory->__w("Settings"); ?></span>
                 <span class='tabbtn' target='effects'><? echo $this->factory->__w("Effects"); ?></span>
+                <?
+                if (count($this->factory->getApi()->getUserManager()->getAllGroups())) {
+                ?>
+                    <span class='tabbtn' target='groupaccess'><? echo $this->factory->__w("Group Access"); ?></span>
+                <?
+                }
+                ?>
+            </div>
+            <div class='gspage' target='groupaccess' style='padding: 10px;'>
+                <? include("layoutbuilder/groupaccess.php"); ?>
             </div>
             <div class='gspage' target='effects' style='padding: 10px;'>
                 <? include("layoutbuilder/effects.php"); ?>

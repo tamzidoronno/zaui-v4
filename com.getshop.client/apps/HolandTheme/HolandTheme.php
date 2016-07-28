7c2f1bdd2870ec3a9b95da52411053d5840a41ac
boggi <boggi@boggi-ThinkPad-W550s>
2016-07-28 13:55:19 +0200

    imporoved styling for gardsmotellet.

--- com.getshop.client/apps/HolandTheme/HolandTheme.php
+++ com.getshop.client/apps/HolandTheme/HolandTheme.php
@@ -12,9 +12,9 @@ class HolandTheme extends \ThemeApplication implements \Application {
     
     public function addScripts() {
         $text = "";
-        $text .= "\n"."<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>";
-        $text .= "\n"."<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>";
-        return $text;
+        $text .= "\n"."<link href='https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400,600,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>";
+        $text .= "\n"."<link href='https://fonts.googleapis.com/css?family=Raleway:200,300,400,400,600,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>";
+        echo $text;
     }
     
     public function render() {

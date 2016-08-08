8577f97cc9e678b491afd37f77b8a318b0d7188e
boggi <boggi@boggi-ThinkPad-W550s>
2016-08-08 22:13:25 +0200

    improvements to booking view.

--- core/src/main/java/com/thundashop/core/pmsmanager/PmsManager.java
+++ core/src/main/java/com/thundashop/core/pmsmanager/PmsManager.java
@@ -266,13 +266,6 @@ public class PmsManager extends GetShopSessionBeanNamed implements IPmsManager {
             result = startBooking();
         }
 
-        if (result.sessionEndDate != null && result.sessionStartDate != null && result.sessionStartDate.after(result.sessionEndDate) && !configuration.hasNoEndDate) {
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(result.sessionStartDate);
-            cal.add(Calendar.DAY_OF_YEAR, 1);
-            result.sessionEndDate = cal.getTime();
-        }
-
         return result;
     }
 

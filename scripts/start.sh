4e511b952cf2b26be1dc24e85c2aa0c48fca292d
boggi <boggi@boggi-ThinkPad-W550s>
2016-08-09 14:00:31 +0200

    Fix for dibs

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kill -9 `ps aux |grep thunda |grep -v "auto" |awk '{print $2}'`
 java -Xmx4096m -XX:MaxPermSize=4096m -cp *:libs/* com.thundashop.core.databasemanager.AddApplicationsToDatabase;
-java -Xmx4096m -XX:MaxPermSize=4096m -cp *:libs/* com.thundashop.core.start.Runner &> log.txt&
+java -Djsse.enableSNIExtension=false -Xmx4096m -XX:MaxPermSize=4096m -cp *:libs/* com.thundashop.core.start.Runner &> log.txt&

ea8f1b787db3474007777037f31e02720d756c89
boggi <boggi@boggi-ThinkPad-W550s>
2016-08-10 12:16:04 +0200

    wrong total calculation.

--- com.getshop.client/apps/Dibs/Dibs.php
+++ com.getshop.client/apps/Dibs/Dibs.php
@@ -67,11 +67,9 @@ class Dibs extends \PaymentApplication implements \Application {
         
         $merchid = $this->getConfigurationSetting("merchantid");
         $currency = \ns_9de54ce1_f7a0_4729_b128_b062dc70dcce\ECommerceSettings::fetchCurrencyCode();
-        $orderId = $this->getOrder()->incrementOrderId;
-        $amount = $this->getApi()->getCartManager()->calculateTotalCost($this->order->cart)*100;
-        if(isset($this->order->shipping) && isset($this->order->shipping->cost)) {
-            $amount += ($this->order->shipping && $this->order->shipping->cost) ? $this->order->shipping->cost : 0;
-        }
+        $order = $this->getOrder();
+        $orderId = $order->incrementOrderId;
+        $amount = $this->getApi()->getOrderManager()->getTotalAmount($order)*100;
         
         $settings = $this->getFactory()->getSettings();
         $language = $this->getFactory()->getSelectedTranslation();
@@ -102,6 +100,7 @@ class Dibs extends \PaymentApplication implements \Application {
         if($this->isTestMode()) {
                 echo '<input type="hidden" name="test" value="1"/>';
                 echo "This is in test mode...";
+                return;
             }
         echo '</form>';
 

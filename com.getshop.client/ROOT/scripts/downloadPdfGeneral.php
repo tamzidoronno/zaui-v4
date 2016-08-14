eb08b7a6b2b1690d4daad3a2629fa19d4cc44275
Kai Tønder <ktonder@gmail.com>
2016-08-14 09:14:58 +0200

    Fixed PDF download button

---
+++ com.getshop.client/ROOT/scripts/downloadPdfGeneral.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/pdf');
+header('Content-Disposition: inline; filename="bestilling.pdf"');
+
+chdir("../");
+include '../loader.php';
+$factory = IocContainer::getFactorySingelton();
+
+header('Content-Type: application/pdf');
+$id = session_id();
+session_write_close();
+
+$webaddress = $factory->getStore()->webAddressPrimary;
+$webaddress = "promeisterse30.3.0.local.getshop.com";
+$url = "http://$webaddress/renderSingelApplication.php?&id=".$_GET['id'];
+
+$base64 = $factory->getApi()->getGetShop()->getBase64EncodedPDFWebPage($url);
+echo base64_decode($base64);
+?>

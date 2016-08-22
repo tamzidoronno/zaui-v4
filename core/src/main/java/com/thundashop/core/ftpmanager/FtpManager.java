108000ad44004d9fa056c627b01387a1b6916c4f
boggi <boggi@boggi-ThinkPad-W550s>
2016-08-22 15:55:20 +0200

    do not transfer files in dev mode to produciton env.

--- core/src/main/java/com/thundashop/core/ftpmanager/FtpManager.java
+++ core/src/main/java/com/thundashop/core/ftpmanager/FtpManager.java
@@ -1,6 +1,7 @@
 package com.thundashop.core.ftpmanager;
 
 import com.getshop.scope.GetShopSession;
+import com.thundashop.core.common.FrameworkConfig;
 import com.thundashop.core.common.ManagerBase;
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,12 +11,21 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @GetShopSession
 public class FtpManager extends ManagerBase implements IFtpManager {
+    @Autowired
+    FrameworkConfig frameworkConfig;
+    
     public boolean transferFile(String username, String password, String hostname, String filePath, String location, Integer port, boolean useActiveMode, int minutesToWait) throws IOException {
+        if(!frameworkConfig.productionMode) {
+            logPrint("Transferring file: " + filePath + " to " + location + ":" + port + " user: " + username);
+            return true;
+        }
+        
         /**
          * CAREFUL HERE... By exposing this public, you give access to upload whatever file to whoever, DO NOT MAKE THIS AS A PUBLIC API CALL.....
          * THIS IS A MAJOR SECURITY RISK! FILEPATH IS NOT SECURE!!!!!!!

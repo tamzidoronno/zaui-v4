b5996f57f0b664941bdc9b747a6e8f71758a64f1
Kai Tønder <ktonder@gmail.com>
2016-08-25 17:39:16 +0200

    Added logo for promeister

--- com.getshop.client/ROOT/renderSingelApplication.php
+++ com.getshop.client/ROOT/renderSingelApplication.php
@@ -2,7 +2,19 @@
 include '../loader.php';
 
 $factory = IocContainer::getFactorySingelton();
+
+echo "<div style='padding: 20px;'>";
+
+// ProMeister Logo :D
+if ($factory->getStore()->id == "6524eb45-fa17-4e8c-95a5-7387d602a69b") {
+    echo "<img src='http://promeisterse30.getshop.com/displayImage.php?id=b67ff4c6-8759-44a8-a1ce-68276013bce0&width=247'/>";
+    echo "<br/>";
+    echo "<br/>";
+    echo "<br/>";
+}
+
 $appSettings = $factory->getApi()->getStoreApplicationInstancePool()->getApplicationInstance($_GET['id']);
 $instance = $factory->getApplicationPool()->createAppInstance($appSettings);
 $instance->renderApplication();
+echo "</div>";
 ?>

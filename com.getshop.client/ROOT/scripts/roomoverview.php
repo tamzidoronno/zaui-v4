c96775200d87c89b7b494506c7e983797d29be7e
boggi <boggi@boggi-ThinkPad-W550s>
2016-08-28 21:06:25 +0200

    fix for wubook and app login.

--- com.getshop.client/ROOT/scripts/roomoverview.php
+++ com.getshop.client/ROOT/scripts/roomoverview.php
@@ -2,5 +2,5 @@
 $user = $_GET['username'];
 $pass = $_GET['password'];
 $address = "www.wh.no";
-header("Location: http://getshop.express?username=$user&password=$pass&address=$address");
+header("Location: http://getshop.express#/login?username=$user&password=$pass&address=$address");
 ?>

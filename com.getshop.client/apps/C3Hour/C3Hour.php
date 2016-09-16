f4606cebd6975a8f1cc1c8bc3d80a376248b40ff
Kai Tønder <ktonder@gmail.com>
2016-09-16 09:19:33 +0200

    Started on Hour registering

---
+++ com.getshop.client/apps/C3Hour/C3Hour.php
@@ -0,0 +1,32 @@
+<?php
+namespace ns_4a4d17cc_7691_4851_998a_10fae57efcee;
+
+class C3Hour extends \MarketingApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "C3Hour";
+    }
+
+    public function render() {
+        $this->includefile("edithour");
+    }
+    
+    public function saveHours() {
+        if ($this->getModalVariable("hourid")) {
+            $c3Hour = $this->getApi()->getC3Manager()->getHourById($this->getModalVariable("hourid"));
+        } else {
+            $c3Hour = new \core_c3_C3Hour();
+        }
+        
+        $c3Hour->from = $this->convertToJavaDate(strtotime($_POST['data']['from']));
+        $c3Hour->to = $this->convertToJavaDate(strtotime($_POST['data']['to']));
+        $c3Hour->projectId = $this->getModalVariable("projectid");
+        $c3Hour->hours = $_POST['data']['hours'];
+        $c3Hour->workPackageId = $_POST['data']['workpackage'];
+        $this->getApi()->getC3Manager()->addHour($c3Hour);
+    }
+}
+?>
---
+++ messages/src/main/java/com/thundashop/core/c3/C3ProjectPeriode.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thundashop.core.c3;
+
+import com.thundashop.core.common.DataCommon;
+import java.util.Date;
+
+/**
+ *
+ * @author ktonder
+ */
+class C3ProjectPeriode extends DataCommon {
+    public Date from;
+    public Date to;
+    public boolean isLocked = false;
+}

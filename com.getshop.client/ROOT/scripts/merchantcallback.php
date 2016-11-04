e110c0ec7dd2d75e2392a88f304caa36f91400a4
boggi <boggi@boggi-ThinkPad-W550s>
2016-11-04 16:22:37 +0100

    proxy for merchant callbacks.

---
+++ com.getshop.client/ROOT/scripts/merchantcallback.php
@@ -0,0 +1,44 @@
+<?php
+chdir("../");
+include '../loader.php';
+$factory = IocContainer::getFactorySingelton();
+
+$storeId = $factory->getStore()->id;
+
+$gets = http_build_query($_GET);
+$key = $_GET['merchant'];
+
+if($factory->getApi()->getStoreManager()->isProductMode()) {
+    $key .= "-prod";
+} else {
+    $key .= "-debug";
+}
+
+
+$addr = "http://pullserver_" . $key . "_" . $storeId . ".nettmannen.no?" . $gets;
+
+echo $addr;
+
+$ch = curl_init($addr);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST));
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+
+
+//$ch = curl_init($redirect_url);
+//
+//curl_setopt($ch, CURLOPT_POST, 1);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+//curl_setopt($ch, CURLOPT_HEADER, 0);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//$response = curl_exec($ch);
+//
+//if (!isset($response))
+//    return null;
+//return $response;
+
+?>

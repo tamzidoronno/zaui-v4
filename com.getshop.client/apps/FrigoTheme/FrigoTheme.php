bb365c5a9d10439cbef7494a0c6e0b21938d51fb
Kai Tønder <ktonder@gmail.com>
2016-11-26 11:40:44 +0100

    Started on Frigo Theme

---
+++ com.getshop.client/apps/FrigoTheme/FrigoTheme.php
@@ -0,0 +1,22 @@
+<?php
+namespace ns_3478113f_7faa_4c0a_89ad_b4e7f2bf95ad;
+
+class FrigoTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "FrigoTheme";
+    }
+
+    public function render() {
+        
+    }
+    
+    public function addScripts() {
+        echo "<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>";
+    }
+
+}
+?>

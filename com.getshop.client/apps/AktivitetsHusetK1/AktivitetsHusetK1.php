615e4813d09b150979a1abc71c607a7da9a3da25
boggi <boggi@boggi-ThinkPad-W550s>
2016-11-28 13:49:21 +0100

    initial design for aktivitetshuset k1.

---
+++ com.getshop.client/apps/AktivitetsHusetK1/AktivitetsHusetK1.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_0c9deebe_7c99_4697_bc93_54628c74c157;
+
+class AktivitetsHusetK1 extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "AktivitetsHusetK1";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

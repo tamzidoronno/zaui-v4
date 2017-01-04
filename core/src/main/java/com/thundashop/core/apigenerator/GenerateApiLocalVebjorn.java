53e68ae6c535cf360503f13ff7502040e83e8be3
vebjobb <vebjobb@VebJobb>
2017-01-04 15:59:13 +0100

    changes for frigo.

---
+++ core/src/main/java/com/thundashop/core/apigenerator/GenerateApiLocalVebjorn.java
@@ -0,0 +1,20 @@
+package com.thundashop.core.apigenerator;
+
+import com.thundashop.core.common.GetShopLogHandler;
+import com.thundashop.core.common.ManagerSubBase;
+import java.io.IOException;
+
+/**
+ *
+ * @author ktonder
+ */
+public class GenerateApiLocalVebjorn {
+    public static void main(String[] args) throws ClassNotFoundException, IOException, Exception {
+        GetShopLogHandler.logPrintStatic("Generating local api", null);
+        GenerateApi ga = new GenerateApi("/home/vebjobb/netbeans/3.0.0/GetShop Core/", "/home/vebjobb/netbeans/3.0.0/GetShop Messages/", "/source/getshop/3.0.0/com.getshop.client/", "/source/getshop/3.0.0/core", false);
+        ga.generate();
+
+        ga = new GenerateApi("/home/vebjobb/netbeans/3.0.0/GetShop Core/", "/home/vebjobb/netbeans/3.0.0/GetShop Messages/", "/source/getshop/3.0.0/com.getshop.pullserver/", "/source/getshop/3.0.0/core", false);
+        ga.generate();
+    }
+}

6564ea600352f2e031c6f9f49a33c4b96a2a8780
Kai Tønder <ktonder@gmail.com>
2017-01-05 14:31:51 +0100

    Dont including workpackages that is not activated in SFI report

--- core/src/main/java/com/thundashop/core/c3/C3Manager.java
+++ core/src/main/java/com/thundashop/core/c3/C3Manager.java
@@ -800,7 +800,7 @@ public class C3Manager extends ManagerBase implements IC3Manager {
     private List<SFIExcelReportData> createReportDatas(Date start, Date end, String companyId, boolean segregate) {
         List<C3Project> projectsToUse = getAllProjectsConnectedToCompany(companyId);
         List<User> users = userManager.getUsersByCompanyId(companyId);
-        List<WorkPackage> workPackagesToUse = getWorkPackages(projectsToUse, users, start, end);
+        List<WorkPackage> workPackagesToUse = getWorkPackages(projectsToUse, users, start, end, companyId);
         
         Collections.sort(workPackagesToUse, (o1, o2) -> {
             return o1.name.compareTo(o2.name);
@@ -995,12 +995,14 @@ public class C3Manager extends ManagerBase implements IC3Manager {
         return retMap;
     }
 
-    private List<WorkPackage> getWorkPackages(List<C3Project> projectsToUse, List<User> users, Date start, Date end) {
+    private List<WorkPackage> getWorkPackages(List<C3Project> projectsToUse, List<User> users, Date start, Date end, String companyId) {
         TreeSet<String> wpIds = new TreeSet();
         
         for (C3Project project : projectsToUse) {
             for (String wpId : project.workPackages) {
-                wpIds.add(wpId);
+                if (project.isWorkPackageActivated(companyId, wpId)) {
+                    wpIds.add(wpId);
+                }
             }
         }
         

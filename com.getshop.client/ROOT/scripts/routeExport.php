007ae09d231b6cf363fe420d485841c1d7b4a182
Kai Tønder <ktonder@gmail.com>
2017-01-22 12:35:41 +0100

    Can search by parameters

--- com.getshop.client/ROOT/scripts/routeExport.php
+++ com.getshop.client/ROOT/scripts/routeExport.php
@@ -7,8 +7,13 @@ $factory->getApi()->getUserManager()->logOn($_GET['username'], $_GET['password']
 $route = $factory->getApi()->getTrackAndTraceManager()->getExport($_GET["routeId"]); //"2121 Tue 12/06/2016");
 $toPrint = array();
 
-
-if (isset($_GET['podbarcode'])) {
+if (isset($_GET['orderreference'])) {
+    foreach ($route as $order) {
+        if (trim($order->ORReferenceNumber) == $_GET['orderreference']) {
+            $toPrint[] = $order;            
+        }
+    }
+} else if (isset($_GET['podbarcode'])) {
     foreach ($route as $order) {
         if (trim($order->PODBarcodeID) == $_GET['podbarcode']) {
             $toPrint[] = $order;            

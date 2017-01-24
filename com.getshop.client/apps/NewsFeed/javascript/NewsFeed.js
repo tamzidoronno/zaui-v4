f88b7ffc88c5f16e1e8a0d4b00e4e25f8969c2c0
vebjobb <vebjobb@VebJobb>
2017-01-24 13:26:31 +0100

    changes for Frigo, bugfix for Newsfeed. Made 2nd template for newsfeed, and using it on Frigo.

---
+++ com.getshop.client/apps/NewsFeed/javascript/NewsFeed.js
@@ -0,0 +1,14 @@
+app.NewsFeed = { 
+    needPackery : false,
+    init: function() {
+
+    },
+    loadSettings: function(element, application) {
+        var config = { showSettings : true, draggable: true, title: "Settings", items: [] }
+        var toolbox = new GetShopToolbox(config, application);
+        toolbox.show();
+        toolbox.attachToElement(application, 2);
+    }
+};
+
+app.NewsFeed.init();

0011551b8a863de962a07afbbd8caa8d1417aa19
Kai Tønder <ktonder@gmail.com>
2017-02-14 17:49:22 +0100

    Added a new payment method

---
+++ com.getshop.client/apps/Deltaker.No/Deltaker.No.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_4c124a23_7920_4ac5_8759_64ef77e5b67a;
+
+class Deltaker.No extends \PaymentApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "Deltaker.No";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

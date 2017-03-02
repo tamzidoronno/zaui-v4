27c18b377e3e6531308543d56211bfa184b92327
vebjobb <vebjobb@VebJobb>
2017-03-02 17:47:34 +0100

    Changes for Oslo Kommune, bizplay Event.

--- com.getshop.client/ROOT/scripts/simpledayeventlist.php
+++ com.getshop.client/ROOT/scripts/simpledayeventlist.php
@@ -9,7 +9,7 @@ $j = 0;
 echo "<table class='bizplay_simpledayeventlist'>";
 
     foreach($lists as $list) {
-            if($j < ($_GET['page'] * 12)) {
+            if($j < ($_GET['page'] * 11)) {
                 $j++;
                 continue;
             }
@@ -19,7 +19,7 @@ echo "<table class='bizplay_simpledayeventlist'>";
             if($time < $halfhour) {
                 continue;
             }
-            if($i > 12) {
+            if($i > 11) {
                 break;
             }
 
@@ -39,8 +39,8 @@ echo "<table class='bizplay_simpledayeventlist'>";
 
             echo "<tr class='eventlistElement'>";
                 echo "<td class='day'>" . $day . "<br>" . $month . "</td>";
-                echo "<td class='content'><span class='title'>" . $title . "</span><br>" . nl2br(trim($shortDesc)) . "</td>";
-                echo "<td class='kontakt'><span class='place'><b>Sted: </b> " . $place . "</span><br><br><span class='arrangedby'><b>Arrangør: </b>" . $arrangedBy . "</td>";
+                echo "<td class='content'><span class='title'>" . $title . "</span><br><span class='place'><b>Sted: </b> ". $place ."</span></td>";
+                echo "<td class='tdArranged'><span class='arrangedby'><b>Arrangør: </b>" . $arrangedBy . "</td>";
                 echo "<td class='time'>" . $starttime . "</td>";
             echo "</tr>";
             $i++; 
@@ -52,18 +52,11 @@ echo "</table>"
 
 <style>
     body{
-        background-color:#4D4D55;
+        padding-top:200px;
+        padding-bottom: 200px;
+        background-color:#fff;
         font-family:Arial, Helvetica, sans-serif;
-    }
-    .heading{
-        font-size: 48px;
-        text-align: center;
-        font-weight:bold;
-        
-        padding:2px;
-        
-        color: #4D4D55;
-        background-color:#FFF;
+        font-size:20px;
     }
     .bizplay_simpledayeventlist{
         width: 100%;
@@ -72,37 +65,34 @@ echo "</table>"
         color: #4D4D55;
         background-color: #FFF; 
         position: relative;
-        border-collapse: collapse;
-        
-        /*border:10px solid #4D4D55;*/
-        
+        border-collapse: collapse;  
     }
     tr:nth-child(odd){background-color: #f1f1f1}
     .eventlistElement td{
+        height:150px;
         padding: 15px;     
     }
     .eventlistElement td.day{
         text-align: center;
-        font-size: 48px;
+        font-size: 32px;
         font-weight: bold;
         width:10%;
-    }
-    .eventlistElement td.content{
-        text-align: justify;
-        font-size: 16px;
-        padding-right:50px;
-    }
-    .eventlistElement td.kontakt{
-        width:25%;
     }
     td.content .title{
         font-weight: bold;
-        font-size: 24px;
+        font-size: 32px;
     }
     .eventlistElement td.time{
         text-align: left;
-        font-size: 48px;
+        font-size: 32px;
         font-weight: bold;
         width:10%;
+    }
+    .eventlistElement .tdArranged{
+        width:28%;
+    }
+    .eventlistElement .arrangedby,
+    .eventlistElement .place{
+        font-size:20px;
     }
 </style>

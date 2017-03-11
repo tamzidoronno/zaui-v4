9d3ba5fa69f9ef612a52e57d03ec097c0aac3b11
Kai Tønder <ktonder@gmail.com>
2017-03-11 20:35:54 +0100

    Made a new theme

---
+++ com.getshop.client/apps/SmaKidsTheme/SmaKidsTheme.php
@@ -0,0 +1,21 @@
+<?php
+namespace ns_20fc8877_9526_4993_99bf_541a96c00821;
+
+class SmaKidsTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "SmaKidsTheme";
+    }
+
+    public function render() {
+        
+    }
+    
+    public function addScripts() {
+        echo "<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>";
+    }
+}
+?>

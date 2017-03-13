00732cb5fb87352b3554d7457c7e0461fcef75ea
Kai Tønder <ktonder@gmail.com>
2017-03-13 21:33:54 +0100

    Added facebook icon to bildoy marine

--- com.getshop.client/apps/BildoyMarineTheme/javascript/BildoyMarineTheme.js
+++ com.getshop.client/apps/BildoyMarineTheme/javascript/BildoyMarineTheme.js
@@ -23,7 +23,7 @@ app.BildoyMarineTheme = {
             var divInner = $('<div/>');
             divInner.addClass('gs_page_width');
             
-            divInner.html('<a href="/været.html"><i class="fa fa-video-camera"></i>&nbsp; <span>Webkamera</span></a> <a id="topWeatherLink" href="/været.html"><img id="topSymbol" style="display: none" alt="Været" src="http://symbol.yr.no/grafikk/sym/b30/04.png"> <i class="fa fa-spinner fa-spin" id="topWaitingForWeatherData"></i> <span id="topTemp">-</span><span>° Været</span></a>');
+            divInner.html('<a target="_blank" href="http://facebook.com/bildoymarina/"><i class="fa fa-facebook"></i> Facebook </a>&nbsp;&nbsp;&nbsp;<a href="/været.html"><i class="fa fa-video-camera"></i>&nbsp; <span>Webkamera</span></a> <a id="topWeatherLink" href="/været.html"><img id="topSymbol" style="display: none" alt="Været" src="http://symbol.yr.no/grafikk/sym/b30/04.png"> <i class="fa fa-spinner fa-spin" id="topWaitingForWeatherData"></i> <span id="topTemp">-</span><span>° Været</span></a>');
             
             divOuter.prepend(divInner);
             

6e0ac711d3fbd001e7e218bbd886a5e06a7b36a5
vebjobb <vebjobb@VebJobb>
2017-03-22 13:59:50 +0100

    Added BookingTemplateTheme to bookingtemplate site.

---
+++ com.getshop.client/apps/BookingTemplateTheme/BookingTemplateTheme.php
@@ -0,0 +1,20 @@
+<?php
+namespace ns_d5dd54ea_2580_4a5c_9c45_8ca919dfeced;
+
+class BookingTemplateTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "BookingTemplateTheme";
+    }
+
+    public function render() {
+        
+    }
+    public function addScripts() {
+        echo "<link href='//fonts.googleapis.com/css?family=Exo' rel='stylesheet'>";
+    }
+}
+?>

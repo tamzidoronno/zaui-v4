0890973d74d44e837e58c55bf6406b36e279f3e9
boggi <boggi@boggi-ThinkPad-W550s>
2017-04-25 13:48:48 +0200

    push to ratemanager async. save time.

---
+++ core/src/main/java/com/thundashop/core/ratemanager/RateManagerPushBookingThread.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thundashop.core.ratemanager;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ *
+ * @author boggi
+ */
+public class RateManagerPushBookingThread extends Thread {
+    public String url;
+    public String data;
+
+    public RateManagerPushBookingThread(String url, String data) {
+        this.url = url;
+        this.data = data;
+    }
+    
+    
+    
+    public void run() {
+        try {
+        long start = System.currentTimeMillis();
+            String encoding = "UTF-8";
+
+            URL urlObj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+            connection.setRequestProperty("Content-Type", "application/xml");
+            connection.setRequestProperty("Accept", "application/xml");
+
+            connection.setDoOutput(true);
+            DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(new String(data.getBytes(), encoding));
+            outputStream.flush();
+            outputStream.close();
+
+            try {
+                BufferedReader responseStream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                String responseLine;
+                StringBuilder responseBuffer = new StringBuilder();
+
+                while((responseLine = responseStream.readLine()) != null) {
+                    responseBuffer.append(responseLine);
+                }
+                long end = System.currentTimeMillis();
+                System.out.println("Time: " + (end-start));
+            }catch(IOException ex) {
+                BufferedReader responseStream = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+
+                String responseLine;
+                StringBuilder responseBuffer = new StringBuilder();
+
+                while((responseLine = responseStream.readLine()) != null) {
+                    responseBuffer.append(responseLine);
+                }
+                String res = responseBuffer.toString();
+                System.out.println(res);
+
+                throw ex;
+            }        
+        }catch(Exception ee) {
+            ee.printStackTrace();
+        }
+    }
+}

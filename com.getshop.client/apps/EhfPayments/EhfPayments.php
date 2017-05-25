dbdaeb368808d94b61f325dc648c9d933b32e0f9
boggi <boggi@boggi-ThinkPad-W550s>
2017-05-25 15:28:04 +0200

    start of supporting ehf payments.

---
+++ com.getshop.client/apps/EhfPayments/EhfPayments.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_bd13472e_87ee_4b8d_a1ae_95fb471cedce;
+
+class EhfPayments extends \PaymentApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "EhfPayments";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

9782718d66e7209bcb9ff8ede98f5f494f79b2e1
Kai Tønder <ktonder@gmail.com>
2017-05-29 13:35:00 +0200

    Started on new website

---
+++ com.getshop.client/apps/Verksgate12StavangerTheme/Verksgate12StavangerTheme.php
@@ -0,0 +1,21 @@
+<?php
+namespace ns_e377a31e_b48e_4758_aa1a_cbdf5aa47cea;
+
+class Verksgate12StavangerTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "Verksgate12StavangerTheme";
+    }
+
+    public function render() {
+       
+    }
+    
+    public function addScripts() {
+        echo "<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>";
+    }
+}
+?>

e9d317c81c66d6d6c1d17e6657c6e53e67148d57
boggi <boggi@boggi-ThinkPad-W550s>
2017-08-08 15:57:30 +0200

    Added pms terminal

---
+++ html5apps/PmsTerminal/public_html/app/routes.js
@@ -0,0 +1,32 @@
+angular.module('TrackAndTrace')
+.config(['$stateProvider', function ($stateProvider) {
+    $stateProvider.state('base', {
+        url: '',
+        templateUrl: 'components/general/main.html',
+        abstract: true,
+        controller : controllers.BaseController,
+
+    });
+    
+    $stateProvider.state('base.home', {
+        url: '/home',
+
+        params: {
+            action : { refreshData : false } 
+        },
+
+        views: {
+            main: {
+                templateUrl : 'components/home/home.html',
+                controller : controllers.HomeController
+            },
+            footer: {
+                templateUrl : 'components/home/footer.html',
+                controller : controllers.HomeController
+            }
+        }
+    });
+
+ 
+   
+}]);

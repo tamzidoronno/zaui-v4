20e9bad5e762bc0ce50320770f1ba2302e2d70e6
Kai Tønder <ktonder@gmail.com>
2017-08-11 11:15:41 +0200

    Fixed PDF problem

--- com.getshop.client/ROOT/scripts/promeister/doDownloadCoursesPdf.php
+++ com.getshop.client/ROOT/scripts/promeister/doDownloadCoursesPdf.php
@@ -5,8 +5,8 @@ include('downloadCoursesPdf.php');
 $content = ob_get_contents();
 ob_end_clean();
 
-//echo $content;
 
+$content = base64_encode($content);
 header('Content-Type: application/pdf');
 $base64 = $factory->getApi()->getGetShop()->getBase64EncodedPDFWebPageFromHtml($content);
 echo base64_decode($base64);

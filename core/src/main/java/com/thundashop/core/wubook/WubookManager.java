1759f02247e63a776eae3c078d449099a6fc9c59
boggi <boggi@boggi-ThinkPad-W550s>
2017-08-15 09:34:39 +0200

    forgot this one as well.

--- core/src/main/java/com/thundashop/core/wubook/WubookManager.java
+++ core/src/main/java/com/thundashop/core/wubook/WubookManager.java
@@ -1427,7 +1427,12 @@ public class WubookManager extends GetShopSessionBeanNamed implements IWubookMan
                 Date start = startcal.getTime();
                 endCal.add(Calendar.DAY_OF_YEAR, 1);
                 Date end = endCal.getTime();
-                int count = pmsManager.getNumberOfAvailable(rdata.bookingEngineTypeId, start, end);
+                int count = 0;
+                try {
+                    count = bookingEngine.getNumberOfAvailableWeakButFaster(rdata.bookingEngineTypeId, start, end);
+                }catch(BookingEngineException e) {
+                    
+                }
                 int totalForType = bookingEngine.getBookingItemsByType(rdata.bookingEngineTypeId).size();
                 if(count > 0 && totalForType > 2) {
                     count -= toRemove;

81413e6d468bfb40b71fbbba14064a2b0fa200d8
boggi <boggi@boggi-ThinkPad-W550s>
2017-08-16 12:14:47 +0200

    should include sublocks as well.

--- core/src/main/java/com/thundashop/core/getshoplock/GetShopLockManager.java
+++ core/src/main/java/com/thundashop/core/getshoplock/GetShopLockManager.java
@@ -826,7 +826,7 @@ public class GetShopLockManager extends GetShopSessionBeanNamed implements IGetS
             List<BookingItem> items = bookingEngine.getBookingItems();
             GetShopDevice toSet = null;
             for(GetShopDevice dev : devices.values()) {
-                if(connectedToBookingEngineItem(dev, items) == null ) {
+                if(connectedToBookingEngineItem(dev, items) == null && !dev.isSubLock()) {
                     continue;
                 }
 

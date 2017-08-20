b0e8185fc8676e4c21fd881c7792110427c1800c
boggi <boggi@boggi-ThinkPad-W550s>
2017-08-20 21:01:55 +0200

    ignore if the booking coming from OTA's / wubook if the addons should be included into the room type.

--- core/src/main/java/com/thundashop/core/pmsmanager/PmsManager.java
+++ core/src/main/java/com/thundashop/core/pmsmanager/PmsManager.java
@@ -5321,9 +5321,6 @@ public class PmsManager extends GetShopSessionBeanNamed implements IPmsManager {
     }
 
     private void addDefaultAddons(PmsBooking booking) {
-        if(booking.channel != null && !booking.channel.isEmpty()) {
-            return;
-        }
         HashMap<Integer, PmsBookingAddonItem> addons = getConfigurationSecure().addonConfiguration;
         for(PmsBookingRooms room : booking.getActiveRooms()) {
             for(PmsBookingAddonItem item : addons.values()) {

d499612426f1ec4e64c75013414a9daf971f8056
boggi <boggi@boggi-ThinkPad-W550s>
2017-09-04 16:16:58 +0200

    continued on the vipps integration, and created a new payment type for trysilhotell.

--- core/src/main/java/com/thundashop/core/vippsmanager/VippsManager.java
+++ core/src/main/java/com/thundashop/core/vippsmanager/VippsManager.java
@@ -3,7 +3,6 @@ package com.thundashop.core.vippsmanager;
 import com.getshop.pullserver.PullMessage;
 import com.getshop.scope.GetShopSession;
 import com.google.gson.Gson;
-import com.thundashop.core.applications.StoreApplicationInstancePool;
 import com.thundashop.core.applications.StoreApplicationPool;
 import com.thundashop.core.appmanager.data.Application;
 import com.thundashop.core.common.FrameworkConfig;
@@ -12,28 +11,17 @@ import com.thundashop.core.getshop.GetShopPullService;
 import com.thundashop.core.messagemanager.MessageManager;
 import com.thundashop.core.ordermanager.OrderManager;
 import com.thundashop.core.ordermanager.data.Order;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import com.thundashop.core.webmanager.WebManager;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
-import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component 
 @GetShopSession
 public class VippsManager  extends ManagerBase implements IVippsManager {
-    public String sslKeyDev = "/home/boggi/Dropbox/Leverandører/Vipps/ssl/dev.key";
-    public String sslKeyProd = "/home/boggi/Dropbox/Leverandører/Vipps/ssl/prod.key";
-    public String sslCertDev = "/home/boggi/Dropbox/Leverandører/Vipps/ssl/912574784-GetShop-InApp-Test.cer";
-    public String sslCertProd = "/home/boggi/Dropbox/Leverandører/Vipps/ssl/912574784-GetShop-InApp-Prod.cer";
     
     @Autowired
     FrameworkConfig frameworkConfig;
@@ -49,96 +37,83 @@ public class VippsManager  extends ManagerBase implements IVippsManager {
     
     @Autowired
     MessageManager messageManager;
+    
+    @Autowired
+    WebManager webManager;
+    
     private boolean sentPollFailed;
     
+    private String endpointTest = "https://apitest.vipps.no/";
+    private String endpoint = "https://api.vipps.no/";
+    
     public String startMobileRequest(String phoneNumber, String orderId) throws Exception {
-        String sslKey = sslKeyDev;
-        String sslCert = sslCertDev;
-        if(frameworkConfig.productionMode) {
-            sslKey = sslKeyProd;
-            sslCert = sslCertProd;
-        }
+        String token = getToken();
+        
+        Order order = orderManager.getOrderSecure(orderId);
         
         Application vippsApp = storeApplicationPool.getApplication("f1c8301d-9900-420a-ad71-98adb44d7475");
-        String key = vippsApp.getSetting("merchantid") + "-vippsdev";
+        String clientId = vippsApp.getSetting("clientid"); 
+        String ocp = vippsApp.getSetting("subscriptionkeysecondary");
+        String adress = vippsApp.getSetting("adress");
+        String serialNumber = vippsApp.getSetting("serialNumber");
+        
+        String startEndpoint = endpoint;
+        
+        if(!frameworkConfig.productionMode) {
+            startEndpoint = endpointTest;
+        }
+        
+        String key = clientId + "-vippsdev";
         if(frameworkConfig.productionMode) {
-            key = vippsApp.getSetting("merchantid") + "-vippsprod";
+            key = clientId + "-vippsprod";
         }
         
         String callback = "http://pullserver_"+key+"_"+storeId+".nettmannen.no";
+        callback = "TEST";
         
-        Order order = orderManager.getOrder(orderId);
+        
+        String url = startEndpoint + "/Ecomm/v1/payments";
         
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        String exportFile = "/tmp/" +UUID.randomUUID().toString() + ".txt";
-        
-        try{
-            PrintWriter writer = new PrintWriter(exportFile, "UTF-8");
-            writer.println("{\n" +
-            " \"merchantInfo\": {\n" +
-            " \"merchantSerialNumber\": \""+vippsApp.getSetting("merchantid")+"\",\n" +
-            " \"callBack\": \""+callback+"\"\n" +
-            " },\n" +
-            " \"customerInfo\": {\n" +
-            " \"mobileNumber\": \""+phoneNumber+"\"\n" +
-            " },\n" +
-            " \"transaction\": {\n" +
-            " \"orderId\": \""+order.incrementOrderId+"\",\n" +
-            " \"refOrderId\": \""+order.incrementOrderId+"\",\n" +
-            " \"amount\": "+orderManager.getTotalAmount(order)*100+",\n" +
-            " \"transactionText\": \"\"\n" +
-            " }\n" +
-            "}");
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-           // do something
-        }
-        
-        String[] cmd = {
-            "/usr/bin/curl", "https://gw.atest.dnbnor.no:1447/vipps/v1/payments", "-sL",
-            "--key", sslKey, 
-            "--cert", sslCert,  
-            "-H", "Content-type: application/json",
-            "-H", "X-UserId:" + vippsApp.getSetting("userid"),
-            "-H", "Authorization:Secret " + vippsApp.getSetting("auth_secret"),
-            "-H", "X-Request-Id:" + vippsApp.getSetting("xrequestid"),
-            "-H", "X-TimeStamp:" + sdf.format(new Date()),
-            "-H", "X-Source-Address:148.251.15.227",
-            "--data","@" + exportFile
-        };
         
         
+        HashMap<String, String> header = new HashMap();
+        header.put("Authorization", token);
+        header.put("X-Request-Id", orderId);
+        header.put("X-TimeStamp", sdf.format(new Date()));
+        header.put("X-Source-Address", adress);
+        header.put("X-App-Id", clientId);
+        header.put("Ocp-Apim-Subscription-Key", ocp);
         
-        ProcessBuilder processBuilder = new ProcessBuilder(cmd);
-        processBuilder.redirectErrorStream(true);
-        Process process = processBuilder.start();
-        StringBuilder processOutput = new StringBuilder();
-
-        try (BufferedReader processOutputReader = new BufferedReader(
-                new InputStreamReader(process.getInputStream()));)
-        {
-            String readLine;
-
-            while ((readLine = processOutputReader.readLine()) != null)
-            {
-                processOutput.append(readLine + System.lineSeparator());
-            }
-
-            process.waitFor();
-        }
-        order.status = Order.Status.NEEDCOLLECTING;
-        orderManager.saveOrderInternal(order);
         
         try {
-            File file = new File(exportFile);
-            file.delete();
-        } catch (Exception x) {
-            x.printStackTrace();
+            Gson gson = new Gson();
+            
+            StartTransactionBody body = new StartTransactionBody();
+            body.merchantInfo.merchantSerialNumber = serialNumber;
+            body.merchantInfo.callBack = callback;
+            
+            body.customerInfo.mobileNumber = phoneNumber;
+            body.transaction.orderId = order.incrementOrderId + "";
+            body.transaction.amount = new Double(orderManager.getTotalAmount(order) * 100).intValue();
+            body.transaction.transactionText = "Payment for order: " + order.incrementOrderId;
+            body.transaction.timeStamp = sdf.format(new Date());
+            String json = gson.toJson(body);
+            
+            for(String k : header.keySet()) {
+                System.out.println(k + " : " + header.get(k));
+            }
+            System.out.println("JsOn:" + json);
+            
+            String res = webManager.htmlPostBasicAuth(url, json, true, "UTF-8", "", "", false, "POST", header);
+            System.out.println(res);
+        }catch(Exception e) {
+            e.printStackTrace();
+            return "";
         }
         
-        String result = processOutput.toString().trim();
-        return result;
+        return "";
+        
     }
     
     @Override
@@ -215,80 +190,48 @@ public class VippsManager  extends ManagerBase implements IVippsManager {
 
     @Override
     public boolean cancelOrder(String orderId) throws Exception {
-        String sslKey = sslKeyDev;
-        String sslCert = sslCertDev;
-        if(frameworkConfig.productionMode) {
-            sslKey = sslKeyProd;
-            sslCert = sslCertProd;
-        }
-        
         Application vippsApp = storeApplicationPool.getApplication("f1c8301d-9900-420a-ad71-98adb44d7475");
         Order order = orderManager.getOrder(orderId);
+        String token = getToken();
         
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        String exportFile = "/tmp/" +UUID.randomUUID().toString() + ".txt";
+        return true;
+    }
+
+    private String getToken() {
+        Application vippsApp = storeApplicationPool.getApplication("f1c8301d-9900-420a-ad71-98adb44d7475");
+        String clientId = vippsApp.getSetting("clientid"); 
+        String secret = vippsApp.getSetting("secret");
+        String ocp = vippsApp.getSetting("subscriptionkeyprimary");
         
-        try{
-            PrintWriter writer = new PrintWriter(exportFile, "UTF-8");
-            writer.println("{\n" +
-            " \"merchantInfo\": {\n" +
-            " \"merchantSerialNumber\": \""+vippsApp.getSetting("merchantid")+"\"\n" +
-            " },\n" +
-            " \"transaction\": {\n" +
-            " \"transactionText\": \"\"\n" +
-            " }\n" +
-            "}");
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-           // do something
+        String startEndpoint = endpoint;
+        
+        if(!frameworkConfig.productionMode) {
+            startEndpoint = endpointTest;
         }
         
-        String[] cmd = {
-            "/usr/bin/curl",
-            "-X", "PUT",
-            "https://gw.atest.dnbnor.no:1447/vipps/v1/payments/"+order.incrementOrderId+"/cancel", "-sL",
-            "--key", sslKey, 
-            "--cert", sslCert,  
-            "-H", "Content-type: application/json",
-            "-H", "X-UserId:" + vippsApp.getSetting("userid"),
-            "-H", "Authorization:Secret " + vippsApp.getSetting("auth_secret"),
-            "-H", "X-Request-Id:" + vippsApp.getSetting("xrequestid"),
-            "-H", "X-TimeStamp:" + sdf.format(new Date()),
-            "-H", "X-Source-Address:148.251.15.227",
-            "--data","@" + exportFile
-        };
+        String url = startEndpoint + "accessToken/get";
         
-
+        System.out.println(clientId + " - " + secret + "(" + url + ")");
         
-        ProcessBuilder processBuilder = new ProcessBuilder(cmd);
-        processBuilder.redirectErrorStream(true);
-        Process process = processBuilder.start();
-        StringBuilder processOutput = new StringBuilder();
-
-        try (BufferedReader processOutputReader = new BufferedReader(
-                new InputStreamReader(process.getInputStream()));)
-        {
-            String readLine;
-
-            while ((readLine = processOutputReader.readLine()) != null)
-            {
-                processOutput.append(readLine + System.lineSeparator());
-            }
-
-            process.waitFor();
-        }
-        order.status = Order.Status.WAITING_FOR_PAYMENT;
-        orderManager.saveOrderInternal(order);
+        HashMap<String, String> header = new HashMap();
+        header.put("client_id", clientId);
+        header.put("client_secret", secret);
+        header.put("Ocp-Apim-Subscription-Key", ocp);
         
         try {
-            File file = new File(exportFile);
-            file.delete();
-        } catch (Exception x) {
-            x.printStackTrace();
+            Gson gson = new Gson();
+                        
+            for(String k : header.keySet()) {
+                System.out.println(k + " : " + header.get(k));
+            }
+            
+            String res = webManager.htmlPostBasicAuth(url, "test", false, "UTF-8", "", "", false, "POST", header);
+            AccessTokenResponse tokeResp = gson.fromJson(res, AccessTokenResponse.class);
+            return tokeResp.access_token;
+        }catch(Exception e) {
+            e.printStackTrace();
+            return "";
         }
-
-        return true;
     }
 
     

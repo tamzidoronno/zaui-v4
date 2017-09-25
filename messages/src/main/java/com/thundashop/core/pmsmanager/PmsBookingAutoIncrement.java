d56f553e29ec70f0180bb35ad24baa925b95b817
boggi <boggi@boggi-ThinkPad-W550s>
2017-09-25 14:04:39 +0200

    added support for an inc booking id.

---
+++ messages/src/main/java/com/thundashop/core/pmsmanager/PmsBookingAutoIncrement.java
@@ -0,0 +1,7 @@
+package com.thundashop.core.pmsmanager;
+
+import com.thundashop.core.common.DataCommon;
+
+public class PmsBookingAutoIncrement extends DataCommon {
+    public Integer incrementBookingId = 1000000;
+}

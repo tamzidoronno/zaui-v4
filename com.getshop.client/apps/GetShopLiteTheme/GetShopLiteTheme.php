1540f843c7ef96c1fe80ca30d89bbff50ce6d182
vebjobb <vebjobb@VebJobb>
2017-10-05 16:08:00 +0200

    Added GetShopLiteTheme. - Used for New Booking application

---
+++ com.getshop.client/apps/GetShopLiteTheme/GetShopLiteTheme.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_cf1cbe7b_9aed_4d00_b67f_40be9b43a11d;
+
+class GetShopLiteTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "GetShopLiteTheme";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

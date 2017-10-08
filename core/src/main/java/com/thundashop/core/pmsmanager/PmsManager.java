bfe4b28cd682f4f05b9653c18b8e296081c43819
boggi <boggi@boggi-ThinkPad-W550s>
2017-10-08 11:47:39 +0200

    exception handling while autoassigining.

--- core/src/main/java/com/thundashop/core/pmsmanager/PmsManager.java
+++ core/src/main/java/com/thundashop/core/pmsmanager/PmsManager.java
@@ -2650,55 +2650,60 @@ public class PmsManager extends GetShopSessionBeanNamed implements IPmsManager {
     }
 
     void autoAssignItem(PmsBookingRooms room) {
-        List<BookingItem> items = bookingEngine.getAvailbleItemsWithBookingConsidered(room.bookingItemTypeId, room.date.start, room.date.end, room.bookingId);
-        Collections.sort(items, new Comparator<BookingItem>() {
-            public int compare(BookingItem o1, BookingItem o2) {
-                return o1.order.compareTo(o2.order);
-            }
-        });
-        
-        if(!getConfigurationSecure().avoidRandomRoomAssigning) {
-            long seed = System.nanoTime();
-            Collections.shuffle(items, new Random(seed));
-            Collections.shuffle(items, new Random(seed));
-        }
-            
-        if (items.isEmpty()) {
-            if(!warnedAboutAutoassigning) {
-                messageManager.sendErrorNotification("Failed to autoassign room, its critical since someone will not recieve the code for the room now.", null);
-                warnedAboutAutoassigning = true;
-            }
-            logPrint("....");
-            logPrint("No items available?");
-        } else {
-            BookingItem item = null;
-            for(BookingItem tmpitem : items) {
-                item = tmpitem;
-                PmsAdditionalItemInformation additionalroominfo = getAdditionalInfo(item.id);
-                //Comment to commit.
-                if(additionalroominfo.isClean()) {
-                    break;
+        try {
+            List<BookingItem> items = bookingEngine.getAvailbleItemsWithBookingConsidered(room.bookingItemTypeId, room.date.start, room.date.end, room.bookingId);
+            Collections.sort(items, new Comparator<BookingItem>() {
+                public int compare(BookingItem o1, BookingItem o2) {
+                    return o1.order.compareTo(o2.order);
                 }
-            }
-            room.bookingItemId = item.id;
-            room.bookingItemTypeId = item.bookingItemTypeId;
+            });
 
-            if (room.bookingId != null) {
-                try {
-                    bookingEngine.changeBookingItemOnBooking(room.bookingId, item.id);
-                }catch(Exception e) {
-                    if(warnedAbout.contains("Itemchangedfailed_" + room.pmsBookingRoomId)) {
-                        messageManager.sendErrorNotification("Booking failure for room: " + room.pmsBookingRoomId + ", rooms where not reserved in booking engine. address: " + storeManager.getMyStore().webAddress, null);
-                        warnedAbout.add("Itemchangedfailed_" + room.pmsBookingRoomId);
+            if(!getConfigurationSecure().avoidRandomRoomAssigning) {
+                long seed = System.nanoTime();
+                Collections.shuffle(items, new Random(seed));
+                Collections.shuffle(items, new Random(seed));
+            }
+
+            if (items.isEmpty()) {
+                if(!warnedAboutAutoassigning) {
+                    messageManager.sendErrorNotification("Failed to autoassign room, its critical since someone will not recieve the code for the room now.", null);
+                    warnedAboutAutoassigning = true;
+                }
+                logPrint("....");
+                logPrint("No items available?");
+            } else {
+                BookingItem item = null;
+                for(BookingItem tmpitem : items) {
+                    item = tmpitem;
+                    PmsAdditionalItemInformation additionalroominfo = getAdditionalInfo(item.id);
+                    //Comment to commit.
+                    if(additionalroominfo.isClean()) {
+                        break;
                     }
                 }
+                room.bookingItemId = item.id;
+                room.bookingItemTypeId = item.bookingItemTypeId;
+
+                if (room.bookingId != null) {
+                    try {
+                        bookingEngine.changeBookingItemOnBooking(room.bookingId, item.id);
+                    }catch(Exception e) {
+                        if(warnedAbout.contains("Itemchangedfailed_" + room.pmsBookingRoomId)) {
+                            messageManager.sendErrorNotification("Booking failure for room: " + room.pmsBookingRoomId + ", rooms where not reserved in booking engine. address: " + storeManager.getMyStore().webAddress, null);
+                            warnedAbout.add("Itemchangedfailed_" + room.pmsBookingRoomId);
+                        }
+                    }
+                }
+                PmsBooking booking = getBookingFromRoomSecure(room.pmsBookingRoomId);
+                String bookingId = "";
+                if (booking != null) {
+                    bookingId = booking.id;
+                }
+                logEntry("Autoasigned item " + item.bookingItemName, bookingId, null);
             }
-            PmsBooking booking = getBookingFromRoomSecure(room.pmsBookingRoomId);
-            String bookingId = "";
-            if (booking != null) {
-                bookingId = booking.id;
-            }
-            logEntry("Autoasigned item " + item.bookingItemName, bookingId, null);
+        }catch(Exception d) {
+            messageManager.sendErrorNotification("Exception happened while autoassigning room.", null);
+            logPrintException(d);
         }
     }
 

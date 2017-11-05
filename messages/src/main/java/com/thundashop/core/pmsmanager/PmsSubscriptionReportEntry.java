c2777455361faa2859e2c032d4cdad62b4475f41
boggi <boggi@boggi-ThinkPad-W550s>
2017-11-05 12:20:18 +0100

    created a list for creditcare to use to follow up on non paid orders.

--- messages/src/main/java/com/thundashop/core/pmsmanager/PmsSubscriptionReportEntry.java
+++ messages/src/main/java/com/thundashop/core/pmsmanager/PmsSubscriptionReportEntry.java
@@ -1,12 +1,15 @@
 package com.thundashop.core.pmsmanager;
 
 import com.thundashop.core.ordermanager.data.Order;
+import java.util.Date;
 import java.util.LinkedList;
 
 public class PmsSubscriptionReportEntry {
     public String itemName = "";
     public String owner = "";
     public LinkedList<Order> orders = new LinkedList();
+    public Date start;
+    public Date end;
     
     @Override
     public String toString() {

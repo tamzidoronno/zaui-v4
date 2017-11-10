a9f146e2f5ca45e6e8b70e8264e3039f88a7e297
vebjobb <vebjobb@VebJobb>
2017-11-10 14:47:12 +0100

    Changes for Aktivitetshuset k1 - Added the notorius includeExtraCss

--- com.getshop.client/apps/AktivitetsHusetK1/AktivitetsHusetK1.php
+++ com.getshop.client/apps/AktivitetsHusetK1/AktivitetsHusetK1.php
@@ -9,7 +9,26 @@ class AktivitetsHusetK1 extends \ThemeApplication implements \Application {
     public function getName() {
         return "AktivitetsHusetK1";
     }
-
+    
+    public function includeExtraCss() {
+        ?>
+        <style>
+            .gsarea[area="header"],.gsarea[area="footer"] { display: none; }
+            .gs_page_width { max-width: 1024px; width: inherit; }
+            @media only all and (min-width: 1000px) {
+                .gs_page_width {
+                    -webkit-box-shadow: -1px 0px 15px 0px rgba(0,0,0,0.82);
+                    -moz-box-shadow: -1px 0px 15px 0px rgba(0,0,0,0.82);
+                    box-shadow: -1px 0px 15px 0px rgba(0,0,0,0.82);
+                }
+            }
+            body {
+                background-color: #fff;
+            }
+        </style>
+        <?php
+    }
+    
     public function render() {
         
     }

22f32a57aa19ba5f310377dc41ef17a95623dd7d
vebjobb <vebjobb@VebJobb>
2017-11-16 10:57:09 +0100

    Added theme for Jegtvolden Fjordhotell.

---
+++ com.getshop.client/apps/JegtvoldenFjordhotellTheme/JegtvoldenFjordhotellTheme.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_12e63799_7f8c_40e9_8661_01133c9f8ddc;
+
+class JegtvoldenFjordhotellTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "JegtvoldenFjordhotellTheme";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

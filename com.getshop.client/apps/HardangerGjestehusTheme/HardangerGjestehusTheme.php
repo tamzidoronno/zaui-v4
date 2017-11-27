12fb0cb7f903587dfaeda533876bb2586d1b5418
ktonder <ktonder@ktonder-ThinkPad-P51s>
2017-11-27 09:45:45 +0100

    Hardanger theme

---
+++ com.getshop.client/apps/HardangerGjestehusTheme/HardangerGjestehusTheme.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_a70f0f5f_b5e5_4e29_a0ff_0d3816ef6d39;
+
+class HardangerGjestehusTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "HardangerGjestehusTheme";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

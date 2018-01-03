6635abbfa4de2d5526e9329302703f69e35861ed
ktonder <ktonder@ktonder-ThinkPad-P51s>
2018-01-03 19:42:07 +0100

    Started on page for sokndal turnforening

---
+++ com.getshop.client/apps/SokndalTurnforeningTheme/SokndalTurnforeningTheme.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_85496f08_efdf_4657_ad3b_ed6a82b5f26a;
+
+class SokndalTurnforeningTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "SokndalTurnforeningTheme";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

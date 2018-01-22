095254d7c6f22eb573b91cc828a2cfd72980878d
boggi <boggi@boggi-ThinkPad-W550s>
2018-01-22 14:19:15 +0100

    better visual feedback.

--- com.getshop.client/apps/PmsNewBooking/javascript/PmsNewBooking.js
+++ com.getshop.client/apps/PmsNewBooking/javascript/PmsNewBooking.js
@@ -6,13 +6,18 @@ app.PmsNewBooking = {
         
         $(document).on('keyup', '.PmsNewBooking [gsname="numberOfRooms"]', function() {
             $('.moreThenAvailableWarning').hide();
-           var number = $(this).val();
-           var type = $('select[gsname="type"]').val();
-           var available = parseInt($('.availableroomscounter[roomtype="'+type+'"]').text());
-           if(number > available) {
-               $(this).addClass('moreThenAvailable');
-            $('.moreThenAvailableWarning').slideDown();
-           }
+            var number = $(this).val();
+            var type = $('select[gsname="type"]').val();
+            var available = parseInt($('.availableroomscounter[roomtype="'+type+'"]').text());
+            var typeName = $('.availabletypename[roomtype="'+type+'"]').text();
+            if(number > available) {
+                $(this).addClass('moreThenAvailable');
+                $('.moreThenAvailableWarning').find('.maxrooms').html(available);
+                $('.moreThenAvailableWarning').find('.numberofrooms').html(number);
+                $('.moreThenAvailableWarning').find('.typename').html(typeName);
+                $('.moreThenAvailableWarning').find('.roomsdiff').html((number-available));
+                $('.moreThenAvailableWarning').fadeIn();
+            }
         });
     },
     reloadAvailability : function() {

8b9f5235cdf902385802270a282d89d8299690bc
ktonder <ktonder@ktonder-ThinkPad-P51s>
2018-01-24 21:49:57 +0100

    Disabled check of dead codes

--- core/src/main/java/com/thundashop/core/pmsmanager/PmsManagerProcessor.java
+++ core/src/main/java/com/thundashop/core/pmsmanager/PmsManagerProcessor.java
@@ -1327,7 +1327,7 @@ public class PmsManagerProcessor {
                 .forEach(masterUserSlot -> {
                     boolean isSlotInUse = isMasterUserSlotInUse(group, masterUserSlot, bookings);
                     if (!isSlotInUse) {
-                        manager.getShopLockSystemManager.renewCodeForSlot(group.id, masterUserSlot.slotId);
+//                        manager.getShopLockSystemManager.renewCodeForSlot(group.id, masterUserSlot.slotId);
                     }
                 });
         });

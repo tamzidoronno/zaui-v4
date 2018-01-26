f495b603d8f30ed83c1df0c0a4ce960ca50dc9d7
boggi <boggi@boggi-ThinkPad-W550s>
2018-01-26 12:21:14 +0100

    bug in long term deals.

--- core/src/main/java/com/thundashop/core/pmsmanager/PmsInvoiceManager.java
+++ core/src/main/java/com/thundashop/core/pmsmanager/PmsInvoiceManager.java
@@ -407,7 +407,7 @@ public class PmsInvoiceManager extends GetShopSessionBeanNamed implements IPmsIn
         int percentages = 0;
         int daysUsed = 0;
         if(room != null) {
-            for(Integer days : plan.longTermDeal.keySet()) {
+            for(Integer days : plan.getLongTermDeal().keySet()) {
                 if(days <= room.getNumberOfNights() && daysUsed < days) {
                     percentages = plan.longTermDeal.get(days);
                 }

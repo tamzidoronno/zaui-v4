6e6d8aa57aef617c62a51641231560cb06a17799
vebjobb <vebjobb@VebJobb>
2018-02-02 15:32:29 +0100

    Added theme for Ullensaker

---
+++ com.getshop.client/apps/UllensakerKommuneTheme/UllensakerKommuneTheme.php
@@ -0,0 +1,17 @@
+<?php
+namespace ns_fee8ad07_7cb1_4a54_a048_858fed76edde;
+
+class UllensakerKommuneTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "UllensakerKommuneTheme";
+    }
+
+    public function render() {
+        
+    }
+}
+?>

05e67b1a3febcf71c5be4c7dca95a706f20b4eb8
boggi <boggi@boggi-ThinkPad-W550s>
2018-02-06 20:41:05 +0100

    remove all sms addons.

--- core/src/main/java/com/thundashop/core/pmsmanager/SmsHistoryManager.java
+++ core/src/main/java/com/thundashop/core/pmsmanager/SmsHistoryManager.java
@@ -88,24 +88,26 @@ public class SmsHistoryManager extends GetShopSessionBeanNamed implements ISmsHi
                         boolean doNot = false;
                         PmsBookingAddonItem toAddonInternal = null;
                         PmsBookingAddonItem toAddonExternal = null;
-
+                        List<PmsBookingAddonItem> remove = new ArrayList();
                         for(PmsBookingAddonItem item : room.addons) {
                             if(!PmsBookingRooms.isSameDayStatic(item.date, postingDate)) {
                                 continue;
                             }
                             if(item.productId.equals(productIdInner)) {
-                                toAddonInternal = item;
+//                                toAddonInternal = item;
+                                remove.add(item);
                             }
                             if(item.productId.equals(productIdOuter)) {
-                                toAddonExternal = item;
+//                                toAddonExternal = item;
+                                remove.add(item);
                             }
 
                         }
+                        room.addons.removeAll(remove);
                         
                         if(doNot) {
                             continue;
                         }
-                        
                         if(res.inner == 0 && res.outer == 0) {
                             continue;
                         }

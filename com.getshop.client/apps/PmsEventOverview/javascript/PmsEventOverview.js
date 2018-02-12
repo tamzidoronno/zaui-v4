ae30f3d27369cdaa435bd753206eff9fc4375e27
vebjobb <vebjobb@VebJobb>
2018-02-12 14:40:18 +0100

    Design changes for PmsEventOverview

--- com.getshop.client/apps/PmsEventOverview/javascript/PmsEventOverview.js
+++ com.getshop.client/apps/PmsEventOverview/javascript/PmsEventOverview.js
@@ -1,9 +1,33 @@
 app.PmsEventOverview = {
     init : function() {
-        $(document).on('click', 'PmsEventOverview .button', app.PmsEventOverview.showMessage)
+        $(document).on('click', '.PmsEventOverview .calendarSettings .fa-cog', app.PmsEventOverview.openSettings);
+        $(document).on('click', '.PmsEventOverview #calendarConfiguration li', app.PmsEventOverview.toggleVisibility);
     },
-    showMessage : function() {
-        console.log('Message logged');
+    openSettings : function() {
+        $(this).toggleClass('active');
+        $('#calendarConfiguration').toggle();
+    },
+    toggleVisibility : function(){
+        var btn = $(this);
+        if(btn.hasClass('toggleborders')){
+            btn.toggleClass('active');
+            $('.timeblock').toggleClass('timeblockBorderClass');
+        } else if(btn.hasClass('toggleoccupied')){
+            btn.toggleClass('active');
+            $('.PmsEventOverview .occupied').toggleClass('hidden');
+        } else if(btn.hasClass('togglenotconfirmed')){
+            btn.toggleClass('active');
+            $('.PmsEventOverview .notconfirmed').toggleClass('hidden');
+        } else if(btn.hasClass('toggleopenforpublic')){
+            btn.toggleClass('active');
+            $('.PmsEventOverview .openforpublic').toggleClass('hidden');
+        } else if(btn.hasClass('toggleavailable')){
+            btn.toggleClass('active');
+            $('.PmsEventOverview .available').toggleClass('hidden');
+        } else if(btn.hasClass('togglenotavailable')){
+            btn.toggleClass('active');
+            $('.PmsEventOverview .not_available').toggleClass('hidden');
+        }
     },
     loadSettings: function (element, application) {
         var config = {

dae5c10f51c9417b260f343a20c12b4e4ab94152
boggi <boggi@boggi-ThinkPad-W550s>
2018-02-20 10:14:08 +0100

    fix for embedding code.

--- com.getshop.client/ROOT/js/getshop.bookingembed.js
+++ com.getshop.client/ROOT/js/getshop.bookingembed.js
@@ -2,6 +2,9 @@
     $.fn.getshopbooking = function( options ) {
         var box = $(this);
         getshop_endpoint = options.endpoint;
+        getshop_viewmode = options.viewmode;
+        getshop_terminalid = options.terminalid;
+        localStorage.setItem('getshop_endpoint',options.endpoint);
         
         $.ajax({
             "type": "get",
@@ -17,6 +20,7 @@
 (function ( $ ) {
         $.fn.getshopbookingfront = function( options ) {
         var box = $(this);
+        localStorage.setItem('getshop_endpoint',options.endpoint);
         getshop_endpoint = options.endpoint;
         getshop_urltonextpage = options.nextpage;
         $.ajax({
@@ -31,3 +35,27 @@
     };
 
 }( jQuery ));
+
+$('<link/>', {
+   rel: 'stylesheet',
+   type: 'text/css',
+   href: 'https://s3.amazonaws.com/icomoon.io/135206/GetShopIcons/style.css?tyxklk'
+}).appendTo('head');
+
+$('<link/>', {
+   rel: 'stylesheet',
+   type: 'text/css',
+   href: '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'
+}).appendTo('head');
+
+$('<link/>', {
+   rel: 'stylesheet',
+   type: 'text/css',
+   href: 'https://www.getshop.com/scripts/booking/bookingstyles.php'
+}).appendTo('head');
+
+$('<link/>', {
+   rel: 'stylesheet',
+   type: 'text/css',
+   href: 'https://www.getshop.com/js/daterangepicker/daterangepicker.css'
+}).appendTo('head');

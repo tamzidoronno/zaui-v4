a0175a53b79161c336b30cae80288f9618eead4a
boggi <boggi@boggi-ThinkPad-W550s>
2018-03-15 09:42:43 +0100

    make sure it runs

--- core/src/main/java/com/thundashop/core/wubook/WubookManager.java
+++ core/src/main/java/com/thundashop/core/wubook/WubookManager.java
@@ -107,7 +107,7 @@ public class WubookManager extends GetShopSessionBeanNamed implements IWubookMan
     
     @Override
     public boolean updateAvailability() throws Exception {
-        return updateAvailabilityInternal(370);
+        return updateAvailabilityInternal(730);
     }
     
     private boolean isWubookActive() { 

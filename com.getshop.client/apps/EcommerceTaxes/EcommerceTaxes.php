91b470cb172d912bf6c7e2e74164badbf4b37966
boggi <boggi@boggi-ThinkPad-W550s>
2018-03-27 13:14:08 +0200

    support for configuration taxes.

---
+++ com.getshop.client/apps/EcommerceTaxes/EcommerceTaxes.php
@@ -0,0 +1,34 @@
+<?php
+namespace ns_d0c34aa6_36c7_40dd_9ef2_4fa8844f442d;
+
+class EcommerceTaxes extends \WebshopApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "EcommerceTaxes";
+    }
+
+    public function render() {
+        $this->includefile("taxconfig");
+    }
+    
+    public function saveTaxes() {
+        $result = array();
+        $grp = new \core_productmanager_data_TaxGroup();
+        $grp->groupNumber = 0;
+        $grp->taxRate = 0;
+        $result[] = $grp;
+
+        for ($i = 1; $i <= 5; $i++) {
+            $grp = new \core_productmanager_data_TaxGroup();
+            $grp->groupNumber = $i;
+            $grp->taxRate = $_POST['data']['group'.$i];
+            $result[] = $grp;
+        }
+        
+        $this->getApi()->getProductManager()->setTaxes($result);
+    }
+}
+?>

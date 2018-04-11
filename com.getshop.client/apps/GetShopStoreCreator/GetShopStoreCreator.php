f293c8a73e5b72880dd8c3f16ae0ecfeb37e963b
ktonder <ktonder@ktonder-ThinkPad-P51s>
2018-04-11 15:12:20 +0200

    Finished GetShop Store Creator

--- com.getshop.client/apps/GetShopStoreCreator/GetShopStoreCreator.php
+++ com.getshop.client/apps/GetShopStoreCreator/GetShopStoreCreator.php
@@ -11,7 +11,13 @@ class GetShopStoreCreator extends \MarketingApplication implements \Application 
     }
 
     public function render() {
-        $this->includefile("create");
+        if (isset($_POST['event']) && $_POST['event'] == "create") {
+            echo "<div class='thankyou'>";
+            echo $this->getConfigurationSetting("thankyou");
+            echo "</div>";
+        } else {
+            $this->includefile("create");
+        }
     }
     
     public function getTimeZones() {
@@ -40,6 +46,13 @@ class GetShopStoreCreator extends \MarketingApplication implements \Application 
             $this->doError($obj); // Code will stop here.
         }
         
+        if (!filter_var($_POST['data']['email'], FILTER_VALIDATE_EMAIL)) {
+            $obj = $this->getStdErrorObject(); // Get a default error message
+            $obj->fields->errorMessage = "Email is not valid"; // The message you wish to display in the gserrorfield
+            $obj->gsfield->email = 1; // Will highlight the field that has gsname "hours"
+            $this->doError($obj); // Code will stop here.
+        }
+        
         if (!$_POST['data']['email']) {
             $obj = $this->getStdErrorObject(); // Get a default error message
             $obj->fields->errorMessage = "Email can not be blank"; // The message you wish to display in the gserrorfield
@@ -61,6 +74,20 @@ class GetShopStoreCreator extends \MarketingApplication implements \Application 
             $this->doError($obj); // Code will stop here.
         }
         
+        if (!ctype_digit ( $_POST['data']['cellphone'] )) {
+            $obj = $this->getStdErrorObject(); // Get a default error message
+            $obj->fields->errorMessage = "Cellphone must be a number"; // The message you wish to display in the gserrorfield
+            $obj->gsfield->cellphone = 1; // Will highlight the field that has gsname "hours"
+            $this->doError($obj); // Code will stop here.
+        }
+        
+        if (!ctype_digit ( $_POST['data']['prefix'] )) {
+            $obj = $this->getStdErrorObject(); // Get a default error message
+            $obj->fields->errorMessage = "Prefix must be a number, example 47"; // The message you wish to display in the gserrorfield
+            $obj->gsfield->prefix = 1; // Will highlight the field that has gsname "hours"
+            $this->doError($obj); // Code will stop here.
+        }
+        
         $startData = new \core_getshop_data_StartData();
         $startData->email = $_POST['data']['email'];
         $startData->name = $_POST['data']['name'];
@@ -68,6 +95,9 @@ class GetShopStoreCreator extends \MarketingApplication implements \Application 
         $startData->prefix = $_POST['data']['prefix'];
         $startData->emailText = $this->getConfigurationSetting("text");
         $startData->emailSubject = $this->getConfigurationSetting("subject");
+        $startData->currency = $_POST['data']['currency'];
+        $startData->country = $_POST['data']['country'];
+        $startData->timeZone = $_POST['data']['timezone'];
         $this->getApi()->getGetShop()->createNewStore($startData);
     }
     

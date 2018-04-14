6e1410e7dc8e9924ecaa257762a837b4b4e9c8e9
ktonder <ktonder@ktonder-ThinkPad-P51s>
2018-04-14 19:23:53 +0200

    Adding actionpoint to price calculator

--- com.getshop.client/ROOT/scripts/downloadPriceCalculator.php
+++ com.getshop.client/ROOT/scripts/downloadPriceCalculator.php
@@ -10,8 +10,8 @@ header('Content-Type: application/pdf');
 $id = session_id();
 session_write_close();
 
-$url = "http://no.3.0.local.getshop.com/".base64_decode($_GET['link'])."&id=".  $id;
-//$url = "https://no.getshop.com/".base64_decode($_GET['link'])."&id=".  $id;
+//$url = "http://no.3.0.local.getshop.com/".base64_decode($_GET['link'])."&id=".  $id;
+$url = "https://no.getshop.com/".base64_decode($_GET['link'])."&id=".  $id;
 
 $base64 = $factory->getApi()->getGetShop()->getBase64EncodedPDFWebPage($url);
 echo base64_decode($base64);

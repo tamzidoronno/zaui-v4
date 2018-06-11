9fa1cadb979274857b6350960b3fa242d36a6d15
boggi <boggi@boggi-ThinkPad-W550s>
2018-06-11 14:26:57 +0200

    start of the eilerth smith design.

---
+++ com.getshop.client/apps/EilerthSmithTheme/EilerthSmithTheme.php
@@ -0,0 +1,21 @@
+<?php
+namespace ns_4c3d7314_f9c1_4e9c_9eaa_1e37ef29ccb8;
+
+class EilerthSmithTheme extends \ThemeApplication implements \Application {
+    public function getDescription() {
+        
+    }
+
+    public function getName() {
+        return "EilerthSmithTheme";
+    }
+
+    public function render() {
+        
+    }
+
+    public function addScripts() {
+        echo '<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">';
+    }
+}
+?>

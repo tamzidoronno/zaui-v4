e90e2510a4b8cff4a994fd6d585eec94d0be0f13
Kai Tønder <kai@getshop.com>
2018-07-06 09:42:52 +0200

    Sorting correctly

--- core/src/main/java/com/thundashop/core/bookingengine/BookingEngineAbstract.java
+++ core/src/main/java/com/thundashop/core/bookingengine/BookingEngineAbstract.java
@@ -79,9 +79,12 @@ public class BookingEngineAbstract extends GetShopSessionBeanNamed {
         List<BookingItemType> result = new ArrayList(types.values());
         Comparator<BookingItemType> comparator = new Comparator<BookingItemType>() {
             public int compare(BookingItemType c1, BookingItemType c2) {
-                if(c1.name == null || c2.name == null) {
-                    return 0;
+                if(c1.name == null) {
+                    return -1;
                 } 
+                if(c2.name == null) {
+                    return 1;
+		} 
                 return c1.name.compareTo(c2.name); // use your logic
             }
         };

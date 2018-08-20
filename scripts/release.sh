af97753a8dfa737280ef6845c33f7ee812a246a5
ktonder <ktonder@ktonder-ThinkPad-P51s>
2018-08-20 11:59:21 +0200

    Started on kronengruppen server

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,6 +9,7 @@ if [ $releaseAnswer = "1" ]; then
 	echo "What server do you want to release to?"
 	echo "3 = Server 3 ( First server ever created )";
 	echo "4 = Server 4 ( Created: 8 june 2017 )";
+	echo "5 = Server Kronen ( Created: 19 aug 2018 )";
 	read serverQuestion;
 
 	if [ $serverQuestion = "3" ]; then
@@ -16,6 +17,8 @@ if [ $releaseAnswer = "1" ]; then
 		ssh naxa@jenkins.getshop.com '/home/naxa/scripts/release.sh'
 	elif [ $serverQuestion = "4" ]; then
 		ssh naxa@jenkins.getshop.com '/home/naxa/scripts/releaseSoftware.sh 138.201.203.177'
+	elif [ $serverQuestion = "5" ]; then
+		ssh naxa@jenkins.getshop.com '/home/naxa/scripts/releaseSoftware.sh 88.99.1.121'
 	else 
 		echo "Invalid server setup";
 	fi;

4063802b3953be72361701a8be5523f0b4737b16
boggi <boggi@boggi-ThinkPad-W550s>
2018-09-03 08:59:48 +0200

    information message about pms2.0

--- com.getshop.client/ROOT/login.php
+++ com.getshop.client/ROOT/login.php
@@ -165,6 +165,12 @@ if (isset($_POST['pincoderequest']) && $_POST['username'] && $_POST['password'])
                         foreach ($modules as $module) {
                             echo "<a class='gs_ignorenavigate' href='/?changeGetShopModule=" . strtolower($module->id) . "&scopeid=NEW' style='text-decoration:none;'>";
                             echo "<div style='background-color:#fff; border: solid 1px #bbb;margin: auto; width: 200px; padding: 10px;margin-bottom: 5px; cursor:pointer;'><span class='" . $module->fontAwesome . "'></span> " . $module->name . "</div>";
+                            echo "</a>";
+                            if($module->name == "PMS") {
+                                echo "<a class='gs_ignorenavigate' href='/pms.php' style='text-decoration:none;'>";
+                                echo "<div style='background-color:#fff; border: solid 1px #bbb;margin: auto; width: 200px; padding: 10px;margin-bottom: 5px; cursor:pointer;'><span class='" . $module->fontAwesome . "'></span>PMS 2.0</div>";
+                                echo "</a>";
+                            }
                         }
                     } else {
                         echo "<br> Please wait while we are logging you on.</center>";

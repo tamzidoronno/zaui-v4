8eb3fdb22d5fbd8cb588d050357485f9ab2a2e52
ktonder <ktonder@ktonder-ThinkPad-P51s>
2018-10-04 15:06:53 +0200

    New receipt printer functionallity. Dont showing credit card to send to verifone if its not setup Dont show printing possibility for other then cash and verifone

--- messages/src/main/java/com/thundashop/core/gsd/DevicePrintMessage.java
+++ messages/src/main/java/com/thundashop/core/gsd/DevicePrintMessage.java
@@ -5,12 +5,23 @@
  */
 package com.thundashop.core.gsd;
 
+import com.thundashop.core.pdf.data.AccountingDetails;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  *
  * @author ktonder
  */
-public class DevicePrintMessage extends GetShopDeviceMessage implements Serializable {
-    public String payLoad;
+public class DevicePrintMessage extends GetShopDeviceMessage implements Serializable {    
+    public String payLoad = "";
+    public double totalExVat = 0.0;
+    public double totalIncVat = 0.0;
+    public List<ItemLine> itemLines = new ArrayList();
+    public List<VatLine> vatLines = new ArrayList();
+    public AccountingDetails accountDetails;
+    public String paymentMethod;
+    public Date paymentDate;
 }

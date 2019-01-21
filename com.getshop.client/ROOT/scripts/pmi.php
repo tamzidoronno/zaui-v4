220b413e9ea6a1f90761530f77f395cb121b20fa
boggi <boggi@boggi-ThinkPad-W550s>
2019-01-21 18:57:44 +0100

    added segment support and are now using billed on roomrevenue and total on reservations.

--- com.getshop.client/ROOT/scripts/pmi.php
+++ com.getshop.client/ROOT/scripts/pmi.php
@@ -7,35 +7,38 @@ header('Content-Type: application/json');
 $factory->getApi()->getUserManager()->logOn($_GET['username'], $_GET['password']);
 $storeId = $factory->getApi()->getStoreManager()->getMyStore()->id;
 $appBase = new ApplicationBase();
+$segments = $factory->getApi()->getPmsCoverageAndIncomeReportManager()->getSegments($_GET['domain']);
 
 if($_GET['type'] == "roomrevenue") {
     $filter = new core_pmsmanager_PmsBookingFilter();
     $filter->startDate = $appBase->convertToJavaDate(strtotime($_GET['start'] . " 00:00"));
     $filter->endDate = $appBase->convertToJavaDate(strtotime($_GET['end'] . " 23:59"));
-    if(isset($_GET['includevirtual']) && $_GET['includevirtual'] == "1") {
-        $filter->includeVirtual = true;
-    }
-    
-    //Coverage + virtual orders
-    $stats = $factory->getApi()->getPmsManager()->getStatistics($_GET['domain'], $filter);
+    $filter->removeAddonsIncludedInRoomPrice = true;
     $list = array();
-    foreach($stats->entries as $s) {
-        if(!$s->date) { 
-            continue;
+    
+    foreach($segments as $segment) {
+        //Coverage + virtual orders
+        $filter->segments = array();
+        $filter->segments[] = $segment->id;
+
+        $stats = $factory->getApi()->getPmsManager()->getStatistics($_GET['domain'], $filter);
+        foreach($stats->entries as $s) {
+            if(!$s->date) { 
+                continue;
+            }
+            $day = array();
+            $day['propertyid'] = $storeId;
+            $day['date'] = date("d.m.Y", strtotime($s->date));
+            $day['segment'] = $segment->name;
+            $day['roomnights'] = $s->roomsRentedOut;
+            $day['guestnights'] = $s->guestCount;
+            $day['guestnightsadults'] = $s->guestCount;
+            $day['guestnightschildren'] = 0;
+            $day['revenuerooms'] = $s->totalPrice;
+            $day['arrivals'] = $s->arrivals;
+            $day['departures'] = $s->departures;
+            $list[] = $day;
         }
-        $day = array();
-        $day['propertyid'] = $storeId;
-        $day['date'] = date("d.m.Y", strtotime($s->date));
-        $day['code'] = "";
-        $day['segment'] = "Total";
-        $day['roomnights'] = $s->roomsRentedOut;
-        $day['guestnights'] = $s->guestCount;
-        $day['guestnightsadults'] = $s->guestCount;
-        $day['guestnightschildren'] = 0;
-        $day['revenuerooms'] = $s->totalPrice;
-        $day['arrivals'] = $s->arrivals;
-        $day['departures'] = $s->departures;
-        $list[] = $day;
     }
     
     echo json_encode($list);
@@ -74,29 +77,35 @@ if($_GET['type'] == "reservations") {
     $filter = new core_pmsmanager_PmsBookingFilter();
     $filter->startDate = $appBase->convertToJavaDate(strtotime($_GET['start'] . " 00:00"));
     $filter->endDate = $appBase->convertToJavaDate(strtotime($_GET['end'] . " 23:59"));
-    $filter->includeVirtual = true;
 
-    //Coverage + virtual orders
-    $stats = $factory->getApi()->getPmsManager()->getStatistics($_GET['domain'], $filter);
     $list = array();
-    foreach($stats->entries as $s) {
-        if(!$s->date) { 
-            continue;
+    foreach($segments as $segment) {
+        //Coverage + virtual orders
+        $filter->segments = array();
+        $filter->segments[] = $segment->id;
+        $filter->includeVirtual = false;
+        $filter->removeAddonsIncludedInRoomPrice = true;
+        
+        $stats = $factory->getApi()->getPmsManager()->getStatistics($_GET['domain'], $filter);
+    
+        foreach($stats->entries as $s) {
+            if(!$s->date) { 
+                continue;
+            }
+            $day = array();
+            $day['propertyid'] = $storeId;
+            $day['snapshotdate'] = date("d.m.Y", time());
+            $day['date'] = date("d.m.Y", strtotime($s->date));
+            $day['segment'] = $segment->name;
+            $day['roomnights'] = $s->roomsRentedOut;
+            $day['guestnights'] = $s->guestCount;
+            $day['guestnightsadults'] = $s->guestCount;
+            $day['guestnightschildren'] = 0;
+            $day['revenuerooms'] = $s->totalForcasted;
+            $day['arrivals'] = $s->arrivals;
+            $day['departures'] = $s->departures;
+            $list[] = $day;
         }
-        $day = array();
-        $day['propertyid'] = $storeId;
-        $day['snapshotdate'] = date("d.m.Y", time());
-        $day['date'] = date("d.m.Y", strtotime($s->date));
-        $day['code'] = "";
-        $day['segment'] = "Total";
-        $day['roomnights'] = $s->roomsRentedOut;
-        $day['guestnights'] = $s->guestCount;
-        $day['guestnightsadults'] = $s->guestCount;
-        $day['guestnightschildren'] = 0;
-        $day['revenuerooms'] = $s->totalPrice;
-        $day['arrivals'] = $s->arrivals;
-        $day['departures'] = $s->departures;
-        $list[] = $day;
     }
     
     echo json_encode($list);

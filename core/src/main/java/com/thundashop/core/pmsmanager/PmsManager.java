6b03ddc8996de0050cf8c6db016822882188a773
ktonder <ktonder@ktonder-ThinkPad-P51s>
2019-03-05 21:50:55 +0100

    Avoiding nullpointer exception

--- core/src/main/java/com/thundashop/core/pmsmanager/PmsManager.java
+++ core/src/main/java/com/thundashop/core/pmsmanager/PmsManager.java
@@ -9683,6 +9683,10 @@ public class PmsManager extends GetShopSessionBeanNamed implements IPmsManager {
     public List<String> getAllOrderIds(String pmsBookingId) {
         PmsBooking booking = getBooking(pmsBookingId);
         
+        if (booking == null) {
+            return new ArrayList();
+        }
+        
         List<String> orderIds = getExtraOrderIds(pmsBookingId);
         orderIds.addAll(booking.orderIds);
         

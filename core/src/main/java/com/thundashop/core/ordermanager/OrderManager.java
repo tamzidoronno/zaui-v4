b529ca3408b900b64f84ef2527267d02ec864758
ktonder <ktonder@ktonder-ThinkPad-P51s>
2019-03-11 13:58:49 +0100

    Reverted back possibility to reset a month

--- core/src/main/java/com/thundashop/core/ordermanager/OrderManager.java
+++ core/src/main/java/com/thundashop/core/ordermanager/OrderManager.java
@@ -2843,32 +2843,23 @@ public class OrderManager extends ManagerBase implements IOrderManager {
 
     @Override
     public void resetLastMonthClose(String password, Date start, Date end) {
-        if (password != null && password.equals("adfs9a9087293451q2oi4h1234khakslhfasidurh23")) {
+       if (password != null && password.equals("adfs9a9087293451q2oi4h1234khakslhfasidurh23")) {
             List<DayIncome> dayIncomes = getDayIncomes(start, end);
-            
-            DayIncomeFilter filter = new DayIncomeFilter();
-            filter.start = start;
-            filter.end = end;
-            OrderDailyBreaker breaker = new OrderDailyBreaker(getAllOrders(), filter, paymentManager, productManager, getOrderManagerSettings().whatHourOfDayStartADay);
-            breaker.breakOrders();
-            List<DayIncome> newIncomes = breaker.getDayIncomes();
             
             for (DayIncome income : dayIncomes) {
                 BasicDBObject query = new BasicDBObject();
                 query.put("incomes.id", income.id);
                 List<DataCommon> datas = database.query("OrderManager", storeId, query);
-                datas.stream()
-                        .map(o -> (DayIncomeReport)o)
-                        .forEach(o -> {
-                            o.replaceDayIncomes(newIncomes);
-                            saveObject(o);
-                        });
+                datas.stream().forEach(o -> {
+                    System.out.println("Deleted report");
+                    deleteObject(o);
+                });
             }
             
-//            OrderManagerSettings settings = getOrderManagerSettings();
-//            settings.closedTilPeriode = start;
-//
-//            saveObject(settings);
+            OrderManagerSettings settings = getOrderManagerSettings();
+            settings.closedTilPeriode = start;
+
+            saveObject(settings);
         }
     }
     

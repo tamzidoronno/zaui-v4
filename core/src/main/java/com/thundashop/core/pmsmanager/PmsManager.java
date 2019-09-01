7b4cca61f980a9e9225ee54ee1aeeaf00361d50f
boggi <boggi@boggi-ThinkPad-W550s>
2019-09-01 09:08:41 +0200

    only readd default addons if they are added to the room already.

--- core/src/main/java/com/thundashop/core/pmsmanager/PmsManager.java
+++ core/src/main/java/com/thundashop/core/pmsmanager/PmsManager.java
@@ -8574,7 +8574,7 @@ public class PmsManager extends GetShopSessionBeanNamed implements IPmsManager {
                     continue;
                 }
                 if (room.bookingItemTypeId != null) {
-                    if (item.includedInBookingItemTypes.contains(room.bookingItemTypeId) || item.alwaysAddAddon) {
+                    if (item.includedInBookingItemTypes.contains(room.bookingItemTypeId) || item.alwaysAddAddon || (room.hasAddonOfProduct(item.productId) && !item.isSingle)) {
                         int size = 1;
                         if (item.dependsOnGuestCount) {
                             size = room.numberOfGuests;

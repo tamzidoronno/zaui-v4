61bcbca6c788db3affd320794231a354281efbcd
boggi <boggi@getshop-boggi>
2019-09-16 20:21:01 +0200

    Better backup system

--- com.getshop.client/ROOT/scripts/getbackupstatus.php
+++ com.getshop.client/ROOT/scripts/getbackupstatus.php
@@ -4,7 +4,7 @@ include '../loader.php';
 $factory = IocContainer::getFactorySingelton();
 
 $url = "http://system.3.0.local.getshop.com/scripts/systembackupstatus.php?id=" . $_GET['id'];
-if($this->getApi()->getStoreManager()->isProductMode()) {
+if($factory->getApi()->getStoreManager()->isProductMode()) {
     $url = "https://system.getshop.com/scripts/systembackupstatus.php?id=" . $_GET['id'];
 }
 $content = file_get_contents($url);

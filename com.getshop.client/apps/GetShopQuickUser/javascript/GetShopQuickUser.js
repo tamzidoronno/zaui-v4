4e3d8d92164886a02a6648fc41c5460061fb2050
boggi <boggi@getshop-boggi>
2019-10-06 17:21:49 +0200

    timezone problem related to update prices.

--- com.getshop.client/apps/GetShopQuickUser/javascript/GetShopQuickUser.js
+++ com.getshop.client/apps/GetShopQuickUser/javascript/GetShopQuickUser.js
@@ -7,6 +7,17 @@ app.GetShopQuickUser = {
         $(document).on('click', '.GetShopQuickUser .searchbox .tab', this.changeTab);
         $(document).on('click','.GetShopQuickUser .searchbrregbutton', app.GetShopQuickUser.showBrregSearch);
         $(document).on('click','.GetShopQuickUser .brregsearchresultrow', app.GetShopQuickUser.selectBrregResult);
+        $(document).on('keyup','.GetShopQuickUser .newuserinputfield', app.GetShopQuickUser.searchExistingCustomer);
+    },
+    
+    searchExistingCustomer : function() {
+        var args = thundashop.framework.createGsArgs($("[method='searchForUsers']"));
+        args.searchvalue = $(this).val();
+        
+        var event = thundashop.Ajax.createEvent('','searchForUsers', $(this),args);
+        thundashop.Ajax.postWithCallBack(event, function(res) {
+            $('.GetShopQuickUser .change_user_form .searchresult').html(res);
+        });
     },
     editUserSaveButton : function() {
         var form = $(this).closest("[gstype='form']");

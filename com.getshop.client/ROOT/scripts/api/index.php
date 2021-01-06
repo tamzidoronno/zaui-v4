e68e5502c40e3ce39a9c171733888dc23c87cfba
Thomas W <thomas@getshop.com>
2021-01-06 14:38:11 +0000

    Headers for better cross site functionality added...

--- com.getshop.client/ROOT/scripts/api/index.php
+++ com.getshop.client/ROOT/scripts/api/index.php
@@ -6,7 +6,12 @@
  * valid authentication in login.php
  */
 
+//some custom response headers
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
 chdir('../../');
 include '../loader.php';
--- com.getshop.client/ROOT/scripts/api/login.php
+++ com.getshop.client/ROOT/scripts/api/login.php
@@ -3,7 +3,12 @@
  * This file is based on api.php form the parent folder. all it is supposed to do is login the user and return its data or an error.
  */
 
+//some custom response headers
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
 chdir('../../');
 include '../loader.php';

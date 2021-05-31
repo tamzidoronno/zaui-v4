13732104b0b0325c2e6779acff6715b6b2bf4679
sourov <atequer@getshop.com>
2021-05-31 11:46:51 +0600

    log added for debugging purpose.

--- core/src/main/java/com/thundashop/core/common/ZReportProcessor.java
+++ core/src/main/java/com/thundashop/core/common/ZReportProcessor.java
@@ -20,6 +20,7 @@ public class ZReportProcessor extends GetShopSchedulerBase {
         try {
             PmsConfiguration pmsConfiguration = getApi().getPmsManager().getConfiguration(multilevelname);
             String scheduledTimeForZReport = pmsConfiguration.zReportProcessingTime;
+            System.out.println("scheduledTimeForZReport: "+scheduledTimeForZReport);
             if (StringUtils.nonNull(scheduledTimeForZReport) && scheduledTimeForZReport.equals(DateUtils.getCurrentTime())) {
                 getApi().getPosManager().createZReport("");
             }
--- core/src/main/java/com/thundashop/core/utils/DateUtils.java
+++ core/src/main/java/com/thundashop/core/utils/DateUtils.java
@@ -7,6 +7,7 @@ import java.time.format.DateTimeFormatter;
 public class DateUtils {
 
     public static String getCurrentTime() {
+        System.out.println("DateUtils.getCurrentTime: "+DateTimeFormatter.ofPattern("HH:mm").format(LocalDateTime.now().atZone(ZoneId.systemDefault())));
         return DateTimeFormatter.ofPattern("HH:mm").format(LocalDateTime.now().atZone(ZoneId.systemDefault()));
     }
 }

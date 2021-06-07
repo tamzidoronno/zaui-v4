2b7380dbe66defedd644382a19ce43d8eaa1e6c6
muztaba <muztaba@cefalo.com>
2021-06-07 18:45:11 +0600

    [GD-92] Introduce logback

--- messages/src/main/java/com/thundashop/core/common/GetShopLogHandler.java
+++ messages/src/main/java/com/thundashop/core/common/GetShopLogHandler.java
@@ -1,57 +1,24 @@
 
 package com.thundashop.core.common;
 
-import java.io.File;
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class GetShopLogHandler {
     public static boolean isDeveloper = false;
     
-    public static List<String> started = new ArrayList();
-    public static List<String> authenticationError = new ArrayList();
-    
+    public static List<String> started = new ArrayList<>();
+    public static List<String> authenticationError = new ArrayList<>();
+
+    public static final Logger log = LoggerFactory.getLogger(GetShopLogHandler.class);
+
     public static void logPrintStatic(Object key, String storeId) {
-        if (key == null)
-            return;
-        
-        if (isDeveloper) {
-            System.out.println(key);
-            return;
-        }
-        
-        try {
-            createLogPath();
-            String storeIdToUse = storeId == null || storeId.isEmpty() ? "all" : storeId;           
-            String fileName = "log/"+storeIdToUse+".log";
-            
-            File logFileName = new File(fileName);
-            if (!logFileName.exists()) {
-                logFileName.createNewFile(); 
-            }
-
-            String textToLog = new Date().toString() + "\t" + key + "\n";
-            Files.write(Paths.get(fileName), textToLog.getBytes(), StandardOpenOption.APPEND);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            System.out.println("Cant log... check log access etc");
-        }
-        
-    }
-
-    private static void createLogPath() throws IOException {
-        Path logPath = Paths.get("log");
-        if (Files.notExists(logPath)) {
-            Files.createDirectories(logPath);
-        }
+        log.info("storeId `{}` {}", storeId, key);
     }
 
     public static void logPrintStaticSingle(Object key, String storeId) {

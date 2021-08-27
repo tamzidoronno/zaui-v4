1a2700caaf4a622461b1851b04685f113561e6db
muztaba <muztaba@cefalo.com>
2021-08-27 13:49:59 +0600

    [GD-184] Add yes-no input in pmslog-delete.sh

--- scripts/pmslog-migration/pmslog-delete.sh
+++ scripts/pmslog-migration/pmslog-delete.sh
@@ -18,13 +18,22 @@ readonly COLLECTIONS=$(echo "db.getMongo().getDB(\"${PMS_MANAGER}\").getCollecti
 
 echo "Going to delete from collections: ${COLLECTIONS}"
 
+while true; do
+    read -p "Really want to delete? Think again..." yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 for it in $COLLECTIONS; do
 
   echo -e "\n ********************************** \n"
   countQuery="db.getMongo().getDB(\"${PMS_MANAGER}\").getCollection('${it}').find(${FILTER_CONDITION}).count()"
   echo "$(echo $countQuery | mongo --port $port --quiet) PmsLog will be deleted from ${it}"
 
-  deleteQuery="db.getMongo().getDB(\"${PMS_MANAGER}\").getCollection('${it}').deleteMany(${FILTER_CONDITION})"
-  echo $deleteQuery | mongo --port $port --quiet
+#  deleteQuery="db.getMongo().getDB(\"${PMS_MANAGER}\").getCollection('${it}').deleteMany(${FILTER_CONDITION})"
+#  echo $deleteQuery | mongo --port $port --quiet
 
 done

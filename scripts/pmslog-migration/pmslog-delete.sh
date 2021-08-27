4a3c06fc216338bc3731ea454699e78afa8a13e9
muztaba <muztaba@cefalo.com>
2021-08-27 14:25:28 +0600

    [GD-184] Uncomment delete code pmslog-delete.sh

--- scripts/pmslog-migration/pmslog-delete.sh
+++ scripts/pmslog-migration/pmslog-delete.sh
@@ -33,7 +33,7 @@ for it in $COLLECTIONS; do
   countQuery="db.getMongo().getDB(\"${PMS_MANAGER}\").getCollection('${it}').find(${FILTER_CONDITION}).count()"
   echo "$(echo $countQuery | mongo --port $port --quiet) PmsLog will be deleted from ${it}"
 
-#  deleteQuery="db.getMongo().getDB(\"${PMS_MANAGER}\").getCollection('${it}').deleteMany(${FILTER_CONDITION})"
-#  echo $deleteQuery | mongo --port $port --quiet
+  deleteQuery="db.getMongo().getDB(\"${PMS_MANAGER}\").getCollection('${it}').deleteMany(${FILTER_CONDITION})"
+  echo $deleteQuery | mongo --port $port --quiet
 
 done

a4d82959c7e98c5e66b3ad1321b051d5eaaecd75
muztaba <muztaba@cefalo.com>
2021-09-13 17:17:39 +0600

    [GD-177] Read storeCreationIp from config
    If the `config.txt` file contain `storeCreationIp` and `production == true`, then and only then the database for store creation will be configured with the provided ip. Otherwise DB host will be set to "localhost".

--- core/src/main/java/com/thundashop/core/config/DatabaseConfig.java
+++ core/src/main/java/com/thundashop/core/config/DatabaseConfig.java
@@ -5,12 +5,17 @@ import com.thundashop.core.databasemanager.Database3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+
 @Configuration
 public class DatabaseConfig {
 
     @Bean(name = "storeIdDb")
     public Database3 storeIdDd(FrameworkConfig frameworkConfig) {
-        return new Database3("10.0.9.33", 27018);
+        String host = isNotEmpty(frameworkConfig.getStoreCreationIP()) && frameworkConfig.productionMode
+                ? frameworkConfig.getStoreCreationIP() : "localhost";
+
+        return new Database3(host, 27018);
     }
 
 }

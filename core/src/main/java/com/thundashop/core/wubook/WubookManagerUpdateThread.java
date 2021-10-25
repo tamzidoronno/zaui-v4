dd005830fa024980f2b3b8f77da8b7e6021c4cb6
muztaba <muztaba@cefalo.com>
2021-10-25 16:53:46 +0600

    [GD-298] Replace logging with SLF4J

--- core/src/main/java/com/thundashop/core/wubook/WubookManagerUpdateThread.java
+++ core/src/main/java/com/thundashop/core/wubook/WubookManagerUpdateThread.java
@@ -1,26 +1,36 @@
 package com.thundashop.core.wubook;
 
 import org.apache.xmlrpc.XmlRpcClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.util.StopWatch;
 
+import java.util.Map;
 import java.util.Vector;
 import java.util.concurrent.*;
 
 public class WubookManagerUpdateThread extends Thread {
 
+    private static final Logger logger = LoggerFactory.getLogger(WubookManagerUpdateThread.class);
+
     private final Vector params;
     private final WubookManager mgr;
     private final XmlRpcClient client;
     private final String action;
+    private final Map<String, String> mdcContext;
 
-    WubookManagerUpdateThread(String action, XmlRpcClient client, WubookManager mgr, Vector params) {
+    WubookManagerUpdateThread(String action, XmlRpcClient client, WubookManager mgr, Vector params, Map<String, String> mdcContext) {
         this.client = client;
         this.mgr = mgr;
         this.params = params;
         this.action = action;
+        this.mdcContext = mdcContext;
     }
     
     @Override
     public void run() {
+        MDC.setContextMap(mdcContext);
         mgr.logPrint(Thread.currentThread().getName() + " " + getClass() + " " + "Starting thread...");
 
         ExecutorService executor = Executors.newFixedThreadPool(1);
@@ -29,9 +39,14 @@ public class WubookManagerUpdateThread extends Thread {
 
         Vector result;
         try {
-            mgr.logPrint(Thread.currentThread().getName() + " " + getClass() + "Calling wubookManger api, apiCall: " + action + " params: " + params);
+            logger.info("Calling wubookManger api, apiCall: {} , params: {}", action, params);
+            StopWatch stopWatch = new StopWatch("Api Call: " + action);
+            stopWatch.start();
+
             result = taskFuture.get(3, TimeUnit.MINUTES);
-            mgr.logPrint(getClass() + "Response from wubookManager api, apiCall: " + action + " response: " + result);
+
+            stopWatch.stop();
+            logger.info("Executed api: {} , time: {} , response: {}", action, stopWatch, result);
             if ((Integer)result.get(0) != 0) {
                 mgr.logText("Failed to update availability (" + result.get(0) + ") " + result.get(1) + " Parameters sent: " + params.toString() );
             } else {
@@ -39,10 +54,12 @@ public class WubookManagerUpdateThread extends Thread {
             }
         } catch (Exception ex) {
             mgr.logText(Thread.currentThread().getName() + " " + getClass() + " Exception : " + ex.getMessage());
+            logger.error("Exception while calling wubook api: {}", action, ex);
             mgr.messageManager.sendErrorNotification(Thread.currentThread().getName() + " " + getClass() + " params: " + params + " Exception " + ex.getMessage(), ex);
         } finally {
             taskFuture.cancel(true);
             executor.shutdownNow();
+            mdcContext.forEach((k, v) -> MDC.remove(k));
         }
 
     }

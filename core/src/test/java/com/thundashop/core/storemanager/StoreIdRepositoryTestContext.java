aee5308d3ed65cc22c99f616d15b8b9919b24c4d
muztaba <muztaba@cefalo.com>
2021-11-17 03:06:08 +0600

    [GD-331] Fix test

--- core/src/test/java/com/thundashop/core/storemanager/StoreIdRepositoryTestContext.java
+++ core/src/test/java/com/thundashop/core/storemanager/StoreIdRepositoryTestContext.java
@@ -1,14 +1,20 @@
 package com.thundashop.core.storemanager;
 
+import com.thundashop.core.databasemanager.MongoClientProviderImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.UnknownHostException;
 
 @Configuration
 public class StoreIdRepositoryTestContext {
 
     @Bean
-    public ExtendedDatabase3 database3() {
-        return new ExtendedDatabase3("localhost", 27019);
+    public ExtendedDatabase3 database3() throws UnknownHostException {
+        return new ExtendedDatabase3(MongoClientProviderImpl.builder()
+                .setHost("localhost")
+                .setPort(27019)
+                .build());
     }
 
 }

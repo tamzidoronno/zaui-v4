0294e0c5dedb8cd9fb30b864360c7c097d602aa4
tamzid_cefalo <tamzid@cefalo.com>
2022-03-09 14:51:02 +0600

    default timezone set to Oslo

--- messages/src/main/java/com/thundashop/core/storemanager/data/Store.java
+++ messages/src/main/java/com/thundashop/core/storemanager/data/Store.java
@@ -151,6 +151,8 @@ public class Store extends DataCommon {
     public void setTimeZone(String timezone) {
         this.timeZone = timezone;
     }
-    public String getTimeZone() {return this.timeZone;}
+    public String getTimeZone() {
+        return !timeZone.isEmpty() ? this.timeZone : "Europe/Oslo";
+    }
     
 }

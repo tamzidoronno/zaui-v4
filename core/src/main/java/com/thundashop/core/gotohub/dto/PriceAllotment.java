aec2d97938fb7ab353025c47c04d3f672fe22e22
Asma Chowdhury <asma@cefalo.com>
2022-05-09 00:14:37 +0600

    add PriceAllotment dto

---
+++ core/src/main/java/com/thundashop/core/gotohub/dto/PriceAllotment.java
@@ -0,0 +1,10 @@
+package com.thundashop.core.gotohub.dto;
+
+public class PriceAllotment {
+    public String start_Date="";
+    public String end_date="";
+    public String room_type_code="";
+    public String rate_plan_code="";
+    public int allotment=0;
+    public double price=0.1;
+}

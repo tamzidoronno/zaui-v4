103b26676f9a2b11fc560c743f0b38ba9e9733ea
Asma Chowdhury <asma@cefalo.com>
2022-05-22 03:16:28 +0600

    add permenantly delete annotation

--- core/src/main/java/com/thundashop/core/jomres/JomresBookingData.java
+++ core/src/main/java/com/thundashop/core/jomres/JomresBookingData.java
@@ -1,7 +1,9 @@
 package com.thundashop.core.jomres;
 
 import com.thundashop.core.common.DataCommon;
+import com.thundashop.core.common.PermenantlyDeleteData;
 
+@PermenantlyDeleteData
 public class JomresBookingData extends DataCommon {
     String pmsBookingId = "";
     long jomresBookingId=0;

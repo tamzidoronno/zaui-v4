e6b6cbdaa12152456c34dd462942fa8afd8f059f
Asma Chowdhury <asma@cefalo.com>
2022-05-22 02:26:18 +0600

    add jomres room data for JomresProperty<>PmsRoom mapping

---
+++ core/src/main/java/com/thundashop/core/jomres/JomresRoomData.java
@@ -0,0 +1,13 @@
+package com.thundashop.core.jomres;
+
+import com.thundashop.core.common.DataCommon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JomresRoomData extends DataCommon {
+    String bookingItemId = "";
+    String bookingItemTypeId="";
+    int jomresPropertyId;
+    List<Integer> jomresRoomIds = new ArrayList<Integer>();
+}

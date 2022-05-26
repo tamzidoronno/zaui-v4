a0c841836fb11435d4c5a3f7e406d86c524ddba9
Asma Chowdhury <asma@cefalo.com>
2022-05-26 19:05:41 +0600

    change naming convention

--- core/src/main/java/com/thundashop/core/jomres/Constants.java
+++ core/src/main/java/com/thundashop/core/jomres/Constants.java
@@ -3,20 +3,15 @@ package com.thundashop.core.jomres;
 import com.thundashop.core.common.DataCommon;
 
 public class Constants extends DataCommon {
-    protected String CMF_CLIENT_TOKEN_URL = "https://jomres.zauistay.com/jomres/api/";
-    protected String CLIENT_BASE_URL = "https://jomres.zauistay.com/jomres/api/";
+    String CMF_CLIENT_TOKEN_URL = "https://jomres.zauistay.com/jomres/api/";
+    String CLIENT_BASE_URL = "https://jomres.zauistay.com/jomres/api/";
 
-    protected String ADMIN_CLIENT_ID = "system";
-    protected String ADMIN_CLIENT_SECRET = "DpnPPlSlmbnXLLbxNaioNqDJQwidlCdubqTnlRUHWpsznVgIqo";
-    protected String JOMRES_CMF_REST_API_CLIENT_ID = "xUirogUaOFQFOQF";
-    protected String JOMRES_CMF_REST_API_CLIENT_SECRET = "HdZDhHbHPxGXjNhZsQSLDDvbrcgLgaDXfGhfyawUJNbbLALtxs";
+    String ADMIN_CLIENT_ID = "system";
+    String ADMIN_CLIENT_SECRET = "DpnPPlSlmbnXLLbxNaioNqDJQwidlCdubqTnlRUHWpsznVgIqo";
+    String JOMRES_CMF_REST_API_CLIENT_ID = "xUirogUaOFQFOQF";
+    String JOMRES_CMF_REST_API_CLIENT_SECRET = "HdZDhHbHPxGXjNhZsQSLDDvbrcgLgaDXfGhfyawUJNbbLALtxs";
 
-    protected int CMS_USER_ID = 1743;
-    protected String CHANNEL_NAME = "test_channel";
-    protected int CHANNEL_ID = 1;
-    protected int HOTEL_PROPERTY_TYPE_ID = 1;
-    protected int Ferienhaus_PROPERTY_TYPE_ID = 6;
-    protected int Ferienhaus_inklusive_Motorboot_PROPERTY_TYPE_ID = 8;
-    protected int Ferienhaus_Motorboot_mietbar_PROPERTY_TYPE_ID = 9;
-    protected int Ferienwohnung_PROPERTY_TYPE_ID = 7;
+    int CMS_USER_ID = 1743;
+    String CHANNEL_NAME = "test_channel";
+    int CHANNEL_ID = 1;
 }
--- core/src/main/java/com/thundashop/core/jomres/services/Constants.java
+++ core/src/main/java/com/thundashop/core/jomres/services/Constants.java
@@ -1,14 +1,17 @@
 package com.thundashop.core.jomres.services;
 
 public class Constants {
-    protected static String CHANNEL_ANNOUNCEMENT_URL = "cmf/channel/announce/";
-    protected static String GET_PROPERTY_IDS_URL_THROUGH_CHANNEL = "cmf/properties/ids/";
-    protected static String GET_PROPERTY_IDS_URL = "properties/all/";
-    protected static String LIST_BOOKING_URL = "listbooking/";
-    protected static String BOOKING_DETAILS_BY_ID = "cmf/property/booking/";
-    protected static String MAKE_PROPERTY_UNAVAILABLE = "cmf/property/availability/";
-    protected static String AVAILABILITY_SENDING_DATE_FORMAT = "yyyy-MM-dd";
-    protected static String PRICE_GETTING_DATE_FORMAT = "yyyy-MM-dd";
-    protected static String PRICE_GETTING_URL = "cmf/property/list/prices/";
+    static String CHANNEL_ANNOUNCEMENT_URL = "cmf/channel/announce/";
+    static String GET_PROPERTY_IDS_URL_THROUGH_CHANNEL = "cmf/properties/ids/";
+    static String GET_PROPERTY_IDS_URL = "properties/all/";
+    static String LIST_BOOKING_URL = "listbooking/";
+    static String BOOKING_DETAILS_BY_ID = "cmf/property/booking/";
+    static String MAKE_PROPERTY_UNAVAILABLE = "cmf/property/availability/";
+    public static String AVAILABILITY_SENDING_DATE_FORMAT = "yyyy-MM-dd";
+    public static String ARRIVAL_DEPARTURE_IN_LIST_DATE_FORMAT = "yyyy/MM/dd";
+    public static String ARRIVAL_DEPARTURE_IN_DETAILS_DATE_FORMAT = "yyyy-MM-dd";
+    public static String CREATED_MODIFIED_IN_DETAILS_DATE_FORMAT = "yyyy-MM-dd' 'HH:mm:ss";
+    public static String PRICE_GETTING_DATE_FORMAT = "yyyy-MM-dd";
+    static String PRICE_GETTING_URL = "cmf/property/list/prices/";
 }
 

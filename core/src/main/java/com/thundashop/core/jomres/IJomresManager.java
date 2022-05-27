74a142aaf78f7104d17cdd26442afbda94529ba1
Asma Chowdhury <asma@cefalo.com>
2022-05-27 12:40:56 +0600

    change method signature

--- core/src/main/java/com/thundashop/core/jomres/IJomresManager.java
+++ core/src/main/java/com/thundashop/core/jomres/IJomresManager.java
@@ -1,3 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package com.thundashop.core.jomres;
 
 import com.thundashop.core.common.Administrator;

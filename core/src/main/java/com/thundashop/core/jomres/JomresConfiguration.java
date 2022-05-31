99f2c5f207e19f1fefa727fc430a9792cc1e8265
Asma Chowdhury <asma@cefalo.com>
2022-05-31 18:50:01 +0600

    change variable name according to convention, remove admin credentials

--- core/src/main/java/com/thundashop/core/jomres/JomresConfiguration.java
+++ core/src/main/java/com/thundashop/core/jomres/JomresConfiguration.java
@@ -3,14 +3,10 @@ package com.thundashop.core.jomres;
 import com.thundashop.core.common.DataCommon;
 
 public class JomresConfiguration extends DataCommon {
-    String CMF_CLIENT_TOKEN_URL = "";
-    String CLIENT_BASE_URL = "";
-
-    String ADMIN_CLIENT_ID = "";
-    String ADMIN_CLIENT_SECRET = "";
-    String JOMRES_CMF_REST_API_CLIENT_ID = "";
-    String JOMRES_CMF_REST_API_CLIENT_SECRET = "";
-
-    int CMS_USER_ID = 0;
-    String CHANNEL_NAME = "";
+    String cmfClientTokenUrl = "";
+    String clientBaseUrl = "";
+    String jomresCmfRestApiClientId = "";
+    String jomresCmfRestApiClientSecret = "";
+    int cmsUserId = 0;
+    String channelName = "";
 }

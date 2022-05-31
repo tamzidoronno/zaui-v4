812da6fbdcd8df604c208e2e86ce143d2c10490a
Asma Chowdhury <asma@cefalo.com>
2022-05-31 18:52:29 +0600

    run scheduler only in production mode

--- core/src/main/java/com/thundashop/core/jomres/JomresManagerProcessor.java
+++ core/src/main/java/com/thundashop/core/jomres/JomresManagerProcessor.java
@@ -2,9 +2,6 @@ package com.thundashop.core.jomres;
 
 import com.getshop.scope.GetShopSchedulerBase;
 import com.thundashop.core.common.GetShopLogHandler;
-import com.thundashop.core.jomres.dto.FetchBookingResponse;
-
-import java.util.List;
 
 public class JomresManagerProcessor extends GetShopSchedulerBase {
     public JomresManagerProcessor(String webAddress, String username, String password, String scheduler, String multiLevelName) throws Exception {
@@ -13,16 +10,16 @@ public class JomresManagerProcessor extends GetShopSchedulerBase {
 
     @Override
     public void execute() throws Exception {
-//        if(!getApi().getStoreManager().isProductMode()) {
-//            return;
-//        }
+        if(!getApi().getStoreManager().isProductMode()) {
+            return;
+        }
 
         String storeId = getApi().getStoreManager().getStoreId();
 
         String multilevelname = getMultiLevelName();
         try {
-            List<FetchBookingResponse> booking = getApi().getJomresManager().fetchBookings(multilevelname);
-            boolean availability = getApi().getJomresManager().updateAvailability(multilevelname);
+            getApi().getJomresManager().fetchBookings(multilevelname);
+            getApi().getJomresManager().updateAvailability(multilevelname);
         }catch(Exception e) {
             GetShopLogHandler.logPrintStatic("Failed to handle jomres api call, " + e.getMessage() + " multilevelname: " + multilevelname, storeId);
             GetShopLogHandler.logStack(e, storeId);

a2b37ca8863e709e69f9d329a8c29db4c0948600
Asma Chowdhury <asma@cefalo.com>
2022-05-31 15:58:23 +0600

    remove real values in jomres configuration, added default values

--- core/src/main/java/com/thundashop/core/jomres/JomresConfiguration.java
+++ core/src/main/java/com/thundashop/core/jomres/JomresConfiguration.java
@@ -3,15 +3,14 @@ package com.thundashop.core.jomres;
 import com.thundashop.core.common.DataCommon;
 
 public class JomresConfiguration extends DataCommon {
-    String CMF_CLIENT_TOKEN_URL = "https://jomres.zauistay.com/jomres/api/";
-    String CLIENT_BASE_URL = "https://jomres.zauistay.com/jomres/api/";
+    String CMF_CLIENT_TOKEN_URL = "";
+    String CLIENT_BASE_URL = "";
 
-    String ADMIN_CLIENT_ID = "system";
-    String ADMIN_CLIENT_SECRET = "DpnPPlSlmbnXLLbxNaioNqDJQwidlCdubqTnlRUHWpsznVgIqo";
-    String JOMRES_CMF_REST_API_CLIENT_ID = "xUirogUaOFQFOQF";
-    String JOMRES_CMF_REST_API_CLIENT_SECRET = "HdZDhHbHPxGXjNhZsQSLDDvbrcgLgaDXfGhfyawUJNbbLALtxs";
+    String ADMIN_CLIENT_ID = "";
+    String ADMIN_CLIENT_SECRET = "";
+    String JOMRES_CMF_REST_API_CLIENT_ID = "";
+    String JOMRES_CMF_REST_API_CLIENT_SECRET = "";
 
-    int CMS_USER_ID = 1743;
-    String CHANNEL_NAME = "test_channel";
-    int CHANNEL_ID = 1;
+    int CMS_USER_ID = 0;
+    String CHANNEL_NAME = "";
 }

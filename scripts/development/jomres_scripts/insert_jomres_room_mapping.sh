eb758a4c0890bf5530088b7e4d7b719734ad72ce
Asma Chowdhury <asma@cefalo.com>
2022-06-06 00:09:17 +0600

    eliminate unnecessary variables from Jomres script

--- scripts/development/jomres_scripts/insert_jomres_room_mapping.sh
+++ scripts/development/jomres_scripts/insert_jomres_room_mapping.sh
@@ -1,5 +1,4 @@
-storeId='687cd85e-4812-405a-9532-7748acc29d13';
-host='localhost:27018'
+storeId='687cd85e-4812-405a-9532-7748acc29d13'
 hotelName="development_hotel"
-mongo --port=27018 --eval "const storeId='$storeId', host='$host', hotelName='$hotelName'" \
+mongo --port=27018 --eval "const storeId='$storeId', hotelName='$hotelName'" \
  insertion_room_mapping_helper.js

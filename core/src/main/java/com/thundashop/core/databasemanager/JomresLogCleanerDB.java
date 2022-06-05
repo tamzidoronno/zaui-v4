0dafc83fd2044b6133b0a96bf9c7df5a677385d8
Asma Chowdhury <asma@cefalo.com>
2022-06-06 02:04:33 +0600

    add Jomres Log cleaner scheduler

---
+++ core/src/main/java/com/thundashop/core/databasemanager/JomresLogCleanerDB.java
@@ -0,0 +1,33 @@
+package com.thundashop.core.databasemanager;
+
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class JomresLogCleanerDB {
+
+    private final Mongo mongo;
+
+    @Autowired
+    public JomresLogCleanerDB(@Qualifier("localMongo") MongoClientProvider provider) {
+        this.mongo = provider.getMongoClient();
+    }
+
+    public Set<String> getAllCollection(String manager) {
+        return mongo.getDB(manager).getCollectionNames();
+    }
+
+    public int deleteByQuery(String manager, String collectionName, DBObject query) {
+        return mongo.getDB(manager)
+                .getCollection(collectionName)
+                .remove(query)
+                .getN();
+    }
+
+
+}

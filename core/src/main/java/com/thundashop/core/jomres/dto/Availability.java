9570bbfd172ad587d62e526db0aac81cf9404c3c
Asma Chowdhury <asma@cefalo.com>
2022-06-08 12:19:38 +0600

    add todo and comment as explanation of using "_" in property name

--- core/src/main/java/com/thundashop/core/jomres/dto/Availability.java
+++ core/src/main/java/com/thundashop/core/jomres/dto/Availability.java
@@ -1,6 +1,9 @@
 package com.thundashop.core.jomres.dto;
 
 public class Availability {
+    // TODO: will use json_property annotation and camel form of variable name
+
+    //Camel format is not used for Gson convertion of Jomres response. Please don't change the variable names
     String date_from;
     String date_to;
     boolean available;

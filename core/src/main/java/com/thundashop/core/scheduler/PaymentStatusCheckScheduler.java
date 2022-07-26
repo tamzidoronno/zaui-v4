d12a4eba03840108307cf0e4d142a25b5169d5e4
naim <naim.murad@cefalo.com>
2022-07-26 19:47:21 +0600

    Correct log level

--- core/src/main/java/com/thundashop/core/scheduler/PaymentStatusCheckScheduler.java
+++ core/src/main/java/com/thundashop/core/scheduler/PaymentStatusCheckScheduler.java
@@ -1,15 +1,23 @@
 package com.thundashop.core.scheduler;
 
-import com.thundashop.core.paymentmanager.PaymentStatusCheckService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.getshop.scope.GetShopSchedulerBase;
 
 /**
  * @author Naim Murad (naim)
  * @since 6/28/22
  */
-public class PaymentStatusCheckScheduler {
+public class PaymentStatusCheckScheduler extends GetShopSchedulerBase {
 
-    @Autowired private PaymentStatusCheckService service;
+    public PaymentStatusCheckScheduler(String webAddress, String username, String password, String scheduler, String multiLevelName) throws Exception {
+        super(webAddress, username, password, scheduler, multiLevelName);
+    }
 
+    public PaymentStatusCheckScheduler() {
+    }
+
+    @Override
+    public void execute() throws Exception {
+        getApi().getOrderManager().checkPaymentStatusAndUpdatePayment();
+    }
 
 }

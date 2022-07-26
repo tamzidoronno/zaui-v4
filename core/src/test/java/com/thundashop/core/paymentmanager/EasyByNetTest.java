de756130d3d9e5ac9607577e51de0b2762ca0461
naim <naim.murad@cefalo.com>
2022-07-26 19:21:57 +0600

    * Added test case

--- core/src/test/java/com/thundashop/core/paymentmanager/EasyByNetTest.java
+++ core/src/test/java/com/thundashop/core/paymentmanager/EasyByNetTest.java
@@ -1,27 +1,32 @@
 package com.thundashop.core.paymentmanager;
 
+import com.getshop.bookingengine.BookingEngineTestContext;
+import com.thundashop.core.bookingengine.BookingEngine;
+import com.thundashop.core.databasemanager.data.Credentials;
+import com.thundashop.core.paymentmanager.EasyByNets.DTO.RetrievePayment;
 import com.thundashop.core.paymentmanager.EasyByNets.EasyByNetService;
-import com.thundashop.core.storemanager.StoreIdRepository;
-import com.thundashop.core.storemanager.data.Store;
-import org.junit.After;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.UUID;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {BookingEngineTestContext.class})
 public class EasyByNetTest {
     // To run this tests, a mongo needs to be run on host: localhost and port: 27019
 
-    @Autowired
-    EasyByNetService easyByNetService;
+    //@Autowired private EasyByNetService easyByNetService;
 
     @Test
     public void testGetNextIncrementalStoreId() {
-        easyByNetService.retrievePayment("","sss");
-        //Assert.assertEquals(3, nextIncrementalStoreId);
+        RetrievePayment pay = new RetrievePayment();
+       // TODO initialize StorePool pay = easyByNetService.retrievePayment("sss");
+        assertNotNull(pay);
     }
+
+
 }

5778465094321fb5b352fc57232ffeb5b16c022d
tamzid_cefalo <tamzid@cefalo.com>
2022-08-10 09:08:54 +0600

    added new endpoint for syncing backup data

--- core/src/main/java/com/thundashop/core/storemanager/BackupServerSyncManager.java
+++ core/src/main/java/com/thundashop/core/storemanager/BackupServerSyncManager.java
@@ -2,17 +2,18 @@ package com.thundashop.core.storemanager;
 
 import com.getshop.scope.GetShopSessionScope;
 import com.google.gson.Gson;
+import com.thundashop.core.common.ErrorException;
 import com.thundashop.core.getshoplocksystem.GetShopLockSystemManager;
 import com.thundashop.core.getshoplocksystem.LockServer;
 import com.thundashop.core.getshoplocksystem.LockServerBase;
 import com.thundashop.core.gsd.GdsManager;
 import com.thundashop.core.gsd.GetShopDevice;
 import com.thundashop.core.webmanager.WebManager;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -68,8 +69,12 @@ public class BackupServerSyncManager {
                 try {
                     info.webaddr = storeManager.getMyStore().getDefaultWebAddress();
                     info.storeId = storeId;
-                    String toSend = URLEncoder.encode(new Gson().toJson(info), "UTF-8");
-                    String result = webManager.htmlGet("http://10.0.7.10:8800/?setServer=" + toSend);
+                    String toSend = new Gson().toJson(info);
+                    String result = webManager.htmlPostBasicAuth("https://api.pibackup.zauistay.tech/api/sync" ,toSend,true,"UTF-8", "z@uiBack$tay2022");
+                    if(StringUtils.isBlank(result)) {
+                        storeManager.logPrintException(new ErrorException(500, "Result not found!"));
+                        continue;
+                    }
 
                     if (result.equals("added")) {
                         RemoteServerMetaData serverInfo = new RemoteServerMetaData();

e48528ab647557b0d4036a3a7a1770984dad517d
Asma Chowdhury <asma@cefalo.com>
2022-08-23 10:59:46 +0600

    use logger error instead of info for exception

--- core/src/main/java/com/thundashop/core/jomres/services/PropertyService.java
+++ core/src/main/java/com/thundashop/core/jomres/services/PropertyService.java
@@ -70,8 +70,8 @@ public class PropertyService extends BaseService{
                 jomresProperties.add((JomresProperty) result.get());
             } catch (ExecutionException | InterruptedException e) {
                 logger.error(Throwables.getStackTraceAsString(e));
-                logger.info("Failed to get a property, check log files");
-                logger.info(e.getMessage());
+                logger.error("Failed to get a property, check log files");
+                logger.error(e.getMessage());
             }
         }
         return jomresProperties.stream()

b17101283a794879a1e62597871325aaae6b28f1
tamzid_cefalo <tamzid@cefalo.com>
2022-08-31 13:15:13 +0600

    adding securityListSchedule to PmsConfiguration dto

---
+++ messages/src/main/java/com/thundashop/core/pmsmanager/SecurityListSchedule.java
@@ -0,0 +1,10 @@
+package com.thundashop.core.pmsmanager;
+
+import com.thundashop.core.common.DataCommon;
+
+import java.util.List;
+
+public class SecurityListSchedule extends DataCommon {
+    private List<String> emails = null;
+    private Integer schedule = 3;
+}

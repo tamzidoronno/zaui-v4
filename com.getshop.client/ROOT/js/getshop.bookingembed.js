2ea71166015fc3cb243969454d43ff512f732dc9
tamzid_cefalo <tamzid@cefalo.com>
2022-09-23 12:53:56 +0600

    removing unused suffix

--- com.getshop.client/ROOT/js/getshop.bookingembed.js
+++ com.getshop.client/ROOT/js/getshop.bookingembed.js
@@ -1,68 +1,59 @@
 (function ($) {
-  $.fn.getshopbooking = function (options) {
-    old_url = options.jsendpoint;
-    let splitted = old_url.split(".");
-    let hotelCode = splitted[0].replace("https://", "");
-    let glueware_url = getGluewareUrl(hotelCode);
-    options.webaddress = options.endpoint;
-    options.endpoint = glueware_url;
-    $.getScript(`${glueware_url}/js/getshop.bookingembed.js`, function () {
-      $("#bookingprocess").getshopbooking(options);
-    });
+    $.fn.getshopbooking = function (options) {
+      old_url = options.jsendpoint;
+      let splitted = old_url.split(".");
+      let hotelCode = splitted[0].replace("https://", "");
+      let glueware_url = getGluewareUrl(hotelCode);
+      options.webaddress = options.endpoint;
+      options.endpoint = glueware_url;
+      $.getScript(`${glueware_url}/js/getshop.bookingembed.js`, function () {
+        $("#bookingprocess").getshopbooking(options);
+      });
+    };
+  })(jQuery);
+
+  function getGluewareUrl(hotelCode) {
+    let gluewareUrl = "";
+    if (hotelCode.endsWith(ClusterSuffix.Cluster9)) {
+      gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Cluster9];
+    } else if (hotelCode.endsWith(ClusterSuffix.Cluster5)) {
+      gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Cluster5];
+    }
+    else if (hotelCode.endsWith(ClusterSuffix.Cluster6)) {
+      gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Cluster6];
+    }
+    else if (hotelCode.endsWith(ClusterSuffix.Cluster41)) {
+      gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Cluster41];
+    }
+    else if (hotelCode.endsWith(ClusterSuffix.Qp)) {
+      gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Qp];
+    }
+    else if (hotelCode.endsWith(ClusterSuffix.Local)) {
+      gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Local];
+    }
+    else {
+    // as all named domain hotels reside at c4
+      gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Cluster4];
+    }
+
+    return gluewareUrl;
+  }
+  const ClusterSuffix = {
+    Cluster9: "c9",
+    Cluster4: "c4",
+    Cluster5: "c5",
+    Cluster6: "c6",
+    Cluster41: "c41",
+    Qp: "qp",
+    Local: "local",
   };
-})(jQuery);
 
-function getGluewareUrl(hotelCode) {
-  let gluewareUrl = "";
-  let suffix = "";
-
-  if (hotelCode.endsWith(ClusterSuffix.Cluster9)) {
-    gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Cluster9];
-    suffix = ClusterSuffix.Cluster9;
-  } else if (hotelCode.endsWith(ClusterSuffix.Cluster5)) {
-    gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Cluster5];
-    suffix = ClusterSuffix.Cluster5;
-  }
-  else if (hotelCode.endsWith(ClusterSuffix.Cluster6)) {
-    gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Cluster6];
-    suffix = ClusterSuffix.Cluster6;
-  }
-  else if (hotelCode.endsWith(ClusterSuffix.Cluster41)) {
-    gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Cluster41];
-    suffix = ClusterSuffix.Cluster41;
-  }
-  else if (hotelCode.endsWith(ClusterSuffix.Qp)) {
-    gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Qp];
-    suffix = ClusterSuffix.Qp;
-  }
-  else if (hotelCode.endsWith(ClusterSuffix.Local)) {
-    gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Local];
-    suffix = ClusterSuffix.Local;
-  }
-  else {
-  // as all named domain hotels reside at c4
-    gluewareUrl = GLUEWARE_URL_LIST[ClusterSuffix.Cluster4];
-    suffix = ClusterSuffix.Cluster4;
-  }
-
-  return gluewareUrl;
-}
-const ClusterSuffix = {
-  Cluster9: "c9",
-  Cluster4: "c4",
-  Cluster5: "c5",
-  Cluster6: "c6",
-  Cluster41: "c41",
-  Qp: "qp",
-  Local: "local",
-};
-
-const GLUEWARE_URL_LIST = {
-  [ClusterSuffix.Cluster9]: "https://gluegc9.getshop.com/",
-  [ClusterSuffix.Cluster6]: "https://glueware6.zauistay.com/",
-  [ClusterSuffix.Cluster4]: "https://glueware4.zauistay.com/",
-  [ClusterSuffix.Cluster5]: "https://glueware5.zauistay.com/",
-  [ClusterSuffix.Cluster41]: "https://glueware41.zauistay.com/",
-  [ClusterSuffix.Qp]: "https://glue.mdev.getshop.com/",
-  [ClusterSuffix.Local]: "http://localhost:3000/",
-};
+  const GLUEWARE_URL_LIST = {
+    [ClusterSuffix.Cluster9]: "https://gluegc9.getshop.com/",
+    [ClusterSuffix.Cluster6]: "https://glueware6.zauistay.com/",
+    [ClusterSuffix.Cluster4]: "https://glueware4.zauistay.com/",
+    [ClusterSuffix.Cluster5]: "https://glueware5.zauistay.com/",
+    [ClusterSuffix.Cluster41]: "https://glueware41.zauistay.com/",
+    [ClusterSuffix.Qp]: "https://glue.mdev.getshop.com/",
+    [ClusterSuffix.Local]: "http://localhost:3000/",
+  };

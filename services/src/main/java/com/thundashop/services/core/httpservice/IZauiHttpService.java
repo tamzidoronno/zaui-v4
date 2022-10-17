3a8710df4c506c81345f03c5f9c65d7b1502b1c9
manik-getshop <mahbubur@cefalo.com>
2022-10-17 20:21:22 +0600

    - add service for zauiHttpService

--- services/src/main/java/com/thundashop/services/core/httpservice/IZauiHttpService.java
+++ services/src/main/java/com/thundashop/services/core/httpservice/IZauiHttpService.java
@@ -1,5 +1,7 @@
 package com.thundashop.services.core.httpservice;
 
 public interface IZauiHttpService {
+    OkHttpResponse get(OkHttpRequest httpRequest);
+    OkHttpResponse post(OkHttpRequest httpRequest);
     
 }

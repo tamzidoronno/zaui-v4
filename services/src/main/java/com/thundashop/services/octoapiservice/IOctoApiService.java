12a331542cd94404c5bf682dc28d99655ae9f9c4
manik-getshop <mahbubur@cefalo.com>
2022-11-17 22:17:05 +0600

    - refactor octoapi service

--- services/src/main/java/com/thundashop/services/octoapiservice/IOctoApiService.java
+++ services/src/main/java/com/thundashop/services/octoapiservice/IOctoApiService.java
@@ -7,9 +7,15 @@ import com.thundashop.zauiactivity.dto.*;
 
 public interface IOctoApiService {
     public String getSupplierName(Integer supplierId);
+
     public List<OctoProduct> getProducts(Integer supplierId) throws IOException;
+
     public List<Availability> getAvailability(Integer supplierId, AvailabilityRequest availabilityRequest);
+
     public List<BookingReserve> reserveBooking(Integer supplierId, BookingReserveRequest bookingReserveRequest);
-    public List<BookingConfirm> confirmBooking(Integer supplierId, String bookingId, BookingConfirmRequest bookingConfirmRequest);
+
+    public List<BookingConfirm> confirmBooking(Integer supplierId, String bookingId,
+            BookingConfirmRequest bookingConfirmRequest);
+
     public List<BookingConfirm> cancelBooking(Integer supplierId, String bookingId);
 }

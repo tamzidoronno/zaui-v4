07f063e04f21ab3b8bde6305ca17268d18146a93
manik-getshop <mahbubur@cefalo.com>
2022-11-17 13:33:01 +0600

    - move http3 library to common gradle
     - remove unused interface from Iwebmanager

--- core/src/main/java/com/thundashop/core/webmanager/IWebManager.java
+++ core/src/main/java/com/thundashop/core/webmanager/IWebManager.java
@@ -8,9 +8,6 @@ package com.thundashop.core.webmanager;
 import com.google.gson.JsonObject;
 import com.thundashop.core.common.GetShopApi;
 
-import java.io.IOException;
-import java.util.Map;
-
 /**
  *
  * @author hung
@@ -18,19 +15,15 @@ import java.util.Map;
 
 @GetShopApi
 public interface IWebManager {
-    
-    public String htmlGet(String url) throws Exception;
-    
-    public JsonObject htmlGetJson(String url) throws Exception;
-    
-    public String htmlPost(String url, String data, boolean jsonPost, String encoding) throws Exception;
-    
-    public String htmlPostBasicAuth(String url, String data, boolean jsonPost, String encoding, String auth) throws Exception;
-    
-    public JsonObject htmlPostJson(String url, JsonObject data, String encoding) throws Exception;
 
-    public default String getResponseWithHeaders(
-            String url, String accessToken, Map<String, String> headers, Object requestBody, Class classType, String method) throws IOException {
-        return "";
-    }
+    public String htmlGet(String url) throws Exception;
+
+    public JsonObject htmlGetJson(String url) throws Exception;
+
+    public String htmlPost(String url, String data, boolean jsonPost, String encoding) throws Exception;
+
+    public String htmlPostBasicAuth(String url, String data, boolean jsonPost, String encoding, String auth)
+            throws Exception;
+
+    public JsonObject htmlPostJson(String url, JsonObject data, String encoding) throws Exception;
 }

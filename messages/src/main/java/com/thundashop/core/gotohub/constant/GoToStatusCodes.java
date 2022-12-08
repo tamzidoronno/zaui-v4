7a3c92f4a86aea78188199e592c54a530754c011
manik-getshop <mahbubur@cefalo.com>
2022-12-08 16:02:23 +0600

    - improve valitation

--- messages/src/main/java/com/thundashop/core/gotohub/constant/GoToStatusCodes.java
+++ messages/src/main/java/com/thundashop/core/gotohub/constant/GoToStatusCodes.java
@@ -14,12 +14,14 @@ public enum GoToStatusCodes {
     ROOM_TYPE_NOT_FOUND(1302, "Goto Booking Failed.. Reason: Room type not found"),
     EMPTY_ROOM_LIST(1303, "Goto Booking Failed.. Reason: Empty room list"),
     NO_ALLOTMENT(1304, "Goto Booking Failed.. Reason: No Allotment Found for the time period"),
-    INVALID_CHECKIN_CHECKOUT_FORMAT(1305, "Goto Booking Failed.. Reason: Invalid checkin/ checkout date format"),
+    INVALID_CHECKIN_CHECKOUT_FORMAT(1305, "Goto Booking Failed.. Reason: Invalid checkin/checkout date format"),
     INVALID_RATE_PLAN_CODE(1306, "Goto Booking Failed.. Reason: Invalid rate-plan code"),
-    NUMBER_OF_GUESTS_RATE_PLAN_CODE_MISMATCHED(1307, "Goto Booking Failed.. Reason: Number of guests not matched with rate-plan"),
-    OVERFLOW_MAX_NUMBER_OF_GUESTS(1308, "Goto Booking Failed.. Reason: Number of guests overflowed max number of guests"),
+    NUMBER_OF_GUESTS_RATE_PLAN_CODE_MISMATCHED(1307,
+            "Goto Booking Failed.. Reason: Number of guests not matched with rate-plan"),
+    OVERFLOW_MAX_NUMBER_OF_GUESTS(1308,
+            "Goto Booking Failed.. Reason: Number of guests overflowed max number of guests"),
     SAVE_BOOKING_FAIL(1309, "Goto Booking Failed.. Reason: Unknown"),
-    PRICE_MISSING(1310, "Goto Booking Failed.. Reason: Price/daily price is missing or invalid for room"),
+    PRICE_MISSING(1310, "Goto Booking Failed.. Reason: Price/daily price is missing or invalid"),
     INVALID_DATE_RANGE_BOOKING(1311, "Goto Booking Failed.. Reason: Invalid checkin - checkout date range"),
     INCORRECT_DAILY_PRICE_MATRIX(1312, "Goto Booking Failed.. Reason: Daily price matrix is incorrect"),
     EMAIL_OR_PHONE_MISSING(1313, "Goto Booking Failed.. Reason: " +
@@ -29,14 +31,16 @@ public enum GoToStatusCodes {
     PAYMENT_FAILED(1402, "Goto Booking Confirmation Failed.. Reason: Payment failed"),
     PAYMENT_METHOD_NOT_FOUND(1403, "Goto Booking Payment Confirmation Failed.. " +
             "Reason: No Payment Method found in Goto Configuration"),
-    PAYMENT_METHOD_ACTIVATION_FAILED(1404, "Goto Booking Confirmation Failed.. Reason: Payment method activation failed"),
+    PAYMENT_METHOD_ACTIVATION_FAILED(1404,
+            "Goto Booking Confirmation Failed.. Reason: Payment method activation failed"),
     BOOKING_DELETED(1405, "Goto Booking Confirmation Failed.. Reason: Booking Has Been Deleted"),
     BOOKING_CONFIRMATION_FAILED(1409, "Goto Booking Confirmation Failed.. Reason: Unknown"),
     BOOKING_CANCELLATION_SUCCESS(1500, "Goto Booking has been Cancelled"),
     BOOKING_CANCELLATION_NOT_FOUND(1501, "Goto Booking Cancellation Failed.. Reason: Booking Not Found"),
     ORDER_SYNCHRONIZATION_FAILED(1502, "Goto Booking Cancellation Failed.. Reason: Order Synchronization Failed"),
     CANCELLATION_DEADLINE_PASSED(1503, "Goto Booking Cancellation Failed.. Reason: Cancellation DeadLine Has Passed"),
-    BOOKING_CANCELLATION_ALREADY_CANCELLED(1504, "Goto Booking Cancellation Failed.. Reason: Booking is Already Cancelled"),
+    BOOKING_CANCELLATION_ALREADY_CANCELLED(1504,
+            "Goto Booking Cancellation Failed.. Reason: Booking is Already Cancelled"),
     BOOKING_CANCELLATION_FAILED(1509, "Goto Booking Cancellation Failed.. Reason: Unknown");
 
     public final long code;

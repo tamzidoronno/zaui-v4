52e9f122f404a899773dc418a692d64e89684ae0
manik-getshop <mahbubur@cefalo.com>
2022-12-21 17:20:44 +0600

    - fix build error

--- messages/src/main/java/com/thundashop/zauiactivity/dto/BookingZauiActivityItem.java
+++ messages/src/main/java/com/thundashop/zauiactivity/dto/BookingZauiActivityItem.java
@@ -1,8 +1,12 @@
 package com.thundashop.zauiactivity.dto;
 
 import com.thundashop.core.pmsmanager.PmsBookingAddonItem;
+
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class BookingZauiActivityItem extends PmsBookingAddonItem {
     public String id = java.util.UUID.randomUUID().toString();
     public String zauiActivityId;

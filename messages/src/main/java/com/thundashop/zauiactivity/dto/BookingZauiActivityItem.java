025f0d698b16a1772edfdaa355b32e996997311f
manik-getshop <mahbubur@cefalo.com>
2022-12-21 17:14:45 +0600

    - fix warning

--- messages/src/main/java/com/thundashop/zauiactivity/dto/BookingZauiActivityItem.java
+++ messages/src/main/java/com/thundashop/zauiactivity/dto/BookingZauiActivityItem.java
@@ -1,11 +1,8 @@
 package com.thundashop.zauiactivity.dto;
 
 import com.thundashop.core.pmsmanager.PmsBookingAddonItem;
-import lombok.Data;
-
 import java.util.List;
 
-@Data
 public class BookingZauiActivityItem extends PmsBookingAddonItem {
     public String id = java.util.UUID.randomUUID().toString();
     public String zauiActivityId;
@@ -20,6 +17,5 @@ public class BookingZauiActivityItem extends PmsBookingAddonItem {
     public String localDateTimeEnd;
     public Integer supplierId;
     public String supplierName;
-    private OctoBooking octoBooking;
-
+    public OctoBooking octoBooking;
 }

130c62f92897a2b6fa90162574fb1f1d09c201b7
manik-getshop <mahbubur@cefalo.com>
2022-12-25 14:03:31 +0600

    - code refactoring

--- core/src/main/java/com/thundashop/core/jomres/dto/JomresProperty.java
+++ core/src/main/java/com/thundashop/core/jomres/dto/JomresProperty.java
@@ -1,26 +1,13 @@
 package com.thundashop.core.jomres.dto;
 
-public class JomresProperty {
+import lombok.Data;
 
-    // TODO: Aceess modifier will be added in refactor branch
+@Data
+public class JomresProperty {
     int propertyId = 0;
     String propertyName = "";
 
     public JomresProperty() {
-    }
-
-    public void setPropertyId(int propertyId) {
-        this.propertyId = propertyId;
-    }
-
-    public int getPropertyId() {
-        return propertyId;
-    }
-
-
-    //TODO: @DATA annotation will be used instead of getter-setter
-    public void setPropertyName(String propertyName) {
-        this.propertyName = propertyName;
     }
 
     public JomresProperty(int propertyId, String propertyName) {
